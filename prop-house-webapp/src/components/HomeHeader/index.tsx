55bc07921843b0f9dc919a7cf64c05ce6c770e3e
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2021-11-16 10:15:25 -0500

    Style HomeHeader

--- prop-house-webapp/src/components/HomeHeader/index.tsx
+++ prop-house-webapp/src/components/HomeHeader/index.tsx
@@ -1,18 +1,22 @@
-import { Col, Row, Image, Button } from 'react-bootstrap';
+import { Col, Row, Image } from 'react-bootstrap';
+import Button, { ButtonColor } from '../Button';
+import classes from './HomeHeader.module.css';
 import grempBulb from '../../assets/gremp-lightbulb.png';
 
 const HomeHeader = () => {
   return (
     <Row>
       <Col xl={6}>
-        <h1>Weekly funding for your ideas</h1>
+        <h1 className="display-1">Weekly funding for your ideas</h1>
         <p>
           Bring your ideas to life by submitting a proposal to Nouns DAO.
           Auctions are held regularly, and made available to anyone, anywhere.
           Get funded!
         </p>
-        <Button>Learn more</Button>
-        <Button>Submit Proposal</Button>
+        <div className={classes.btnContainer}>
+          <Button text="Learn more" bgColor={ButtonColor.Pink} />
+          <Button text="Submit proposal" bgColor={ButtonColor.White} />
+        </div>
       </Col>
       <Col xl={{ span: 4, offset: 1 }}>
         <Image src={grempBulb} fluid />
