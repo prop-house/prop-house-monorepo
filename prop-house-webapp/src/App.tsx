5ba316ebb307f12336c7d2faa2af2d26708f8125
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2021-11-16 07:25:59 -0500

    Modify bootstrap max container width

--- prop-house-webapp/src/App.tsx
+++ prop-house-webapp/src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import '../src/css/globals.css';
 import Home from './components/pages/Home';
 import NotFound from './components/pages/NotFound';
 import './App.css';
