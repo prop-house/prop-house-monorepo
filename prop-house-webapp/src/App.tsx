08a1dc37e21c29975a0082f14d48181b5a3ebb8d
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2021-11-16 07:05:15 -0500

    Add react router

--- prop-house-webapp/src/App.tsx
+++ prop-house-webapp/src/App.tsx
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
---
+++ prop-house-webapp/src/components/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from 'react-bootstrap';
+
+const Home = () => {
+  return (
+    <Container>
+      <h1>Home</h1>
+    </Container>
+  );
+};
+
+export default Home;
