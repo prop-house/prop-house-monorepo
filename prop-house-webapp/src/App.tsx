726045486959f7a32a7ed4b0c379d2b4fc5acfff
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2021-11-17 11:23:39 -0500

    Align Containers between NavBar and content

--- prop-house-webapp/src/App.tsx
+++ prop-house-webapp/src/App.tsx
@@ -1,20 +1,21 @@
 import { Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../src/css/globals.css';
-import NavBar from './components/NavBar/NavBar';
+import NavBar from './components/NavBar';
 import Home from './components/pages/Home';
 import NotFound from './components/pages/NotFound';
+import { Container } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
-    <>
+    <Container>
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </>
+    </Container>
   );
 }
 
