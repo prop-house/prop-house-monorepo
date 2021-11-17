e5d52eaeee28f7b4155c01552ef41e22374faf61
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2021-11-17 11:40:04 -0500

    Add Row and Cols to CreateProposalCTA

--- prop-house-webapp/src/components/CreateProposalCTA/index.tsx
+++ prop-house-webapp/src/components/CreateProposalCTA/index.tsx
@@ -1,22 +1,26 @@
 import classes from './CreateProposalCTA.module.css';
 import Card, { CardBgColor, CardBorderRadius } from '../Card';
 import Button, { ButtonColor } from '../Button';
-import { Form } from 'react-bootstrap';
+import { Form, Row, Col } from 'react-bootstrap';
 
 const CreateProposalCTA = () => {
   return (
-    <Card bgColor={CardBgColor.Dark} borderRadius={CardBorderRadius.twenty}>
-      <div className={classes.wrapper}>
-        <Form.Control
-          size="lg"
-          type="text"
-          placeholder="Describe what you want funding for..."
-          className={classes.formInput}
-        />
-        <Button text="Create proposal" bgColor={ButtonColor.Pink} />
-        <Button text="About" bgColor={ButtonColor.White} />
-      </div>
-    </Card>
+    <Row>
+      <Col xl={12}>
+        <Card bgColor={CardBgColor.Dark} borderRadius={CardBorderRadius.twenty}>
+          <div className={classes.wrapper}>
+            <Form.Control
+              size="lg"
+              type="text"
+              placeholder="Describe what you want funding for..."
+              className={classes.formInput}
+            />
+            <Button text="Create proposal" bgColor={ButtonColor.Pink} />
+            <Button text="About" bgColor={ButtonColor.White} />
+          </div>
+        </Card>
+      </Col>
+    </Row>
   );
 };
 
