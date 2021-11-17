6104f1c459d96349679daabc1d6e4ccfe1ec78b6
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2021-11-17 14:26:08 -0500

    Fix AuctionHeader display on desktop

--- prop-house-webapp/src/components/AuctionHeader/index.tsx
+++ prop-house-webapp/src/components/AuctionHeader/index.tsx
@@ -10,30 +10,24 @@ const AuctionHeader: React.FC<{
   const { id, fundingAmount, startDate, endDate } = props;
 
   return (
-    <Row>
-      <Col xl={12}>
-        <div className={classes.wrapper}>
-          <div>
-            <h1 className={classes.leftSectionTitle}>{`Auction ${id}`}</h1>
-            <h3 className={classes.leftSectionSubtitle}>
-              {`${new Date(startDate).toDateString()} - ${new Date(
-                endDate
-              ).toDateString()}`}
-            </h3>
-          </div>
-          <div className={classes.rightSection}>
-            <div>
-              <h3 className={classes.rightSectionTitle}>Funding</h3>
-              <h1
-                className={classes.rightSectionSubtitle}
-              >{`${fundingAmount} Ξ`}</h1>
-            </div>
-            <div>
-              <h3 className={classes.rightSectionTitle}>Proposal deadline</h3>
-              <h1 className={classes.rightSectionSubtitle}>3 days left</h1>
-            </div>
-          </div>
+    <Row className={classes.container}>
+      <Col xl={6} className={classes.leftSectionContainer}>
+        <div className={classes.leftSectionTitle}>{`Auction ${id}`}</div>
+        <div className={classes.leftSectionSubtitle}>
+          {`${new Date(startDate).toDateString()} - ${new Date(
+            endDate
+          ).toDateString()}`}
         </div>
+      </Col>
+      <Col xl={3} xs={6} className={classes.rightSectionSubsection}>
+        <div className={classes.rightSectionTitle}>Funding</div>
+        <div className={classes.rightSectionSubtitle}>{`${fundingAmount.toFixed(
+          2
+        )} Ξ`}</div>
+      </Col>
+      <Col xl={3} xs={6} className={classes.rightSectionSubsection}>
+        <div className={classes.rightSectionTitle}>Proposal deadline</div>
+        <div className={classes.rightSectionSubtitle}>3 days left</div>
       </Col>
     </Row>
   );
