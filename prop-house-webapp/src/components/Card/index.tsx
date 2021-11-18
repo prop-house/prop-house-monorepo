63832c048d0b983b586f004af13ae79fd9449955
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2021-11-18 15:18:59 -0500

    Add onHoverEffect prop to Card comp

--- prop-house-webapp/src/components/Card/index.tsx
+++ prop-house-webapp/src/components/Card/index.tsx
@@ -12,20 +12,26 @@ export enum CardBorderRadius {
   thirty,
 }
 
-const Card: React.FC<{ bgColor: CardBgColor; borderRadius: CardBorderRadius }> =
-  (props) => {
-    const _classes = clsx(
-      classes.card,
-      props.bgColor === CardBgColor.LightPurple
-        ? classes.lightPurpleBg
-        : props.bgColor === CardBgColor.DarkPurple
-        ? classes.darkPurpleBg
-        : classes.whiteBg,
-      props.borderRadius === CardBorderRadius.twenty
-        ? classes.borderRadius20
-        : classes.borderRadius30
-    );
-    return <div className={_classes}>{props.children}</div>;
-  };
+const Card: React.FC<{
+  bgColor: CardBgColor;
+  borderRadius: CardBorderRadius;
+  onHoverEffect?: boolean;
+}> = (props) => {
+  const { bgColor, borderRadius, onHoverEffect } = props;
+
+  const _classes = clsx(
+    classes.card,
+    bgColor === CardBgColor.LightPurple
+      ? classes.lightPurpleBg
+      : bgColor === CardBgColor.DarkPurple
+      ? classes.darkPurpleBg
+      : classes.whiteBg,
+    borderRadius === CardBorderRadius.twenty
+      ? classes.borderRadius20
+      : classes.borderRadius30,
+    onHoverEffect && classes.onHoverEffect
+  );
+  return <div className={_classes}>{props.children}</div>;
+};
 
 export default Card;
