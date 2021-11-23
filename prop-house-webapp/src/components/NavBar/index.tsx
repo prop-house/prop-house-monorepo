73356211db864a355b1a69c8f39d37f2b369672c
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2021-11-23 17:32:52 -0500

    Silence warnings

--- prop-house-webapp/src/components/NavBar/index.tsx
+++ prop-house-webapp/src/components/NavBar/index.tsx
@@ -1,4 +1,4 @@
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import classes from './NavBar.module.css';
 
