e4f89168c0cc783a64743fa20eaa05f93ace46fb
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2021-11-23 16:37:23 -0500

    Fix container spacing issues

--- prop-house-webapp/src/components/NavBar/index.tsx
+++ prop-house-webapp/src/components/NavBar/index.tsx
@@ -5,28 +5,26 @@ import classes from './NavBar.module.css';
 const NavBar = () => {
   return (
     <Navbar bg="transparent" expand="lg" className={classes.navbar}>
-      <Container>
-        <Navbar.Brand>
-          <Link to="/" className={classes.navbarBrand}>
-            Prop House
-          </Link>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link>
-              <Link to="/learn" className={classes.link}>
-                Learn
-              </Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="/browse" className={classes.link}>
-                Browse
-              </Link>
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+      <Navbar.Brand>
+        <Link to="/" className={classes.navbarBrand}>
+          Prop House
+        </Link>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ms-auto">
+          <Nav.Link>
+            <Link to="/learn" className={classes.link}>
+              Learn
+            </Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link to="/browse" className={classes.link}>
+              Browse
+            </Link>
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
