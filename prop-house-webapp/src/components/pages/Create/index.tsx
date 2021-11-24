7f621434b048e7fb7b63ccd9a15053c1f31c1faa
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2021-11-24 11:46:22 -0500

    Add Create page

--- prop-house-webapp/src/components/pages/Create/index.tsx
+++ prop-house-webapp/src/components/pages/Create/index.tsx
@@ -1,5 +1,68 @@
+import classes from './Create.module.css';
+import Card, { CardBgColor, CardBorderRadius } from '../../Card';
+import { Row, Col, Form } from 'react-bootstrap';
+import Button, { ButtonColor } from '../../Button';
+import { Link } from 'react-router-dom';
+import clsx from 'clsx';
+
 const Create = () => {
-  return <h1>Create page here</h1>;
+  const descriptionPlaceholderCopy =
+    'What will you be building?\n\nWho will you be building with?\n\nWhat timeline do you expect to complete the project?\n\nShare links to experience or supporting work relevant to your project';
+
+  return (
+    <>
+      <Row>
+        <Col xl={12}>
+          <h1>Create proposal for Auction 1</h1>
+          <p>Proposals will be voted by Nouners to get funded</p>
+        </Col>
+      </Row>
+      <Card bgColor={CardBgColor.White} borderRadius={CardBorderRadius.twenty}>
+        <Row>
+          <Col xl={10}>
+            <p>
+              We encourage proposals which further proliferate Nouns onto the
+              world while accurately representing the Nouns culture. If your
+              auction is chosen, you will be given the responsibility of
+              completing the work you’ve outlined below. Please be descriptive!
+            </p>
+          </Col>
+          <Col xl={2}>
+            <Link to="/learn">
+              <Button text="Learn more" bgColor={ButtonColor.White} />
+            </Link>
+          </Col>
+        </Row>
+      </Card>
+      <Row>
+        <Form>
+          <Form.Group className={classes.inputGroup}>
+            <Form.Label className={classes.inputLabel}>Title</Form.Label>
+            <Form.Control
+              as="textarea"
+              placeholder="Give your proposal a name..."
+              className={classes.input}
+            />
+          </Form.Group>
+
+          <Form.Group className={classes.inputGroup}>
+            <Form.Label className={classes.inputLabel}>Description</Form.Label>
+            <Form.Control
+              as="textarea"
+              placeholder={descriptionPlaceholderCopy}
+              className={clsx(classes.input, classes.descriptionInput)}
+            />
+          </Form.Group>
+        </Form>
+      </Row>
+      <Row>
+        <div className={classes.connectBtnContainer}>
+          <Button text="Connect wallet" bgColor={ButtonColor.Pink} />
+          <span>to submit proposal</span>
+        </div>
+      </Row>
+    </>
+  );
 };
 
 export default Create;
