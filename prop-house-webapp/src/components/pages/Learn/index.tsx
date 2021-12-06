2d0eb5c68101879b6d17fe68c0f13665524185fb
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2021-12-06 11:18:30 -0500

    Update Learn page with img assets

--- prop-house-webapp/src/components/pages/Learn/index.tsx
+++ prop-house-webapp/src/components/pages/Learn/index.tsx
@@ -2,16 +2,28 @@ import classes from './Learn.module.css';
 import { Row, Col, Image } from 'react-bootstrap';
 import Card, { CardBgColor, CardBorderRadius } from '../../Card';
 import Button, { ButtonColor } from '../../Button';
-import grempBulb from '../../../assets/gremp-lightbulb.png';
-import tile from '../../../assets/tile.png';
+import outletsImg from '../../../assets/learn page/outlets.png';
+import auctionImg from '../../../assets/learn page/auction.png';
+import auctionFullImg from '../../../assets/learn page/auction_full.png';
+import communityImg from '../../../assets/learn page/community.png';
 import clsx from 'clsx';
 
 const Learn = () => {
   return (
     <>
       <div className={clsx('break-out', classes.breakOutMobile)}>
-        <Row className={clsx('g-0', classes.row)}>
-          <Col xl={{ span: 5, offset: 1 }}>
+        <Row className={clsx('g-0', classes.row, 'justify-content-start')}>
+          <Col
+            xl={{ span: 5, order: 'first' }}
+            xs={{ order: 'last' }}
+            className={classes.leftCol}
+          >
+            <Image src={outletsImg} fluid className={classes.img} />
+          </Col>
+          <Col
+            xl={{ span: 5, offset: 1, order: 'last' }}
+            xs={{ order: 'first' }}
+          >
             <h2>Plug into the Nouns treasury</h2>
             <p>
               The Nouns DAO treasury has amassed $70M and counting since
@@ -20,8 +32,34 @@ const Learn = () => {
               their ideas.
             </p>
           </Col>
-          <Col xl={6} className={classes.rightCol}>
-            <Image src={grempBulb} fluid className={classes.img} />
+        </Row>
+      </div>
+
+      <div className={clsx('break-out', classes.breakOutMobile)}>
+        <Row className={clsx('g-0', classes.row)}>
+          <Col xl={{ span: 4, offset: 1 }}>
+            <h2>Bid with your ideas in Funding Auctions</h2>
+            <p>
+              Funding auctions are held regularly. They are auctions where the
+              thing being auctioned is ETH and the bids being placed are
+              proposals. Anyone is free to propose anything.
+              <br />
+              <br />
+              At the end of each auction, members of the Nouns DAO, also known
+              as Nouners, vote on which proposal will receive funding.
+            </p>
+          </Col>
+          <Col xl={{ span: 6, offset: 1 }} className={classes.rightCol}>
+            <Image
+              src={auctionImg}
+              fluid
+              className={clsx(classes.img, classes.auctionImg)}
+            />
+            <Image
+              src={auctionFullImg}
+              fluid
+              className={clsx(classes.img, classes.auctionFullImg)}
+            />
           </Col>
         </Row>
       </div>
@@ -29,28 +67,16 @@ const Learn = () => {
       <div className={clsx('break-out', classes.breakOutMobile)}>
         <Row className={clsx('g-0', classes.row, 'justify-content-start')}>
           <Col
-            xl={{ span: 6, order: 'first' }}
+            xl={{ span: 5, order: 'first' }}
             xs={{ order: 'last' }}
             className={classes.leftCol}
           >
-            <Image src={grempBulb} fluid className={classes.img} />
+            <Image src={communityImg} fluid className={classes.img} />
           </Col>
-          <Col xl={{ span: 5, order: 'last' }} xs={{ order: 'first' }}>
-            <h2>Take part in Funding Auctions</h2>
-            <p>
-              Funding auctions are held regularly. They are auctions where the
-              thing being auctioned is ETH and the bids being placed are
-              proposals. Anyone is free to propose anything. At the end of each
-              auction, Noun DAO members (Nouners) vote on which proposal gets
-              funded.
-            </p>
-          </Col>
-        </Row>
-      </div>
-
-      <div className={clsx('break-out', classes.breakOutMobile)}>
-        <Row className={clsx('g-0', classes.row)}>
-          <Col xl={{ span: 5, offset: 1 }}>
+          <Col
+            xl={{ span: 5, offset: 1, order: 'last' }}
+            xs={{ order: 'first' }}
+          >
             <h2>Become part of the community</h2>
             <p>
               Nouns is a builder-first community. As a prop builder, you will
@@ -58,9 +84,6 @@ const Learn = () => {
               open-source IP and get access to resources and support from the
               Nouns community.
             </p>
-          </Col>
-          <Col xl={6} className={classes.rightCol}>
-            <Image src={grempBulb} fluid className={classes.img} />
           </Col>
         </Row>
       </div>
@@ -84,10 +107,6 @@ const Learn = () => {
           </Col>
         </Row>
       </Card>
-
-      <div className="break-out">
-        <Image src={tile} fluid />
-      </div>
     </>
   );
 };
