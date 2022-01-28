4b5ba6f0f7dc9bd89114d8447460a3551657f74e
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-01-28 08:33:56 -0500

    prettier

--- packages/prop-house-webapp/src/components/Auctions/index.tsx
+++ packages/prop-house-webapp/src/components/Auctions/index.tsx
@@ -4,48 +4,19 @@ import AuctionHeader from '../AuctionHeader';
 import FullAuction, { AuctionStatus } from '../FullAuction';
 import { StatusPillState } from '../StatusPill';
 
-
 const AuctionPartial = (auction: StoredAuction, i: number) => (
   <div key={i}>
-  {i === 0 ? (
-      <FullAuction
-        auction={auction}
-      />
-  ): (
-      <AuctionHeader
-        auction={auction}
-      />
-  )}
+    {i === 0 ? (
+      <FullAuction auction={auction} />
+    ) : (
+      <AuctionHeader auction={auction} />
+    )}
   </div>
-)
+);
 
 const Auctions = () => {
-  const auctions = useAppSelector(state => state.propHouse.auctions)
-  return (
-    <>
-      {
-        auctions.map(AuctionPartial)
-      }
-      {/* <FullAuction
-        showAllProposals={false}
-        status={AuctionStatus.AcceptingProposals}
-      />
-      <AuctionHeader
-        id={2}
-        fundingAmount={10}
-        startDate={Date.now()}
-        endDate={Date.now()}
-        status={StatusPillState.AuctionNotStarted}
-      />
-      <AuctionHeader
-        id={3}
-        fundingAmount={20}
-        startDate={Date.now()}
-        endDate={Date.now()}
-        status={StatusPillState.AuctionNotStarted}
-      /> */}
-    </>
-  );
+  const auctions = useAppSelector((state) => state.propHouse.auctions);
+  return <>{auctions.map(AuctionPartial)}</>;
 };
 
 export default Auctions;
