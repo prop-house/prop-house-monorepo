c10c7a99bb67a1c2e1b692d39bbc7d352903de0b
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-02-03 18:17:15 -0500

    Add useWeb3Modal hook to set provider using the web 3 modal

--- packages/prop-house-webapp/src/components/Web3ModalButton.tsx/index.tsx
+++ packages/prop-house-webapp/src/components/Web3ModalButton.tsx/index.tsx
@@ -1,30 +1,16 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useEthers, shortenAddress, useLookupAddress } from '@usedapp/core';
-import { web3Modal } from '../../utils/web3Modal';
 import classes from './Web3ModalButton.module.css';
+import useWeb3Modal from '../../hooks/useWeb3Modal';
 import clsx from 'clsx';
 
 const Web3ModalButton: React.FC<{
   classNames?: string | string[];
 }> = (props) => {
   const { classNames } = props;
-  const { account, activate, deactivate, error } = useEthers();
+  const { account, deactivate } = useEthers();
+  const connect = useWeb3Modal();
   const ens = useLookupAddress();
-
-  useEffect(() => {
-    if (error) {
-      console.log('error!: ', error);
-    }
-  }, [error]);
-
-  const activateProvider = async () => {
-    try {
-      const provider = await web3Modal.connect();
-      await activate(provider);
-    } catch (error: any) {
-      console.log(error);
-    }
-  };
 
   return (
     <div className={classes.wrapper}>
@@ -38,7 +24,7 @@ const Web3ModalButton: React.FC<{
           </div>
         </>
       ) : (
-        <div className={clsx(classNames)} onClick={activateProvider}>
+        <div className={clsx(classNames)} onClick={connect}>
           Connect
         </div>
       )}
