2c78e2e5aaac6d4cb0b2995ac848299a468714db
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-02-07 14:56:36 -0500

    Add Nouner signifier

--- packages/prop-house-webapp/src/components/FullAuction/index.tsx
+++ packages/prop-house-webapp/src/components/FullAuction/index.tsx
@@ -5,6 +5,8 @@ import ProposalCards from '../ProposalCards';
 import AllProposalsCTA from '../AllProposalsCTA';
 import { Row, Col } from 'react-bootstrap';
 import { StoredAuction } from '@nouns/prop-house-wrapper/dist/builders';
+import auctionStatus from '../../utils/auctionStatus';
+import { StatusPillState } from '../StatusPill';
 
 export enum AuctionStatus {
   NotStarted,
@@ -20,31 +22,44 @@ const FullAuction: React.FC<{
   const { auction, showAllProposals } = props;
 
   return (
-    <Card
-      bgColor={CardBgColor.LightPurple}
-      borderRadius={CardBorderRadius.thirty}
-    >
-      <AuctionHeader auction={auction} />
-      <Row>
-        <Col xs={4} md={2}>
-          <div className={classes.proposalTitle}>Proposals</div>
-        </Col>
-        <Col xs={8} md={10}>
-          <div className={classes.divider} />
-        </Col>
-      </Row>
-      <ProposalCards
-        proposals={
-          showAllProposals ? auction.proposals : auction.proposals.slice(0, 6)
-        }
-      />
-      {!showAllProposals && (
-        <AllProposalsCTA
-          numProposals={auction.proposals.length}
-          auctionId={auction.id}
-        />
+    <>
+      {auctionStatus(auction) === StatusPillState.AuctionVoting && (
+        <Card
+          bgColor={CardBgColor.White}
+          borderRadius={CardBorderRadius.twenty}
+        >
+          <div>
+            You are a Noun owner! Cast your vote for the proposal you believe
+            should receive funding. (TODO: VALIDATE ADDRESS AS NOUNER)
+          </div>
+        </Card>
       )}
-    </Card>
+      <Card
+        bgColor={CardBgColor.LightPurple}
+        borderRadius={CardBorderRadius.thirty}
+      >
+        <AuctionHeader auction={auction} />
+        <Row>
+          <Col xs={4} md={2}>
+            <div className={classes.proposalTitle}>Proposals</div>
+          </Col>
+          <Col xs={8} md={10}>
+            <div className={classes.divider} />
+          </Col>
+        </Row>
+        <ProposalCards
+          proposals={
+            showAllProposals ? auction.proposals : auction.proposals.slice(0, 6)
+          }
+        />
+        {!showAllProposals && (
+          <AllProposalsCTA
+            numProposals={auction.proposals.length}
+            auctionId={auction.id}
+          />
+        )}
+      </Card>
+    </>
   );
 };
 
