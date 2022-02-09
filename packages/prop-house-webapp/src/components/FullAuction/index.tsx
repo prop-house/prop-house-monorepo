64b4c20d22ed3920789bc5c5a4b6eb3239295a55
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-02-09 16:50:39 -0500

    Add alerts depending on Nouner status

--- packages/prop-house-webapp/src/components/FullAuction/index.tsx
+++ packages/prop-house-webapp/src/components/FullAuction/index.tsx
@@ -7,6 +7,12 @@ import { Row, Col } from 'react-bootstrap';
 import { StoredAuction } from '@nouns/prop-house-wrapper/dist/builders';
 import auctionStatus from '../../utils/auctionStatus';
 import { AuctionStatus } from '../../utils/auctionStatus';
+import { useEthers } from '@usedapp/core';
+import { useQuery } from '@apollo/client';
+import { delegatedVotesToAddress } from '../../wrappers/subgraph';
+import { useEffect, useState } from 'react';
+import Button, { ButtonColor } from '../Button';
+import useWeb3Modal from '../../hooks/useWeb3Modal';
 
 const FullAuction: React.FC<{
   auction: StoredAuction;
@@ -14,19 +20,49 @@ const FullAuction: React.FC<{
 }> = (props) => {
   const { auction, showAllProposals } = props;
 
+  const [isNouner, setIsNouner] = useState(undefined);
+  const { account } = useEthers();
+  const connect = useWeb3Modal();
+
+  const { loading, error, data } = useQuery(
+    delegatedVotesToAddress(account ? account : '')
+  );
+
+  useEffect(() => {
+    // checks before verifying nouner status
+    account &&
+      !loading &&
+      !error &&
+      setIsNouner(data.delegates[0] && data.delegates[0].delegatedVotesRaw > 0);
+  }, [loading, error, data, account]);
+
+  // alert to get nouners to connect when auctions in voting stage
+  const disconnectedCopy = (
+    <div className={classes.alertWrapper}>
+      <p>The voting period is now open. Noun owners can vote for proposals.</p>
+      <Button
+        text="Connect wallet"
+        bgColor={ButtonColor.Pink}
+        onClick={connect}
+      />
+    </div>
+  );
+
+  // alert verifying that connected wallet is a nouner
+  const connectedCopy =
+    'You are a Noun owner! Cast your vote for the proposal you believe should receive funding.';
+
   return (
     <>
-      {auctionStatus(auction) === AuctionStatus.AuctionVoting && (
-        <Card
-          bgColor={CardBgColor.White}
-          borderRadius={CardBorderRadius.twenty}
-        >
-          <div>
-            You are a Noun owner! Cast your vote for the proposal you believe
-            should receive funding. (TODO: VALIDATE ADDRESS AS NOUNER)
-          </div>
-        </Card>
-      )}
+      {auctionStatus(auction) === AuctionStatus.AuctionVoting &&
+        (isNouner === true || account === undefined) && (
+          <Card
+            bgColor={CardBgColor.White}
+            borderRadius={CardBorderRadius.twenty}
+          >
+            <div>{isNouner ? connectedCopy : disconnectedCopy}</div>
+          </Card>
+        )}
       <Card
         bgColor={CardBgColor.LightPurple}
         borderRadius={CardBorderRadius.thirty}
