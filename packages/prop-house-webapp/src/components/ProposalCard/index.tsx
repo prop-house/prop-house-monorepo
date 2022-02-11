75e21010a06e6889d465b3b4fbb79ef0888548cc
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-02-11 12:37:52 -0500

    Make proposal cards have equal size w and w/o voting btn

--- packages/prop-house-webapp/src/components/ProposalCard/index.tsx
+++ packages/prop-house-webapp/src/components/ProposalCard/index.tsx
@@ -6,7 +6,7 @@ import { StoredProposal } from '@nouns/prop-house-wrapper/dist/builders';
 import diffTime from '../../utils/diffTime';
 import detailedTime from '../../utils/detailedTime';
 import EthAddress from '../EthAddress';
-import { Row } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
 import Button, { ButtonColor } from '../Button';
 import clsx from 'clsx';
 
@@ -24,17 +24,19 @@ const ProposalCard: React.FC<{
   const { proposal, status } = props;
 
   const ctaButton = (
-    <Button
-      text={
-        status === ProposalCardStatus.CanVoteFor
-          ? 'Cast your vote'
-          : status === ProposalCardStatus.VotedFor
-          ? 'You voted for this'
-          : ''
-      }
-      bgColor={ButtonColor.WhiteYellow}
-      classNames={classes.voteBtn}
-    />
+    <Col xs={12}>
+      <Button
+        text={
+          status === ProposalCardStatus.CanVoteFor
+            ? 'Cast your vote'
+            : status === ProposalCardStatus.VotedFor
+            ? 'You voted for this'
+            : ''
+        }
+        bgColor={ButtonColor.WhiteYellow}
+        classNames={classes.voteBtn}
+      />
+    </Col>
   );
 
   return (
@@ -50,37 +52,35 @@ const ProposalCard: React.FC<{
         classes.proposalCard
       )}
     >
-      <Row className={classes.proposalCardRow}>
-        <div className={classes.author}>
-          <EthAddress>{proposal.address}</EthAddress>
-          <span>proposed</span>
+      <div className={classes.authorContainer}>
+        <EthAddress>{proposal.address}</EthAddress>
+        <span>proposed</span>
+      </div>
+      <div className={classes.title}>{proposal.title}</div>
+      <div className={classes.bottomContainer}>
+        <div
+          className={classes.timestamp}
+          title={detailedTime(proposal.createdDate)}
+        >
+          {diffTime(proposal.createdDate)}
         </div>
-        <div className={classes.title}>{proposal.title}</div>
-        <div className={classes.bottomContainer}>
-          <div
-            className={classes.timestamp}
-            title={detailedTime(proposal.createdDate)}
+        <div className={clsx(classes.readMore)}>
+          <Link
+            to={`/proposal/${proposal.id}`}
+            className={
+              status === ProposalCardStatus.CanVoteFor ||
+              status === ProposalCardStatus.VotedFor
+                ? globalClasses.fontYellow
+                : globalClasses.fontPink
+            }
           >
-            {diffTime(proposal.createdDate)}
-          </div>
-          <div className={clsx(classes.readMore)}>
-            <Link
-              to={`/proposal/${proposal.id}`}
-              className={
-                status === ProposalCardStatus.CanVoteFor ||
-                status === ProposalCardStatus.VotedFor
-                  ? globalClasses.fontYellow
-                  : globalClasses.fontPink
-              }
-            >
-              Read more →
-            </Link>
-          </div>
+            Read more →
+          </Link>
         </div>
-        {(status === ProposalCardStatus.CanVoteFor ||
-          status === ProposalCardStatus.VotedFor) &&
-          ctaButton}
-      </Row>
+      </div>
+      {(status === ProposalCardStatus.CanVoteFor ||
+        status === ProposalCardStatus.VotedFor) &&
+        ctaButton}
     </Card>
   );
 };
