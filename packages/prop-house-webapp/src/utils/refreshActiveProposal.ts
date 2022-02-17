4c2693ce02349dc1ca38d28f408d593c2a715a54
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-02-17 17:50:40 -0500

    add util to refresh active proposals

--- packages/prop-house-webapp/src/utils/refreshActiveProposal.ts
+++ packages/prop-house-webapp/src/utils/refreshActiveProposal.ts
@@ -1,11 +1,30 @@
-import { PropHouseWrapper } from "@nouns/prop-house-wrapper";
-import { StoredProposalWithVotes } from "@nouns/prop-house-wrapper/dist/builders";
-import { Dispatch } from "redux";
-import { setActiveProposal } from "../state/slices/propHouse";
-import { RootState } from "../state/store";
+import { PropHouseWrapper } from '@nouns/prop-house-wrapper';
+import { StoredProposalWithVotes } from '@nouns/prop-house-wrapper/dist/builders';
+import { Dispatch } from 'redux';
+import {
+  setActiveProposal,
+  setActiveProposals,
+} from '../state/slices/propHouse';
+import { RootState } from '../state/store';
 
-const refreshActiveProposal = (client: PropHouseWrapper, activeProposal: StoredProposalWithVotes, dispatch: Dispatch) =>  {
-    client.getProposal(activeProposal.id).then(proposal => dispatch(setActiveProposal(proposal)))
-}
+const refreshActiveProposal = (
+  client: PropHouseWrapper,
+  activeProposal: StoredProposalWithVotes,
+  dispatch: Dispatch
+) => {
+  client
+    .getProposal(activeProposal.id)
+    .then((proposal) => dispatch(setActiveProposal(proposal)));
+};
 
-export default refreshActiveProposal
+export const refreshActiveProposals = (
+  client: PropHouseWrapper,
+  auctionId: number,
+  dispatch: Dispatch
+) => {
+  client
+    .getAuctionProposals(auctionId)
+    .then((proposals) => dispatch(setActiveProposals(proposals)));
+};
+
+export default refreshActiveProposal;
