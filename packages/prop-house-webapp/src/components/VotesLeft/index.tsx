abdc6d0874479a0d0ef290cd07e1eb6ee1c4d117
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-02-25 20:01:06 -0500

    silence warnings

--- packages/prop-house-webapp/src/components/VotesLeft/index.tsx
+++ packages/prop-house-webapp/src/components/VotesLeft/index.tsx
@@ -1,7 +1,5 @@
 import { Row, Col } from 'react-bootstrap';
 import Card, { CardBgColor, CardBorderRadius } from '../Card';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheckSquare } from '@fortawesome/free-solid-svg-icons';
 import classes from './VotesLeft.module.css';
 
 const VotesLeft: React.FC<{
