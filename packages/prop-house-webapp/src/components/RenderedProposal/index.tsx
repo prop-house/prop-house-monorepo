1277f9162ec49c308a67f2ba79138bee732400f4
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-03-02 13:11:59 -0500

    Add submitted by field to proposals

--- packages/prop-house-webapp/src/components/RenderedProposal/index.tsx
+++ packages/prop-house-webapp/src/components/RenderedProposal/index.tsx
@@ -1,6 +1,6 @@
-import { StoredProposal } from "@nouns/prop-house-wrapper/dist/builders";
-import proposalFields from "../../utils/proposalFields";
-import RenderedProposalFields from "../RenderedProposalFields";
+import { StoredProposal } from '@nouns/prop-house-wrapper/dist/builders';
+import proposalFields from '../../utils/proposalFields';
+import RenderedProposalFields from '../RenderedProposalFields';
 
 export interface RenderedProposalProps {
   proposal: StoredProposal;
@@ -8,7 +8,9 @@ export interface RenderedProposalProps {
 
 const RenderedProposal: React.FC<RenderedProposalProps> = (props) => {
   const fields = proposalFields(props.proposal);
-  return <RenderedProposalFields fields={fields} />;
+  return (
+    <RenderedProposalFields fields={fields} address={props.proposal.address} />
+  );
 };
 
 export default RenderedProposal;
--- packages/prop-house-webapp/src/components/RenderedProposalFields/index.tsx
+++ packages/prop-house-webapp/src/components/RenderedProposalFields/index.tsx
@@ -1,19 +1,25 @@
 import classes from './RenderedProposalFields.module.css';
 import { Row, Col } from 'react-bootstrap';
 import { ProposalFields } from '../../utils/proposalFields';
+import EthAddress from '../EthAddress';
 
 export interface RenderedProposalProps {
   fields: ProposalFields;
+  address?: string;
 }
 
 const RenderedProposalFields: React.FC<RenderedProposalProps> = (props) => {
-  const { fields } = props;
+  const { fields, address } = props;
   return (
     <>
       <Row>
         <Col xl={12} className={classes.previewCol}>
           <h1>{fields.title}</h1>
-          <hr />
+          {address && (
+            <div className={classes.submittedBy}>
+              submitted by <EthAddress>{address}</EthAddress>
+            </div>
+          )}
           <h2>Description</h2>
           <p>{fields.what}</p>
         </Col>
