742b4a76ad6a43b844896fe59640b582338290ce
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-03-15 16:57:27 -0400

    Replace vote calls with package exported fns, remove deps

--- packages/prop-house-backend/src/vote/votes.service.ts
+++ packages/prop-house-backend/src/vote/votes.service.ts
@@ -2,16 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Vote } from './vote.entity';
-import { client } from 'src/wrappers/subgraph';
-import { delegatedVotesToAddressQuery } from 'src/wrappers/subgraph';
-import { gql } from '@apollo/client';
 import { CreateVoteDto } from './vote.types';
 import { Proposal } from 'src/proposal/proposal.entity';
 import { isDevEnv } from 'src/config/configuration';
-import { contracts } from 'prop-house-nounish-contracts';
 import { ethers } from 'ethers';
 import { DelegatedVotes, VoteType } from 'src/utils/vote';
 import config from 'src/config/configuration';
+import { getNounerVotes, getNounishVotes } from 'prop-house-nounish-contracts';
 
 @Injectable()
 export class VotesService {
@@ -48,10 +45,11 @@ export class VotesService {
     // Return 10 votes if in development mode
     if (isDevEnv()) return { votes: 10, type: VoteType.Nouner };
 
-    const nounerVotes = await this.getNounerVotes(address);
+    const nounerVotes = await getNounerVotes(address);
     if (nounerVotes > 0) return { votes: nounerVotes, type: VoteType.Nouner };
 
-    const nounishVotes = await this.getNounishVotes(address);
+    const provider = new ethers.providers.JsonRpcProvider(config().JSONRPC);
+    const nounishVotes = await getNounishVotes(address, provider);
     if (nounishVotes > 0)
       return { votes: nounishVotes, type: VoteType.Nounish };
 
@@ -75,44 +73,5 @@ export class VotesService {
     await this.store(vote);
 
     return vote;
-  }
-
-  /**
-   * Fetches number of delegated votes to address
-   * @param address address votes were delegated to
-   * @returns number of delegated votes
-   */
-  async getNounerVotes(address: string): Promise<number> {
-    const result = await client.query({
-      query: gql(delegatedVotesToAddressQuery(address)),
-    });
-
-    return result.data.delegates[0]
-      ? result.data.delegates[0].delegatedVotesRaw
-      : 0;
-  }
-
-  /**
-   * Fetches accumulative balance of owned NFTs of approved contracts from `prop-house-nounish-contracts` package
-   * @param address
-   * @returns
-   */
-  async getNounishVotes(address: string): Promise<number> {
-    const provider = new ethers.providers.JsonRpcProvider(config().JSONRPC);
-
-    let delegatedVotes = 0;
-
-    for (let i = 0; i < contracts.length; i++) {
-      const contract = new ethers.Contract(
-        contracts[i].address,
-        contracts[i].abi,
-        provider,
-      );
-
-      const balance = await contract.balanceOf(address);
-      delegatedVotes += Number(balance);
-    }
-
-    return delegatedVotes;
   }
 }
