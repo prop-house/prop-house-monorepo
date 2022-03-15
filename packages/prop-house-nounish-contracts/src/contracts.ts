026acb330fd61e82b77a4e6017c7d2798c0f7d1d
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-03-15 19:36:29 -0400

    add extension community contract

--- packages/prop-house-nounish-contracts/src/contracts.ts
+++ packages/prop-house-nounish-contracts/src/contracts.ts
@@ -1,5 +1,6 @@
 import NounPunksABI from './abi/NounPunks.json';
 import SideNounABI from './abi/SideNoun.json';
+import LostNounsABI from './abi/LostNouns.json';
 
 interface Contract {
   address: string;
@@ -7,6 +8,7 @@ interface Contract {
 }
 
 export const contracts: Contract[] = [
+  { address: '0x2605afbb22c59296c16ef5e477110357f760b20f', abi: LostNounsABI },
   {
     address: '0xE169c2ED585e62B1d32615BF2591093A629549b6',
     abi: NounPunksABI,
