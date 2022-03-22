cef39aef07f7d324a039673f49067845118fcd64
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-03-22 11:36:05 -0400

    update contracts

--- packages/prop-house-nounish-contracts/src/contracts.ts
+++ packages/prop-house-nounish-contracts/src/contracts.ts
@@ -9,15 +9,7 @@ interface Contract {
 
 export const contracts: Contract[] = [
   {
-    address: '0x2605afbb22c59296c16ef5e477110357f760b20f',
-    abi: LostNounsABI,
-  },
-  {
     address: '0xE169c2ED585e62B1d32615BF2591093A629549b6',
     abi: NounPunksABI,
-  },
-  {
-    address: '0xd9E49f550d0F605e3cCEE3167eC14ee7a9134DdB',
-    abi: SideNounABI,
   },
 ];
