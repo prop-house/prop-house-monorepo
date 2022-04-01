7c7f70c7b80628f75d3a7c500bbb9db87c65c65a
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-04-01 12:47:03 -0400

    add checks for valid address

--- packages/prop-house-webapp/src/components/pages/Community/index.tsx
+++ packages/prop-house-webapp/src/components/pages/Community/index.tsx
@@ -1,10 +1,19 @@
 import { useParams } from 'react-router-dom';
+import { ethers } from 'ethers';
+import { isActiveCommunity } from 'prop-house-nounish-contracts';
 
 const Community = () => {
   const params = useParams();
   const { contract_address } = params;
 
-  return <>address: {contract_address}</>;
+  const isValidAddress =
+    contract_address && ethers.utils.isAddress(contract_address);
+
+  if (!isValidAddress) return <>invalid address</>;
+  if (!isActiveCommunity(contract_address))
+    return <>community does not have prop house yet!</>;
+
+  return <></>;
 };
 
 export default Community;
