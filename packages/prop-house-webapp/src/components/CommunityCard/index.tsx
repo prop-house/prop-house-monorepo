0b9b2759fd3135efe61dd1921cc63d4dacd87873
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-05-06 11:25:40 -0400

    Add CommunityCard component

---
+++ packages/prop-house-webapp/src/components/CommunityCard/index.tsx
@@ -0,0 +1,25 @@
+import classes from './CommunityCard.module.css';
+import img from '../../assets/gremp-lightbulb.png';
+import { Link } from 'react-router-dom';
+
+const CommunityCard: React.FC<{
+  numProposals: number;
+}> = (props) => {
+  const { numProposals } = props;
+
+  return (
+    <div className={classes.container}>
+      <Link to="">
+        <img src={img} className={classes.imageCard} />
+      </Link>
+      <div className={classes.infoContainer}>
+        <div className={classes.title}>Nouns</div>
+        <div className={classes.proposals}>
+          <span>{numProposals}</span> props
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CommunityCard;
