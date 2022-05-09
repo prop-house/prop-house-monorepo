30e616062efd0a1af5fbc160dfc1a1e39a1f1dc1
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-05-09 11:14:48 -0400

    Feed communities into home page corousel

--- packages/prop-house-webapp/src/components/CommunityCarousel/index.tsx
+++ packages/prop-house-webapp/src/components/CommunityCarousel/index.tsx
@@ -1,19 +1,32 @@
 import CommunityCard from '../CommunityCard';
 import CarouselSection from '../CarouselSection';
+import { useEffect, useState, useRef } from 'react';
+import { Community } from '@nouns/prop-house-wrapper/dist/builders';
+import { useAppSelector } from '../../hooks';
+import { PropHouseWrapper } from '@nouns/prop-house-wrapper';
+import { useEthers } from '@usedapp/core';
 
 const CommunityCarousel = () => {
-  const cards = [
-    <CommunityCard numProposals={44} />,
-    <CommunityCard numProposals={34} />,
-    <CommunityCard numProposals={444} />,
-    <CommunityCard numProposals={14} />,
-    <CommunityCard numProposals={44} />,
-    <CommunityCard numProposals={44} />,
-    <CommunityCard numProposals={34} />,
-    <CommunityCard numProposals={444} />,
-    <CommunityCard numProposals={14} />,
-    <CommunityCard numProposals={44} />,
-  ];
+  const [communities, setCommunities] = useState<Community[]>([]);
+
+  const { library } = useEthers();
+  const host = useAppSelector((state) => state.configuration.backendHost);
+  const client = useRef(new PropHouseWrapper(host));
+
+  useEffect(() => {
+    client.current = new PropHouseWrapper(host, library?.getSigner());
+  }, [library, host]);
+
+  // fetch communities
+  useEffect(() => {
+    const getCommunities = async () => {
+      const communities = await client.current.getCommunities();
+      setCommunities(communities);
+    };
+    getCommunities();
+  }, []);
+
+  const cards = communities.map((c) => <CommunityCard community={c} />);
 
   return (
     <CarouselSection
