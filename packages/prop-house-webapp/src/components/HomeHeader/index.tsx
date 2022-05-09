df06f22478eb15a5af775b3f8092febf9abc0541
Pixelz <pixelzpls@gmail.com>
2022-05-08 21:32:52 -0700

    Cleaning up homepage

--- packages/prop-house-webapp/src/components/HomeHeader/index.tsx
+++ packages/prop-house-webapp/src/components/HomeHeader/index.tsx
@@ -1,10 +1,10 @@
-import { Col, Row, Image } from 'react-bootstrap';
-import Button, { ButtonColor } from '../Button';
-import classes from './HomeHeader.module.css';
-import { Link } from 'react-router-dom';
-import grempBulb from '../../assets/gremp-lightbulb.png';
-import { useAppSelector } from '../../hooks';
-import defaultBrowseToAuctionId from '../../utils/defaultBrowseToAuctionId';
+import { Col, Row, Image } from "react-bootstrap";
+import Button, { ButtonColor } from "../Button";
+import classes from "./HomeHeader.module.css";
+import { Link } from "react-router-dom";
+import grempBulb from "../../assets/gremp-lightbulb.png";
+import { useAppSelector } from "../../hooks";
+import defaultBrowseToAuctionId from "../../utils/defaultBrowseToAuctionId";
 
 const HomeHeader = () => {
   const browseToAuctionId = useAppSelector((state) =>
@@ -12,11 +12,11 @@ const HomeHeader = () => {
   );
   return (
     <Row className={classes.wrapper}>
-      <Col xl={6} className={classes.leftCol}>
+      <Col lg={6} className={classes.leftCol}>
         <div className={classes.poweredByNouns}>
-          Powered by{' '}
+          Powered by{" "}
           <a href="https://nouns.wtf" target="_blank" rel="noreferrer">
-            Nouns
+            NounsDAO
           </a>
         </div>
         <h1>Weekly funding for your ideas</h1>
@@ -26,19 +26,19 @@ const HomeHeader = () => {
           anywhere.
         </p>
         <div className={classes.btnsContainer}>
-          <Col xs={6} md="auto">
-            <Link to="/learn">
-              <Button text="Learn more" bgColor={ButtonColor.Pink} />
-            </Link>
-          </Col>
-          <Col xs={6} md="auto">
-            <Link to={`/auction/${browseToAuctionId}`}>
-              <Button text="Browse rounds" bgColor={ButtonColor.White} />
-            </Link>
-          </Col>
+          <Link to="/learn">
+            <Button text="Learn more" bgColor={ButtonColor.Pink} />
+          </Link>
+          <Link to={`/auction/${browseToAuctionId}`}>
+            <Button text="Browse rounds" bgColor={ButtonColor.White} />
+          </Link>
         </div>
       </Col>
-      <Col xl={{ span: 4, offset: 2 }} className={classes.rightCol}>
+      <Col
+        lg={{ span: 5, offset: 1 }}
+        xl={{ span: 4, offset: 2 }}
+        className={classes.rightCol}
+      >
         <Image src={grempBulb} fluid />
       </Col>
     </Row>
