a3ea574b6bc8c61d62a5e920f6f35d11013c4e74
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-05-10 15:21:04 -0400

    remove nounish-contracts package

--- packages/prop-house-nounish-contracts/src/communities/utils.ts
+++
@@ -1,4 +0,0 @@
-import contracts from './contracts';
-
-export const isActiveCommunity = (address: string) =>
-  contracts.some((c) => c.address === address);
--- packages/prop-house-nounish-contracts/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './utils';
-export * from './communities/utils';
-export { contracts } from './contracts';
