67c5d479c218f8ea7e987999f30650653400731f
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-05-11 18:05:54 -0400

    Add tooltip for copying address and link

--- packages/prop-house-webapp/src/components/ProfileHeader/index.tsx
+++ packages/prop-house-webapp/src/components/ProfileHeader/index.tsx
@@ -2,13 +2,18 @@ import { Row, Col } from 'react-bootstrap';
 import classes from './ProfileHeader.module.css';
 import trimEthAddress from '../../utils/trimEthAddress';
 import { Community } from '@nouns/prop-house-wrapper/dist/builders';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { setActiveCommunity } from '../../state/slices/propHouse';
 import { useAppDispatch } from '../../hooks';
 import CommunityProfImg from '../CommunityProfImg';
+import { IoCopy } from 'react-icons/io5';
+import Tooltip from '../Tooltip';
 
 const ProfileHeader: React.FC<{ community: Community }> = (props) => {
   const { community } = props;
+
+  const [addressTooltipCopy, setAddressTooltipCopy] = useState('copy address');
+  const [linkTooltipCopy, setlinkTooltipCopy] = useState('copy link');
 
   const dispatch = useAppDispatch();
   // clean up active community on dismount
@@ -27,9 +32,22 @@ const ProfileHeader: React.FC<{ community: Community }> = (props) => {
         <Col className={classes.communityInfoCol}>
           <Col className={classes.titleRow}>
             <div className={classes.title}>{community.name}</div>
-            <div className={classes.contractAddressPill}>
-              {trimEthAddress(community.contractAddress)}
-            </div>
+            <Tooltip
+              content={
+                <div
+                  className={classes.contractAddressPill}
+                  onMouseEnter={() => setAddressTooltipCopy('copy address')}
+                  onClick={() => {
+                    console.log('het');
+                    setAddressTooltipCopy('copied!');
+                    navigator.clipboard.writeText(community.contractAddress);
+                  }}
+                >
+                  {trimEthAddress(community.contractAddress)}
+                </div>
+              }
+              tooltipContent={addressTooltipCopy}
+            />
           </Col>
           <Col className={classes.subInfoRow}>
             <div>
@@ -37,6 +55,22 @@ const ProfileHeader: React.FC<{ community: Community }> = (props) => {
             </div>
             <div className={classes.spacer}>·</div>
             <div>prop.house/{trimEthAddress(community.contractAddress)}</div>
+            <Tooltip
+              content={
+                <IoCopy
+                  size={'1rem'}
+                  className={classes.copyIcon}
+                  onMouseEnter={() => setlinkTooltipCopy('copy link')}
+                  onClick={() => {
+                    setlinkTooltipCopy('copied!');
+                    navigator.clipboard.writeText(
+                      `prop.house/${community.contractAddress}`
+                    );
+                  }}
+                />
+              }
+              tooltipContent={linkTooltipCopy}
+            />
           </Col>
           <Col className={classes.propHouseDataRow}>
             <div className={classes.item}>
