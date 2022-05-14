c0fc171af168fc8d683b56d4cbb4eb7b23d443a1
Pixelz <pixelzpls@gmail.com>
2022-05-14 13:35:52 -0700

    Formatting & copy

--- packages/prop-house-webapp/src/components/pages/Learn/index.tsx
+++ packages/prop-house-webapp/src/components/pages/Learn/index.tsx
@@ -1,15 +1,15 @@
-import classes from './Learn.module.css';
-import { Row, Col, Image } from 'react-bootstrap';
-import Card, { CardBgColor, CardBorderRadius } from '../../Card';
-import Button, { ButtonColor } from '../../Button';
-import outletsImg from '../../../assets/learn page/outlets.png';
-import auctionImg from '../../../assets/learn page/auction.png';
-import auctionFullImg from '../../../assets/learn page/auction_full.png';
-import communityImg from '../../../assets/learn page/community.png';
-import clsx from 'clsx';
-import { useAppSelector } from '../../../hooks';
-import defaultBrowseToAuctionId from '../../../utils/defaultBrowseToAuctionId';
-import { useNavigate } from 'react-router-dom';
+import classes from "./Learn.module.css";
+import { Row, Col, Image } from "react-bootstrap";
+import Card, { CardBgColor, CardBorderRadius } from "../../Card";
+import Button, { ButtonColor } from "../../Button";
+import outletsImg from "../../../assets/learn page/outlets.png";
+import auctionImg from "../../../assets/learn page/auction.png";
+import auctionFullImg from "../../../assets/learn page/auction_full.png";
+import communityImg from "../../../assets/learn page/community.png";
+import clsx from "clsx";
+import { useAppSelector } from "../../../hooks";
+import defaultBrowseToAuctionId from "../../../utils/defaultBrowseToAuctionId";
+import { useNavigate } from "react-router-dom";
 
 const Learn = () => {
   const browseToAuctionId = useAppSelector((state) =>
@@ -19,16 +19,16 @@ const Learn = () => {
 
   return (
     <>
-      <div className={clsx('break-out', classes.breakOutMobile)}>
-        <Row className={clsx('g-0', classes.row, 'justify-content-start')}>
+      <div className={clsx("break-out", classes.breakOutMobile)}>
+        <Row className={clsx("g-0", classes.row, "justify-content-start")}>
           <Col
             xl={{ span: 5 }}
-            xs={{ order: 'first' }}
+            xs={{ order: "first" }}
             className={classes.leftCol}
           >
             <Image src={outletsImg} fluid className={classes.img} />
           </Col>
-          <Col xl={{ span: 5, offset: 1 }} xs={{ order: 'last' }}>
+          <Col xl={{ span: 5, offset: 1 }} xs={{ order: "last" }}>
             <h2>Plug into the Nouns treasury</h2>
             <p>
               The Nouns DAO treasury has amassed $50M+ and counting since
@@ -44,18 +44,23 @@ const Learn = () => {
                 navigate(`/auction/${browseToAuctionId}`);
               }}
             />
-            <a href="https://nouns.wtf" target="_blank" rel="noreferrer">
+            <a
+              href="https://nouns.wtf"
+              target="_blank"
+              rel="noreferrer"
+              style={{ marginLeft: "1rem" }}
+            >
               <Button text="About Nouns" bgColor={ButtonColor.White} />
             </a>
           </Col>
         </Row>
       </div>
 
-      <div className={clsx('break-out', classes.breakOutMobile)}>
-        <Row className={clsx('g-0', classes.row)}>
+      <div className={clsx("break-out", classes.breakOutMobile)}>
+        <Row className={clsx("g-0", classes.row)}>
           <Col
-            xl={{ span: 4, offset: 1, order: 'first' }}
-            xs={{ order: 'last' }}
+            xl={{ span: 4, offset: 1, order: "first" }}
+            xs={{ order: "last" }}
           >
             <Col xs={12}>
               <h2>Bid with your ideas in Funding Rounds</h2>
@@ -74,7 +79,7 @@ const Learn = () => {
           </Col>
           <Col
             xl={{ span: 6, offset: 1 }}
-            xs={{ order: 'first' }}
+            xs={{ order: "first" }}
             className={classes.rightCol}
           >
             <Image
@@ -91,12 +96,12 @@ const Learn = () => {
         </Row>
       </div>
 
-      <div className={clsx('break-out', classes.breakOutMobile)}>
-        <Row className={clsx('g-0', classes.row, 'justify-content-start')}>
-          <Col xl={5} xs={{ order: 'last' }} className={classes.leftCol}>
+      <div className={clsx("break-out", classes.breakOutMobile)}>
+        <Row className={clsx("g-0", classes.row, "justify-content-start")}>
+          <Col xl={5} xs={{ order: "last" }} className={classes.leftCol}>
             <Image src={communityImg} fluid className={classes.img} />
           </Col>
-          <Col xl={{ span: 5, offset: 1 }} xs={{ order: 'last' }}>
+          <Col xl={{ span: 5, offset: 1 }} xs={{ order: "last" }}>
             <h2>Become part of the Nouns community</h2>
             <p>
               Nouns is a builder-first community. As a prop builder, you will
@@ -105,16 +110,21 @@ const Learn = () => {
               Nouns community.
             </p>
 
-            <a href="https://discord.gg/nouns" target="_blank" rel="noreferrer">
+            <a
+              href="https://discord.gg/nouns"
+              target="_blank"
+              rel="noreferrer"
+              style={{ marginRight: "1rem" }}
+            >
               <Button
-                text="Join discord"
-                bgColor={ButtonColor.White}
+                text="Go to Discord"
+                bgColor={ButtonColor.Purple}
                 classNames={classes.firstBtn}
               />
             </a>
             <a href="https://nouns.wtf/docs" target="_blank" rel="noreferrer">
               <Button
-                text="Visit docs"
+                text="Learn more"
                 bgColor={ButtonColor.White}
                 classNames={classes.firstBtn}
               />
