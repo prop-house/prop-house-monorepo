d5a230f3c61f30a5966060c466e14d5b9ac3cb25
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-05-16 13:05:15 -0400

    Update Lean page copy

--- packages/prop-house-webapp/src/components/pages/Learn/index.tsx
+++ packages/prop-house-webapp/src/components/pages/Learn/index.tsx
@@ -29,29 +29,21 @@ const Learn = () => {
             <Image src={outletsImg} fluid className={classes.img} />
           </Col>
           <Col xl={{ span: 5, offset: 1 }} xs={{ order: 'last' }}>
-            <h2>Plug into the Nouns treasury</h2>
+            <h2>Supercharge your community</h2>
             <p>
-              The Nouns DAO treasury has amassed $50M+ and counting since
-              inception. Prop House aims to make it easy for anyone with an
-              Ethereum address to plug in and use the treasury to build out
-              their ideas.
+              NFT communities have eager members who want to build for their
+              communities. Prop House makes it easy for communities to quickly
+              and fairly deploy their capital in order to capture the energy of
+              their builders.
             </p>
             <Button
-              text="Browse rounds"
+              text="Explore houses"
               bgColor={ButtonColor.Pink}
               classNames={classes.firstBtn}
               onClick={() => {
-                navigate(`/auction/${browseToAuctionId}`);
+                navigate(`/explore`);
               }}
             />
-            <a
-              href="https://nouns.wtf"
-              target="_blank"
-              rel="noreferrer"
-              style={{ marginLeft: '1rem' }}
-            >
-              <Button text="About Nouns" bgColor={ButtonColor.White} />
-            </a>
           </Col>
         </Row>
       </div>
@@ -63,13 +55,14 @@ const Learn = () => {
             xs={{ order: 'last' }}
           >
             <Col xs={12}>
-              <h2>Bid with your ideas in Funding Rounds</h2>
+              <h2>Capture ideas with your own Prop House</h2>
             </Col>
             <Col xs={12}>
               <p>
-                Funding rounds are held regularly. They are auctions where the
-                thing being auctioned is ETH and the bids being placed are
-                proposals. Anyone is free to propose anything.
+                Communities propose and vote on ideas via Funding Rounds held in
+                their Prop House. Funding rounds are auctions where the thing
+                being auctioned is ETH and the bids being placed are proposals.
+                Anyone is free to propose anything.
                 <br />
                 <br />
                 At the end of each auction, members of the Nouns ecosystem vote
@@ -102,33 +95,15 @@ const Learn = () => {
             <Image src={communityImg} fluid className={classes.img} />
           </Col>
           <Col xl={{ span: 5, offset: 1 }} xs={{ order: 'last' }}>
-            <h2>Become part of the Nouns community</h2>
+            <h2>
+              Bring your community together by <i>building</i> together
+            </h2>
             <p>
-              Nouns is a builder-first community. As a prop builder, you will
-              become part of a special group of people building the future of
-              open-source IP and get access to resources and support from the
-              Nouns community.
+              NFTs have enabled a new class of community. One that not only
+              comes together because of shared ideas but also ones that are able
+              to transact, transform and create value. Prop House is the
+              permissionless ETH power source that communities build around.
             </p>
-
-            <a
-              href="https://discord.gg/nouns"
-              target="_blank"
-              rel="noreferrer"
-              style={{ marginRight: '1rem' }}
-            >
-              <Button
-                text="Go to Discord"
-                bgColor={ButtonColor.Purple}
-                classNames={classes.firstBtn}
-              />
-            </a>
-            <a href="https://nouns.wtf/docs" target="_blank" rel="noreferrer">
-              <Button
-                text="Learn more"
-                bgColor={ButtonColor.White}
-                classNames={classes.firstBtn}
-              />
-            </a>
           </Col>
         </Row>
       </div>
