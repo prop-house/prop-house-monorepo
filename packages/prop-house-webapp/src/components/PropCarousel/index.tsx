8c4999f208972a4c344727a88f8cf8394e5163da
christianturner <christianturner@hey.com>
2022-06-09 16:04:08 -0400

    loading for houses

--- packages/prop-house-webapp/src/components/PropCarousel/index.tsx
+++ packages/prop-house-webapp/src/components/PropCarousel/index.tsx
@@ -1,17 +1,19 @@
-import classes from './PropCarousel.module.css';
-import CarouselSection from '../CarouselSection';
-import { useAppSelector } from '../../hooks';
-import ProposalCard from '../ProposalCard';
-import { PropHouseWrapper } from '@nouns/prop-house-wrapper';
-import { useRef, useEffect, useState } from 'react';
-import { useEthers } from '@usedapp/core';
-import { useDispatch } from 'react-redux';
-import { StoredProposalWithVotes } from '@nouns/prop-house-wrapper/dist/builders';
+import classes from "./PropCarousel.module.css";
+import CarouselSection from "../CarouselSection";
+import { useAppSelector } from "../../hooks";
+import ProposalCard from "../ProposalCard";
+import { PropHouseWrapper } from "@nouns/prop-house-wrapper";
+import { useRef, useEffect, useState } from "react";
+import { useEthers } from "@usedapp/core";
+import { useDispatch } from "react-redux";
+import { StoredProposalWithVotes } from "@nouns/prop-house-wrapper/dist/builders";
+import LoadingIndicator from "../LoadingIndicator";
 
 const PropCarousel = () => {
   const { library } = useEthers();
   const dispatch = useDispatch();
   const [proposals, setProposals] = useState<StoredProposalWithVotes[]>();
+  const [isLoading, setIsLoading] = useState(false);
   const host = useAppSelector((state) => state.configuration.backendHost);
   const client = useRef(new PropHouseWrapper(host));
 
@@ -21,9 +23,11 @@ const PropCarousel = () => {
 
   // fetch proposals
   useEffect(() => {
+    setIsLoading(true);
     const fetchAuctionProposals = async () => {
       const proposals = await client.current.getAllProposals();
       setProposals(proposals);
+      setIsLoading(false);
     };
     fetchAuctionProposals();
   }, [dispatch]);
@@ -38,7 +42,9 @@ const PropCarousel = () => {
       );
     });
 
-  return (
+  return isLoading ? (
+    <LoadingIndicator />
+  ) : (
     <CarouselSection
       contextTitle="Browse proposals"
       mainTitle="Recent proposals "
