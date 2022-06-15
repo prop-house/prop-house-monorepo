f38b9a3dc520eaf47f277a8e96d414925a37f0fe
devcarrot <devcarrot@protonmail.com>
2022-06-15 01:13:37 -0600

    Add body parser and increase max payload size

--- packages/prop-house-backend/src/main.ts
+++ packages/prop-house-backend/src/main.ts
@@ -1,9 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});
+  app.use(bodyParser.json({limit: '50mb'}));
+  app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
   app.useGlobalPipes(new ValidationPipe({
     transform: true
   }))
