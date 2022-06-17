83f13b0699368b256040af3517d24811a00db71c
christianturner <christianturner@hey.com>
2022-06-17 16:52:24 -0400

    input data object with 1 UI component

--- packages/prop-house-webapp/src/components/ProposalEditor/index.tsx
+++ packages/prop-house-webapp/src/components/ProposalEditor/index.tsx
@@ -1,43 +1,56 @@
 import classes from "./ProposalEditor.module.css";
 import { Row, Col, Form } from "react-bootstrap";
-import clsx from "clsx";
 import { useAppSelector } from "../../hooks";
 import { ProposalFields } from "../../utils/proposalFields";
 import { useState } from "react";
+import clsx from "clsx";
 
 const ProposalEditor: React.FC<{
   onDataChange: (data: Partial<ProposalFields>) => void;
 }> = (props) => {
   const data = useAppSelector((state) => state.editor.proposal);
   const { onDataChange } = props;
-  const [titleCount, setTitleCount] = useState(0);
-  const [titleError, setTitleError] = useState(false);
-  const [tldrCount, setTldrCount] = useState(0);
-  const [tldrError, setTldrError] = useState(false);
-  const [whatCount, setWhatCount] = useState(0);
-  const [whatError, setWhatError] = useState(false);
+  const [blurred, setBlurred] = useState(false);
 
-  const validateTitle = (input: any) => {
-    setTitleError(false);
+  const validateInput = (min: number, count: number) =>
+    0 < count && count < min;
 
-    if ((0 < input && input < 5) || input > 80) {
-      setTitleError(true);
-    }
-  };
-  const validateTldr = (input: any) => {
-    setTldrError(false);
-
-    if ((0 < input && input < 10) || input > 120) {
-      setTldrError(true);
-    }
-  };
-  const validateWhat = (input: any) => {
-    setWhatError(false);
-
-    if (0 < input && input < 120) {
-      setWhatError(true);
-    }
-  };
+  const formData = [
+    {
+      title: "Title",
+      type: "input",
+      fieldValue: data.title,
+      fieldName: "title",
+      placeholder: "Give your proposal a name",
+      value: "",
+      minCount: 5,
+      maxCount: 100,
+      error: "Title must be 5 characters minimum",
+    },
+    {
+      title: "tl;dr",
+      type: "input",
+      fieldValue: data.tldr,
+      fieldName: "tldr",
+      placeholder:
+        "In the simplest language possible, explain your proposal in one sentence",
+      value: "",
+      minCount: 10,
+      maxCount: 120,
+      error: "TLDR must be between 10 & 120 characters",
+    },
+    {
+      title: "Description",
+      type: "textarea",
+      fieldValue: data.what,
+      fieldName: "what",
+      placeholder:
+        "Project details: what are you building?\nRoadmap: when do you expect to complete it by?\nTeam: who is building this?\nLinks: share relevant links to the team and project",
+      value: "",
+      minCount: 50,
+      error: "Description must be 50 characters minimum",
+    },
+  ];
 
   return (
     <>
@@ -45,87 +58,46 @@ const ProposalEditor: React.FC<{
         <Col xl={12}>
           <Form>
             <Form.Group className={classes.inputGroup}>
-              <div className={classes.inputSection}>
-                <div className={classes.inputInfo}>
-                  <Form.Label className={classes.inputLabel}>Title</Form.Label>
-                  <Form.Label className={classes.inputChars}>
-                    {`${titleCount}/80`}
-                  </Form.Label>
-                </div>
-                <Form.Control
-                  as="input"
-                  maxLength={80}
-                  placeholder="Give your proposal a name..."
-                  className={classes.input}
-                  onChange={(e) => {
-                    setTitleCount(e.target.value.length);
-                    onDataChange({ title: e.target.value });
-                  }}
-                  value={data && data.title}
-                  isInvalid={titleError}
-                  onBlur={() => validateTitle(titleCount)}
-                />
-                {titleError && (
-                  <p className={classes.inputError}>
-                    Title must be 5 characters minimum
-                  </p>
-                )}
-              </div>
+              {formData.map((input) => {
+                return (
+                  <div className={classes.inputSection}>
+                    <div className={classes.inputInfo}>
+                      <Form.Label className={classes.inputLabel}>
+                        {input.title}
+                      </Form.Label>
+                      <Form.Label className={classes.inputChars}>
+                        {input.maxCount
+                          ? `${input.fieldValue.length}/${input.maxCount}`
+                          : input.fieldValue.length}
+                      </Form.Label>
+                    </div>
 
-              <div className={classes.inputSection}>
-                <div className={classes.inputInfo}>
-                  <Form.Label className={classes.inputLabel}>tl;dr</Form.Label>
-                  <Form.Label className={classes.inputChars}>
-                    {`${tldrCount}/120`}
-                  </Form.Label>
-                </div>
-                <Form.Control
-                  as="input"
-                  maxLength={120}
-                  placeholder="In the simplest language possible, explain your proposal in one sentence."
-                  className={classes.input}
-                  onChange={(e) => {
-                    setTldrCount(e.target.value.length);
-                    onDataChange({ tldr: e.target.value });
-                  }}
-                  value={data && data.tldr}
-                  isInvalid={tldrError}
-                  onBlur={() => validateTldr(tldrCount)}
-                />
-                {tldrError && (
-                  <p className={classes.inputError}>
-                    TLDR must be 10 characters minimum
-                  </p>
-                )}
-              </div>
+                    <Form.Control
+                      as={input.type as any}
+                      maxLength={input.maxCount && input.maxCount}
+                      placeholder={input.placeholder}
+                      className={clsx(
+                        classes.input,
+                        input.fieldName === "what" && classes.descriptionInput
+                      )}
+                      onChange={(e) => {
+                        setBlurred(false);
+                        onDataChange({ [input.fieldName]: e.target.value });
+                      }}
+                      value={data && input.fieldValue}
+                      onBlur={() => {
+                        setBlurred(true);
+                      }}
+                    />
 
-              <div className={classes.inputSection}>
-                <div className={classes.inputInfo}>
-                  <Form.Label className={classes.inputLabel}>
-                    Proposal description
-                  </Form.Label>
-                  <Form.Label className={classes.inputChars}>
-                    {`${whatCount}/500`}
-                  </Form.Label>
-                </div>
-                <Form.Control
-                  as="textarea"
-                  placeholder="Project details: what are you building?&#10;Roadmap: when do you expect to complete it by?&#10;Team: who is building this?&#10;Links: share relevant links to the team and project"
-                  className={clsx(classes.input, classes.descriptionInput)}
-                  onChange={(e) => {
-                    setWhatCount(e.target.value.length);
-                    onDataChange({ what: e.target.value });
-                  }}
-                  value={data && data.what}
-                  isInvalid={whatError}
-                  onBlur={() => validateWhat(whatCount)}
-                />
-                {whatError && (
-                  <p className={classes.inputError}>
-                    Description must be 120 characters minimum
-                  </p>
-                )}
-              </div>
+                    {blurred &&
+                      validateInput(
+                        input.minCount,
+                        input.fieldValue.length
+                      ) && <p className={classes.inputError}>{input.error}</p>}
+                  </div>
+                );
+              })}
             </Form.Group>
           </Form>
         </Col>
