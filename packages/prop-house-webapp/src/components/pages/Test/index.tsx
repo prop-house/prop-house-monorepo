333a187b839860b498a76ed0e3cb2f98f4db9e3c
christianturner <christianturner@hey.com>
2022-06-22 21:54:53 -0400

    WIP

--- packages/prop-house-webapp/src/components/pages/Test/index.tsx
+++ packages/prop-house-webapp/src/components/pages/Test/index.tsx
@@ -1,18 +1,14 @@
-import { useReverseENSLookUp } from "../../../utils/ensLookup";
-import trimEthAddress from "../../../utils/trimEthAddress";
+import Davatar from "@davatar/react";
+import { useEthers } from "@usedapp/core";
 
 const Test: React.FC<{}> = () => {
   const address = "0xD19BF5F0B785c6f1F6228C72A8A31C9f383a49c4";
-
-  // const ens = useReverseENSLookUp(address);
-  console.log("ens", useReverseENSLookUp(address));
-
-  // ens ? console.log("ens", ens) : console.log("trim:", trimEthAddress(address));
+  const { library: provider } = useEthers();
 
   return (
     <>
       <h1>hello</h1>
-      {/* <Davatar size={24} address={address} />; */}
+      {/* {address && <Davatar size={24} address={address} provider={provider} />} */}
     </>
   );
 };
