eb20c163eaededff454fe9599bae72a395384c3e
christianturner <christianturner@hey.com>
2022-06-26 13:51:57 -0400

    short address component

--- packages/prop-house-webapp/src/components/pages/Test/index.tsx
+++ packages/prop-house-webapp/src/components/pages/Test/index.tsx
@@ -1,14 +1,15 @@
-import Davatar from "@davatar/react";
-import { useEthers } from "@usedapp/core";
+import ShortAddress from "../../ShortAddress";
 
 const Test: React.FC<{}> = () => {
-  const address = "0xD19BF5F0B785c6f1F6228C72A8A31C9f383a49c4";
-  const { library: provider } = useEthers();
+  const address = "0xd19bf5f0b785c6f1f6228c72a8a31c9f383a49c4";
 
   return (
     <>
       <h1>hello</h1>
-      {/* {address && <Davatar size={24} address={address} provider={provider} />} */}
+
+      {/* <ShortAddress address={address} avatar={true} /> */}
+
+      <h1>end</h1>
     </>
   );
 };
