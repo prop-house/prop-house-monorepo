a814e81e0ea54bc812b7fad25828aef0dc0b7ab5
christianturner <christianturner@hey.com>
2022-07-01 13:53:30 -0400

    rm extra Suspense

--- packages/prop-house-webapp/src/components/pages/Home/index.tsx
+++ packages/prop-house-webapp/src/components/pages/Home/index.tsx
@@ -2,18 +2,14 @@ import HomeHeader from "../../HomeHeader";
 import CommunityCarousel from "../../CommunityCarousel";
 import PropCarousel from "../../PropCarousel";
 import ContactUsCTA from "../../ContactUsCTA";
-import { Suspense } from "react";
-import LoadingIndicator from "../../LoadingIndicator";
 
 const Home = () => {
   return (
     <>
-      <Suspense fallback={<LoadingIndicator />}>
-        <HomeHeader />
-        <CommunityCarousel />
-        <PropCarousel />
-        <ContactUsCTA />
-      </Suspense>
+      <HomeHeader />
+      <CommunityCarousel />
+      <PropCarousel />
+      <ContactUsCTA />
     </>
   );
 };
