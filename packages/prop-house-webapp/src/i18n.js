e78b6807f800792a24f2e0bab419004e510aad06
christianturner <christianturner@hey.com>
2022-07-01 15:23:11 -0400

    local storage setup

--- packages/prop-house-webapp/src/i18n.js
+++ packages/prop-house-webapp/src/i18n.js
@@ -2,14 +2,18 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import HttpBackend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
+import ChainedBackend from "i18next-chained-backend";
+import LocalStorageBackend from "i18next-localstorage-backend";
 
 i18n
   .use(initReactI18next)
-  .use(HttpBackend)
   .use(LanguageDetector)
+  .use(ChainedBackend)
   .init({
-    backend: { loadPath: "/locales/{{lng}}.json" },
-    lng: "en",
     fallbackLng: "en",
+    backend: {
+      backends: [HttpBackend, LocalStorageBackend],
+      backendOptions: [{ loadPath: "/locales/{{lng}}.json" }],
+    },
     interpolation: { escapeValue: false },
   });
