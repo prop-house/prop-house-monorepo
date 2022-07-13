a0266b764650526287cc0bacd7c3a4989f209aa0
christianturner <christianturner@hey.com>
2022-07-13 06:44:41 -0400

    this commit will clarifying what each toggle is sorting by and clean up the top-level var declarations

--- packages/prop-house-webapp/src/components/SortDropdown/index.tsx
+++ packages/prop-house-webapp/src/components/SortDropdown/index.tsx
@@ -1,59 +1,55 @@
-import classes from "./SortDropdown.module.css";
-import { StoredAuction } from "@nouns/prop-house-wrapper/dist/builders";
-import { auctionStatus, AuctionStatus } from "../../utils/auctionStatus";
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { sortProposals } from "../../state/slices/propHouse";
-import { SortType } from "../../utils/sortingProposals";
-import { IoArrowDown, IoArrowUp } from "react-icons/io5";
-import clsx from "clsx";
-import { useTranslation } from "react-i18next";
+import classes from './SortDropdown.module.css';
+import { StoredAuction } from '@nouns/prop-house-wrapper/dist/builders';
+import { auctionStatus, AuctionStatus } from '../../utils/auctionStatus';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { sortProposals } from '../../state/slices/propHouse';
+import { SortType } from '../../utils/sortingProposals';
+import { IoArrowDown, IoArrowUp } from 'react-icons/io5';
+import clsx from 'clsx';
+import { useTranslation } from 'react-i18next';
 
-const SortDropdown: React.FC<{ auction: StoredAuction }> = (props) => {
+const SortDropdown: React.FC<{ auction: StoredAuction }> = props => {
   const { t } = useTranslation();
   const { auction } = props;
 
   const auctionEnded = auctionStatus(auction) === AuctionStatus.AuctionEnded;
-  const isVotingWindow =
-    auctionStatus(auction) === AuctionStatus.AuctionVoting || auctionEnded;
+  const auctionVoting = auctionStatus(auction) === AuctionStatus.AuctionVoting;
+  const allowSortByVotes = auctionVoting || auctionEnded;
 
   const [datesSorted, setDatesSorted] = useState(auctionEnded ? false : true);
   const [votesSorted, setVotesSorted] = useState(auctionEnded ? true : false);
-  const [dateAscending, setDateAscending] = useState(
-    auctionEnded ? false : true
-  );
-  const [votesAscending, setVotesAscending] = useState(
-    auctionEnded ? true : false
-  );
+  const [dateAscending, setDateAscending] = useState(auctionEnded ? false : true);
+  const [votesAscending, setVotesAscending] = useState(auctionEnded ? true : false);
 
   const dispatch = useDispatch();
 
-  const sortDates = () => {
+  const sortByDates = () => {
     setDatesSorted(true);
     setVotesSorted(false);
 
-    setDateAscending((prev) => {
+    setDateAscending(prev => {
       dispatch(
         sortProposals({
           sortType: SortType.CreatedAt,
           ascending: dateAscending,
-        })
+        }),
       );
 
       return !prev;
     });
   };
 
-  const sortVotes = () => {
+  const sortByVotes = () => {
     setDatesSorted(false);
     setVotesSorted(true);
 
-    setVotesAscending((prev) => {
+    setVotesAscending(prev => {
       dispatch(
         sortProposals({
           sortType: SortType.Score,
           ascending: votesAscending,
-        })
+        }),
       );
 
       return !prev;
@@ -63,30 +59,22 @@ const SortDropdown: React.FC<{ auction: StoredAuction }> = (props) => {
   return (
     <>
       <div className={classes.sortContainer}>
-        {isVotingWindow && (
+        {allowSortByVotes && (
           <div
-            onClick={sortVotes}
+            onClick={sortByVotes}
             className={clsx(classes.sortItem, votesSorted && classes.active)}
           >
-            <div>{t("votes")}</div>
-            {votesAscending ? (
-              <IoArrowDown size={"1.5rem"} />
-            ) : (
-              <IoArrowUp size={"1.5rem"} />
-            )}
+            <div>{t('votes')}</div>
+            {votesAscending ? <IoArrowDown size={'1.5rem'} /> : <IoArrowUp size={'1.5rem'} />}
           </div>
         )}
 
         <div
-          onClick={sortDates}
+          onClick={sortByDates}
           className={clsx(classes.sortItem, datesSorted && classes.active)}
         >
-          <div>{t("created")}</div>
-          {dateAscending ? (
-            <IoArrowUp size={"1.5rem"} />
-          ) : (
-            <IoArrowDown size={"1.5rem"} />
-          )}
+          <div>{t('created')}</div>
+          {dateAscending ? <IoArrowUp size={'1.5rem'} /> : <IoArrowDown size={'1.5rem'} />}
         </div>
       </div>
     </>
