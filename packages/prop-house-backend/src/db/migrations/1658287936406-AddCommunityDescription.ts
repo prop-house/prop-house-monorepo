9aeb8bea1e4b437cd4c3530948b3f90ce9f3757c
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-07-20 12:57:49 -0400

    make description nullable

--- packages/prop-house-backend/src/db/migrations/1658287936406-AddCommunityDescription.ts
+++ packages/prop-house-backend/src/db/migrations/1658287936406-AddCommunityDescription.ts
@@ -4,7 +4,7 @@ export class AddCommunityDescription1658287936406 implements MigrationInterface 
     name = 'AddCommunityDescription1658287936406'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "community" ADD "description" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "community" ADD "description" character varying`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
