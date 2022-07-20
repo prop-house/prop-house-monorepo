4392b646c95d01038d8e4f22881ddbd1a97dd431
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-07-19 20:41:15 -0400

    update ProfileHeader design with communtiy descript

--- packages/prop-house-webapp/src/components/ProfileHeader/index.tsx
+++ packages/prop-house-webapp/src/components/ProfileHeader/index.tsx
@@ -4,10 +4,8 @@ import trimEthAddress from '../../utils/trimEthAddress';
 import { Community } from '@nouns/prop-house-wrapper/dist/builders';
 import { useState } from 'react';
 import CommunityProfImg from '../CommunityProfImg';
-import { IoCopy } from 'react-icons/io5';
 import clsx from 'clsx';
 import Tooltip from '../Tooltip';
-import { tempFundedBy } from '../../utils/tempFundingByFiller';
 import { useTranslation } from 'react-i18next';
 
 interface InactiveCommunity {
@@ -24,7 +22,6 @@ const ProfileHeader: React.FC<{
   const { community, inactiveComm } = props;
 
   const [addressTooltipCopy, setAddressTooltipCopy] = useState('Click to copy');
-  const [linkTooltipCopy, setlinkTooltipCopy] = useState('Copy link');
   const { t } = useTranslation();
 
   return (
@@ -70,63 +67,24 @@ const ProfileHeader: React.FC<{
               tooltipContent={addressTooltipCopy}
             />
           </Col>
-          <Col className={classes.subInfoRow}>
-            <div>
-              {t('fundedBy')}{' '}
-              <span className={classes.funder}>
-                {community ? tempFundedBy(community.contractAddress) : 'N/A'}
-              </span>
-            </div>
-            <div className={classes.spacer}>·</div>
-            <div className={classes.phLink}>
-              <div>
-                prop.house/
-                {trimEthAddress(
-                  community
-                    ? community.contractAddress
-                    : inactiveComm
-                    ? inactiveComm.contractAddress
-                    : '0x0000000000000000000000000000000000000000',
-                )}
-              </div>
 
-              <Tooltip
-                content={
-                  <IoCopy
-                    size={'1rem'}
-                    className={classes.copyIcon}
-                    onMouseEnter={() => setlinkTooltipCopy('copy link')}
-                    onClick={() => {
-                      setlinkTooltipCopy('copied!');
-                      navigator.clipboard.writeText(
-                        `prop.house/${
-                          community
-                            ? community.contractAddress
-                            : inactiveComm
-                            ? inactiveComm.contractAddress
-                            : '0x0000000000000000000000000000000000000000'
-                        }`,
-                      );
-                    }}
-                  />
-                }
-                tooltipContent={linkTooltipCopy}
-              />
-            </div>
-          </Col>
           <Col className={classes.propHouseDataRow}>
             <div className={classes.item}>
-              <div className={classes.itemTitle}>{t('proposals2')}</div>
               <div className={classes.itemData}>{community ? community.numProposals : 0}</div>
+              <div className={classes.itemTitle}>{t('proposals2')}</div>
             </div>
             <div className={classes.item}>
-              <div className={classes.itemTitle}>{t('rounds')}</div>
               <div className={classes.itemData}>{community ? community.numAuctions : 0}</div>
+              <div className={classes.itemTitle}>{t('rounds')}</div>
             </div>
             <div className={classes.item}>
-              <div className={classes.itemTitle}>{t('funded')}</div>
               <div className={classes.itemData}>{community ? community.ethFunded : 0} Ξ</div>
+              <div className={classes.itemTitle}>{t('funded')}</div>
             </div>
+          </Col>
+
+          <Col className={classes.communityDescriptionRow}>
+            <p>{community?.description}</p>
           </Col>
         </Col>
       </Col>
