972f4504f27234cba553f666e5a8776a71321c85
christianturner <christianturner@hey.com>
2022-08-25 07:27:02 -0400

    WIP: prop card states

--- packages/prop-house-webapp/src/components/ProposalCards/index.tsx
+++ packages/prop-house-webapp/src/components/ProposalCards/index.tsx
@@ -14,6 +14,10 @@ import Button, { ButtonColor } from '../Button';
 import { useNavigate } from 'react-router-dom';
 import clsx from 'clsx';
 import useWeb3Modal from '../../hooks/useWeb3Modal';
+import { MdOutlineLightbulb as BulbIcon, MdHowToVote as VoteIcon } from 'react-icons/md';
+// import RoundMessage from '../RoundMessage';
+import dayjs from 'dayjs';
+import { findProposalByAddress } from '../../utils/findProposalByAddress';
 
 const ProposalCards: React.FC<{
   auction: StoredAuction;
@@ -51,164 +55,319 @@ const ProposalCards: React.FC<{
   const isVotingWindow = auctionStatus(auction) === AuctionStatus.AuctionVoting;
   const isRoundOver = auctionStatus(auction) === AuctionStatus.AuctionEnded;
 
-  return (
-    <div className={classes.propCards}>
-      <Row style={{ width: '100%' }}>
-        <Col xl={8}>
-          {proposals &&
-            proposals.map((proposal, index) => {
-              return (
-                <Col key={index}>
-                  <ProposalCard
-                    proposal={proposal}
-                    auctionStatus={auctionStatus(auction)}
-                    cardStatus={cardStatus(delegatedVotes ? delegatedVotes > 0 : false, auction)}
-                    votesFor={aggVoteWeightForProp(
-                      extractAllVotes(proposals ? proposals : [], account ? account : ''),
-                      proposal.id,
-                    )}
-                    canAllotVotes={canAllotVotes}
-                    voteAllotments={voteAllotments}
-                    handleVoteAllotment={handleVoteAllotment}
-                  />
-                </Col>
-              );
-            })}
-        </Col>
+  const hasSubmittedProp = () => account && proposals && proposals.some(p => p.address === account);
+  const getVoteTotal = () =>
+    proposals && proposals.reduce((total, prop) => (total = total + Number(prop.score)), 0);
+  // proposals && account && console.log('hasSubmittedProp', hasSubmittedProp());
 
-        <Col xl={4}>
+  // proposals &&
+  //   account &&
+  //   console.log('findProposalByAddress', findProposalByAddress(account, proposals));
+  const myProp =
+    account && proposals && hasSubmittedProp() && findProposalByAddress(account, proposals);
+
+  return (
+    // <div className={classes.propCards}>
+    <Row style={{ width: '100%' }}>
+      <Col xl={8}>
+        {proposals &&
+          proposals.map((proposal, index) => {
+            return (
+              <Col key={index}>
+                <ProposalCard
+                  proposal={proposal}
+                  auctionStatus={auctionStatus(auction)}
+                  cardStatus={cardStatus(delegatedVotes ? delegatedVotes > 0 : false, auction)}
+                  votesFor={aggVoteWeightForProp(
+                    extractAllVotes(proposals ? proposals : [], account ? account : ''),
+                    proposal.id,
+                  )}
+                  canAllotVotes={canAllotVotes}
+                  voteAllotments={voteAllotments}
+                  handleVoteAllotment={handleVoteAllotment}
+                />
+              </Col>
+            );
+          })}
+      </Col>
+
+      <Col xl={4}>
+        {/* VOTING, CONNECTED, YOUR PROP */}
+        {/* {isVotingWindow && account && hasSubmittedProp() && (
           <Card
             bgColor={CardBgColor.White}
             borderRadius={CardBorderRadius.thirty}
-            classNames={classes.sidebarContainerCard}
+            classNames={clsx(classes.sidebarContainerCard, classes.userPropCard)}
           >
-            <div className={classes.content}>
-              {isProposingWindow && (
-                <>
-                  <h1 className={classes.sideCardTitle}>{`Submit a prop`}</h1>
+            <p className={classes.userPropTitle}>{myProp && myProp.title}</p>
+
+            <div className={classes.userPropInfo}>
+              <div className={classes.userPropItem}>
+                <div>
+                  <VoteIcon />
+                </div>
+                <div>
+                  x<sup>th</sup> place
+                </div>
+              </div>
+
+              <div className={classes.userPropItem}>
+                <div>
+                  <VoteIcon />
+                </div>
+                <div>{myProp && `${myProp.score} votes`}</div>
+              </div>
+            </div>
+          </Card>
+        )} */}
+
+        <Card
+          bgColor={CardBgColor.White}
+          borderRadius={CardBorderRadius.thirty}
+          classNames={classes.sidebarContainerCard}
+        >
+          {/* 
+          // 
+          // 
+          // 
+          // 
+          // 
+          // 
+          // 
+          // 
+          // 
+          // 
+          // 
+          // CONTENT
+          // 
+          // 
+          // 
+          // 
+          // 
+          // 
+          // 
+          // 
+          // 
+          // 
+          // 
+          */}
+          <div className={classes.content}>
+            {/* ACCEPTING PROPS */}
+            {isProposingWindow && (
+              <>
+                <div className={classes.sideCardHeader}>
+                  <div className={clsx(classes.icon, classes.greenIcon)}>
+                    <BulbIcon />
+                  </div>
+                  <div className={classes.textContainer}>
+                    <p className={classes.title}>{`Accepting proposals`}</p>
+                    <p className={classes.subtitle}>
+                      Until {dayjs(auction.proposalEndTime).format('MMMM D')}
+                    </p>
+                  </div>
+                </div>
+
+                <hr className={classes.divider} />
+
+                <p className={classes.sideCardBody}>
+                  <b>How proposals work:</b>
+
+                  <div className={classes.bulletList}>
+                    <div className={classes.bulletItem}>
+                      <hr className={classes.bullet} />
+                      <p>Anyone can submit a proposal to get funded.</p>
+                    </div>
+
+                    <div className={classes.bulletItem}>
+                      <hr className={classes.bullet} />
+                      <p>
+                        Owners of the <b>{community.name}</b> token will vote on the best proposals.
+                      </p>
+                    </div>
+
+                    {/* <div className={classes.bulletItem}>
+                      <hr className={classes.bullet} />
+                      <p>
+                        Each <b>{community.name}</b> token has 10 votes.
+                      </p>
+                    </div> */}
+
+                    <div className={classes.bulletItem}>
+                      <hr className={classes.bullet} />
+                      <p>
+                        The top <b>{auction.numWinners}</b> proposals will get funded{' '}
+                        <b>
+                          {auction.fundingAmount} {auction.currencyType}{' '}
+                        </b>
+                        each.
+                      </p>
+                    </div>
+                  </div>
+                </p>
+              </>
+            )}
+
+            {/* VOTING WINDOW */}
+            {isVotingWindow && (
+              <>
+                {/* VOTING, CONNECTED & NOT CONNECTED */}
+                <div className={classes.sideCardHeader}>
+                  <div className={clsx(classes.icon, classes.purpleIcon)}>
+                    <VoteIcon />
+                  </div>
+                  <div className={classes.textContainer}>
+                    <p className={classes.title}>Voting in progress</p>
+                    <p className={classes.subtitle}>
+                      <span className={classes.purpleText}>{getVoteTotal()}</span> votes cast so
+                      far!
+                    </p>
+                  </div>
+                </div>
+
+                <hr className={classes.divider} />
+
+                {/* VOTING, NOT CONNECTED */}
+                {!account && (
                   <p className={classes.sideCardBody}>
-                    {
-                      <>
-                        Anyone can submit a proposal! Something about how the rounds and timing
-                        works?
-                        <br />
-                        <br />
-                        Each noun holder gets 10 votes per noun.
-                      </>
-                    }
+                    Owners of the <b>{community.name}</b> token are voting on their favorite
+                    proposals.
                   </p>
-                </>
-              )}
+                )}
 
-              {isVotingWindow && account && delegatedVotes && votesLeft && (
-                <>
-                  <h1 className={clsx(classes.sideCardTitle, classes.votingInfo)}>
-                    {`Cast your votes`}
+                {/* VOTING, CONNECTED, NO VOTES */}
+                {account && !delegatedVotes && (
+                  <p className={classes.subtitle}>
+                    <b>You don't have any {community.name} required to vote in this house.</b>
+                  </p>
+                )}
+              </>
+            )}
 
-                    <span className={classes.totalVotes}>{`${
-                      votesLeft > 0
-                        ? // ? `${votesLeft && votesLeft} left`
-                          `${
-                            delegatedVotes - (submittedVotesCount ?? 0) - (numAllottedVotes ?? 0)
-                          } left`
-                        : 'no votes left'
-                    }`}</span>
-                  </h1>
+            {/* VOTING WINDOW WITH VOTES: PROGRESS BAR */}
+            {isVotingWindow && account && delegatedVotes && votesLeft ? (
+              <>
+                <h1 className={clsx(classes.sideCardTitle, classes.votingInfo)}>
+                  <span>Cast your votes</span>
+
+                  <span className={classes.totalVotes}>{`${
+                    votesLeft > 0
+                      ? // ? `${votesLeft && votesLeft} left`
+                        `${
+                          delegatedVotes - (submittedVotesCount ?? 0) - (numAllottedVotes ?? 0)
+                        } left`
+                      : 'no votes left'
+                  }`}</span>
+                </h1>
+
+                <ProgressBar
+                  className={clsx(
+                    classes.votingBar,
+                    submittedVotesCount &&
+                      submittedVotesCount > 0 &&
+                      delegatedVotes !== submittedVotesCount &&
+                      'roundAllotmentBar',
+                  )}
+                >
+                  <ProgressBar
+                    variant="success"
+                    // now={submittedVotesCount}
+                    now={
+                      100 -
+                      Math.abs(((submittedVotesCount ?? 0) - delegatedVotes) / delegatedVotes) * 100
+                    }
+                  />
 
                   <ProgressBar
-                    className={clsx(
-                      classes.votingBar,
-                      submittedVotesCount &&
-                        submittedVotesCount > 0 &&
-                        delegatedVotes !== submittedVotesCount &&
-                        'roundAllotmentBar',
-                    )}
-                  >
-                    <ProgressBar
-                      variant="success"
-                      // now={submittedVotesCount}
-                      now={
-                        100 -
-                        Math.abs(((submittedVotesCount ?? 0) - delegatedVotes) / delegatedVotes) *
-                          100
-                      }
-                    />
+                    variant="warning"
+                    now={Math.abs((votesLeft - delegatedVotes) / delegatedVotes) * 100}
+                    key={2}
+                  />
+                </ProgressBar>
 
-                    <ProgressBar
-                      variant="warning"
-                      now={Math.abs((votesLeft - delegatedVotes) / delegatedVotes) * 100}
-                      key={2}
-                    />
-                  </ProgressBar>
+                <p className={classes.sideCardBody}>
+                  {<>Nouns get 10 votes per each Noun they hold or are delegated.</>}
+                </p>
+              </>
+            ) : null}
 
-                  <p className={classes.sideCardBody}>
-                    {<>Nouns get 10 votes per each Noun they hold or are delegated.</>}
-                  </p>
-                </>
-              )}
+            {/* ROUND ENDED */}
+            {isRoundOver && (
+              <>
+                <h1 className={classes.sideCardTitle}>{`Round ended`}</h1>
 
-              {isVotingWindow && account && !delegatedVotes && (
-                <>
-                  <h1 className={classes.sideCardTitle}>{`No Votes`}</h1>
+                <p className={classes.sideCardBody}>
+                  {
+                    <>
+                      This round ended, and winners have been voted on. The winning props are
+                      highlighted in green, check them out!
+                    </>
+                  }
+                </p>
+              </>
+            )}
+          </div>
 
-                  <p className={classes.sideCardBody}>
-                    {<>You do not have any votes for this house.</>}
-                  </p>
-                </>
-              )}
-
-              {isVotingWindow && !account && (
-                <>
-                  <h1 className={classes.sideCardTitle}>{`Connect your wallet`}</h1>
-
-                  <p className={classes.sideCardBody}>{<>Please connect your wallet to vote.</>}</p>
-                </>
-              )}
-
-              {isRoundOver && (
-                <>
-                  <h1 className={classes.sideCardTitle}>{`Round ended`}</h1>
-
-                  <p className={classes.sideCardBody}>
-                    {
-                      <>
-                        This round ended, and winners have been voted on. The winning props are
-                        highlighted in green, check them out!
-                      </>
-                    }
-                  </p>
-                </>
-              )}
-            </div>
-
-            <div className={classes.btnContainer}>
-              {isProposingWindow && (
+          {/* 
+          // 
+          // 
+          // 
+          // 
+          // 
+          // 
+          // 
+          // 
+          // 
+          // 
+          // 
+          // BUTTONS
+          // 
+          // 
+          // 
+          // 
+          // 
+          // 
+          // 
+          // 
+          // 
+          // 
+          // 
+          */}
+          <div className={classes.btnContainer}>
+            {/* ACCEPTING PROPS */}
+            {isProposingWindow &&
+              (account ? (
+                // WALLET CONNECTED
                 <Button
-                  text={'Submit your proposal'}
+                  text={'Create your proposal'}
                   bgColor={ButtonColor.Green}
                   onClick={() => navigate('/create', { state: { auction, community } })}
                 />
-              )}
+              ) : (
+                // WALLET NOT CONNECTED
+                <Button text={'Connect to submit'} bgColor={ButtonColor.Pink} onClick={connect} />
+              ))}
 
-              {isVotingWindow && !account && (
-                <Button text={'Connect'} bgColor={ButtonColor.Purple} onClick={connect} />
-              )}
+            {/* VOTING WINDOW, NOT CONNECTED */}
+            {isVotingWindow && !account && (
+              <Button text={'Connect to vote'} bgColor={ButtonColor.Pink} onClick={connect} />
+            )}
 
-              {isVotingWindow && account && delegatedVotes && (
-                <Button
-                  text={'Submit votes'}
-                  bgColor={ButtonColor.Purple}
-                  onClick={handleVote}
-                  disabled={numAllottedVotes === 0 || submittedVotesCount === delegatedVotes}
+            {/* VOTING PERIOD, CONNECTED, HAS VOTES */}
+            {isVotingWindow && account && delegatedVotes ? (
+              <Button
+                text={'Submit votes'}
+                bgColor={ButtonColor.Purple}
+                onClick={handleVote}
+                disabled={numAllottedVotes === 0 || submittedVotesCount === delegatedVotes}
 
-                  // disabled={!canAllotVotes()}
-                />
-              )}
-            </div>
-          </Card>
-        </Col>
-      </Row>
-    </div>
+                // disabled={!canAllotVotes()}
+              />
+            ) : null}
+          </div>
+        </Card>
+      </Col>
+    </Row>
+    // </div>
   );
 };
 export default ProposalCards;
