82d0dde19f920253173118fd12d917a55426b692
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-08-29 17:32:53 -0400

    consume blocktag on fe, resolve in communities package

--- packages/prop-house-communities/src/subgraph/nounsQuery.ts
+++ packages/prop-house-communities/src/subgraph/nounsQuery.ts
@@ -1,28 +1,18 @@
-/**
- * Attempt to resolve a blocktag into a number, if the tag isn't a number return undefined.
- */
-const parseBlockTag = (blockTag: number | string | "latest" | undefined): number | undefined => {
-  if (typeof blockTag === "number" || typeof blockTag === "undefined") return blockTag;
-  let blockNumber: number | undefined = undefined;
-  const parsedBlockTag = blockTag.includes("0x") ? parseInt(blockTag, 16) : parseInt(blockTag, 10)
-  if (!isNaN(parsedBlockTag)) blockNumber = parsedBlockTag;
-  return blockNumber;
-}
+import { parseBlockTag } from '../utils/parseBlockTag';
 
 export const nounsDelegatedVotesToAddressQuery = (
   address: string,
-  blockTag: number | string | "latest" | undefined
+  blockTag: number | string | 'latest' | undefined,
 ) => {
-  const parsedBlockTag = parseBlockTag(blockTag)
+  const parsedBlockTag = parseBlockTag(blockTag);
   return `
   {
     delegates(
-      ${parsedBlockTag ? `block: { number: ${parsedBlockTag} },` : ""}
+      ${parsedBlockTag ? `block: { number: ${parsedBlockTag} },` : ''}
       where: { id: "${address}" }
     ) {
       delegatedVotesRaw
     }
   }
 `;
-}
-
+};
