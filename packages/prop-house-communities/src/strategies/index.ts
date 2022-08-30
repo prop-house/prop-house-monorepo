3b245090e6e7ae098475a2af12e8686ef1ae4bde
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-08-30 18:06:57 -0400

    Add comments

--- packages/prop-house-communities/src/strategies/index.ts
+++ packages/prop-house-communities/src/strategies/index.ts
@@ -1,6 +1,9 @@
 import { nouns } from './nouns';
 import { onchainmonkey } from './onchainmonkey';
 
+/**
+ * Custom voting strategies for communities.
+ */
 export const strategies = {
   nouns,
   onchainmonkey,
--- packages/prop-house-communities/src/types/Strategy.ts
+++ packages/prop-house-communities/src/types/Strategy.ts
@@ -1,16 +1,18 @@
 import { Provider } from '@ethersproject/providers';
 
 /**
- * Voting strategy for communities that required alt approaches to counting votes (other than the default `balanceOf` call)
+ * Voting strategy for communities that require custom approaches to counting votes.
  */
 export interface Strategy {
+  /** contract address for which the voting strategy is being used for */
   address: string;
+  /** vote strategy  */
   numVotes: (
     userAddress: string,
     provider: Provider,
     commmunityAddress: string,
     blockTag: string,
   ) => Promise<number>;
-  /** [optional] multiplier of votes. number to be used against default number of votes (e.g. make each nft count as 10 votes instead of 1) */
+  /** multiplier of votes. number to be used against default number of votes (e.g. make each nft count as 10 votes instead of 1) */
   multiplier?: number;
 }
