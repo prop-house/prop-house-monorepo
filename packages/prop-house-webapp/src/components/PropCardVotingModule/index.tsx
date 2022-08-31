63e2c70e673e4d7d5bfef971e5e215da7ac7163b
christianturner <christianturner@hey.com>
2022-08-31 07:16:58 -0400

    new voting buttons ui

--- packages/prop-house-webapp/src/components/PropCardVotingModule/index.tsx
+++ packages/prop-house-webapp/src/components/PropCardVotingModule/index.tsx
@@ -4,7 +4,7 @@ import { ProposalCardStatus } from '../../utils/cardStatus';
 import { VoteAllotment, votesForProp } from '../../utils/voteAllotment';
 import { StoredProposal } from '@nouns/prop-house-wrapper/dist/builders';
 import Button, { ButtonColor } from '../Button';
-// import clsx from 'clsx';
+import clsx from 'clsx';
 
 interface PropCardVotingContainerProps {
   proposal: StoredProposal;
@@ -24,25 +24,33 @@ const PropCardVotingModule: React.FC<{
 
   const handleClick = (e: any, direction: boolean) => {
     e.stopPropagation();
+
     handleVoteAllotment(proposal.id, direction);
   };
 
   return (
     <Row>
       <Col xs={12} className={classes.bottomContainer} onClick={(e: any) => e.stopPropagation()}>
-        <div className={classes.votesModuleContainer}>
+        <div
+          className={clsx(
+            classes.votesModuleContainer,
+            allottedVotesForProp > 0 && classes.activelyAllotting,
+          )}
+        >
+          <div className={classes.votesAllottedDisplay}> {allottedVotesForProp} </div>
+        </div>
+
+        <div className={classes.voteBtns}>
           <Button
             text="↓"
-            bgColor={ButtonColor.Purple}
+            bgColor={allottedVotesForProp > 0 ? ButtonColor.PurpleLight : ButtonColor.Gray}
             classNames={classes.voteBtn}
-            // classNames={clsx(classes.voteBtn, classes.disabledVote)}
             onClick={e => handleClick(e, false)}
             disabled={allottedVotesForProp === 0}
           />
-          <div className={classes.votesAllottedDisplay}> {allottedVotesForProp} </div>
           <Button
             text="↑"
-            bgColor={ButtonColor.Purple}
+            bgColor={allottedVotesForProp > 0 ? ButtonColor.PurpleLight : ButtonColor.Gray}
             classNames={classes.voteBtn}
             onClick={e => handleClick(e, true)}
             disabled={!canAllotVotes()}
