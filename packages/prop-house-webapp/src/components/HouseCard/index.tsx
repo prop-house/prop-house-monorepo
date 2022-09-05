a92459f943c0d168da91b420c51ece7a9c849238
christianturner <christianturner@hey.com>
2022-09-05 16:03:38 -0400

    mobile styling for House cards

--- packages/prop-house-webapp/src/components/HouseCard/index.tsx
+++ packages/prop-house-webapp/src/components/HouseCard/index.tsx
@@ -13,6 +13,8 @@ import StatusPill from '../StatusPill';
 import { nameToSlug } from '../../utils/communitySlugs';
 import diffTime from '../../utils/diffTime';
 import { useTranslation } from 'react-i18next';
+import Tooltip from '../Tooltip';
+import dayjs from 'dayjs';
 
 const HouseCard: React.FC<{
   round: StoredAuction;
@@ -43,7 +45,7 @@ const HouseCard: React.FC<{
             classes.houseCard,
           )}
         >
-          <div className={classes.textContainter}>
+          <div className={classes.textContainer}>
             <div className={classes.titleContainer}>
               <div className={classes.authorContainer}>{round.title}</div>
               <StatusPill status={auctionStatus(round)} />
@@ -67,8 +69,19 @@ const HouseCard: React.FC<{
             <div className={classes.divider}></div>
 
             <div className={classes.section}>
-              <p className={classes.title}>{DeadlineCopy(round)}</p>
-              <p className={classes.info}>{diffTime(deadlineTime(round))}</p>
+              <Tooltip
+                content={
+                  <>
+                    <p className={classes.title}>{DeadlineCopy(round)}</p>
+                    <p className={classes.info}>
+                      {diffTime(deadlineTime(round)).replace('months', 'mos')}
+                    </p>
+                  </>
+                }
+                tooltipContent={`${dayjs(deadlineTime(round))
+                  .tz()
+                  .format('MMMM D, YYYY h:mm A z')}`}
+              />
             </div>
 
             <div className={clsx(classes.divider, classes.propSection)}></div>
