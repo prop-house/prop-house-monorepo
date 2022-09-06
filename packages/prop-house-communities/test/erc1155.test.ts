d064cac56a04d6a6190e9d569bdb86fd1513b56e
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-09-06 17:52:37 -0400

    add tests for erc1155

--- packages/prop-house-communities/test/erc1155.test.ts
+++ packages/prop-house-communities/test/erc1155.test.ts
@@ -3,10 +3,11 @@ import { Contract, providers, utils } from 'ethers';
 import { infuraEndpoint } from './src/constants/infuraEndpoint';
 import BalanceOfABI1155 from '../src/abi/BalanceOf1155ABI.json';
 import { balanceOf, erc1155 } from '../src/strategies';
+import { getNumVotes } from '../src/actions';
 
 describe('erc1155 votes', () => {
   let apiKey;
-  const nounSquareAddress = '0x7c2748c7ec984b559eadc39c7a4944398e34911a';
+  const nounsSquareTeamAddress = '0xbfe00921005f86699760c84c38e3cc86d38745cf';
   const holderAddress = '0x0B20ED7418f153a6595E0F88c682DF52d54B00c4';
 
   before('jsonRpcProvider api key should be available in .env file', () => {
@@ -22,23 +23,7 @@ describe('erc1155 votes', () => {
     apiKey = process.env.INFURA_PROJECT_ID;
     if (!apiKey) return;
     const provider = new providers.JsonRpcProvider(infuraEndpoint(apiKey));
-
-    const contract = new Contract(nounSquareAddress, BalanceOfABI1155, provider);
-    const balanceOf = await contract.balanceOf(holderAddress, 1);
-    const numBalanceOf = Number(utils.formatEther(balanceOf));
-
-    expect(numBalanceOf).to.be.gt(0);
-  });
-
-  it('holder address should have a positive balance for tokenId 2', async () => {
-    apiKey = process.env.INFURA_PROJECT_ID;
-    if (!apiKey) return;
-    const provider = new providers.JsonRpcProvider(infuraEndpoint(apiKey));
-
-    const contract = new Contract(nounSquareAddress, BalanceOfABI1155, provider);
-    const balanceOf = await contract.balanceOf(holderAddress, 2);
-    const numBalanceOf = Number(utils.formatEther(balanceOf));
-
-    expect(numBalanceOf).to.be.gt(0);
+    const votes = await getNumVotes(holderAddress, nounsSquareTeamAddress, provider, '15435382');
+    expect(votes).to.be.eq(1);
   });
 });
