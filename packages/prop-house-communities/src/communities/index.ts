84d5877f2d2e93333d6cf2f8a4b57c13231bde90
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-09-12 14:33:39 -0400

    add test for communities

--- packages/prop-house-communities/src/communities/index.ts
+++ packages/prop-house-communities/src/communities/index.ts
@@ -1,4 +1,4 @@
-import { erc1155, onchainMonkey } from '../strategies';
+import { balanceOfErc721, erc1155, onchainMonkey } from '../strategies';
 import { nouns } from '../strategies/nouns';
 
 /**
@@ -13,4 +13,16 @@ export const communities = {
   '0xbfe00921005f86699760c84c38e3cc86d38745cf': erc1155(1, 1),
   // the noun square contests
   '0x7c2748c7ec984b559eadc39c7a4944398e34911a': erc1155(2, 1),
+  // cryptoadz
+  '0x1CB1A5e65610AEFF2551A50f76a87a7d3fB649C6': balanceOfErc721(),
+  // nouns japan
+  '0x898a7dbfddf13962df089fbc8f069fa7ce92cdbb': balanceOfErc721(),
+  // nounpunks
+  '0xe169c2ed585e62b1d32615bf2591093a629549b6': balanceOfErc721(),
+  // uma
+  '0x2381b67c6f1cb732fdf8b3b29d3260ec6f7420bc': balanceOfErc721(),
+  // lil nouns
+  '0x4b10701Bfd7BFEdc47d50562b76b436fbB5BdB3B': balanceOfErc721(),
+  // mfers
+  '0x79FCDEF22feeD20eDDacbB2587640e45491b757f': balanceOfErc721(),
 };
