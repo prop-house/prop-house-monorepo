54437d75f26dc5df596ab80076b858f84de338bb
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-09-12 13:08:54 -0400

    fix useless throw

--- packages/prop-house-communities/src/actions/getName.ts
+++ packages/prop-house-communities/src/actions/getName.ts
@@ -5,14 +5,15 @@ import NameABI from '../abi/NameABI.json';
 /**
  * Gets `name` from contract (assuming it complies w the ERC721/ERC20 standard)
  */
-export const getName = async (commnunityAddress: string, provider: Provider): Promise<string> => {
+export const getName = async (
+  commnunityAddress: string,
+  provider: Provider,
+): Promise<string | undefined> => {
   if (!ethers.utils.isAddress(commnunityAddress)) throw new Error('Community address is not valid');
 
   const contract = new ethers.Contract(commnunityAddress, NameABI, provider);
 
-  try {
-    return await contract.name();
-  } catch (e) {
-    throw e;
-  }
+  const name = await contract.name();
+  if (!name) throw Error(`Error fetching name for contract ${commnunityAddress}`);
+  return name;
 };
