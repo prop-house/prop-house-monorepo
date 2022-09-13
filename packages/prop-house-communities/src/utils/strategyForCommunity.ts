909ffa5ed902afe6e276c82e52c957b5b4b89404
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-09-13 16:23:51 -0400

    replace communities type

--- packages/prop-house-communities/src/utils/strategyForCommunity.ts
+++ packages/prop-house-communities/src/utils/strategyForCommunity.ts
@@ -4,8 +4,5 @@ import { communities } from '../communities';
  * Get strategy for community with address from /strategies dir
  */
 export const strategyForCommunity = (communityAddress: string) => {
-  const strat = Object.entries(communities).find(
-    strat => strat[0].toLowerCase() === communityAddress.toLowerCase(),
-  );
-  return strat ? strat[1] : undefined;
+  return communities.get(communityAddress);
 };
