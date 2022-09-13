522917923e5eb392c79a0c9f16d4b48206716916
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-09-13 13:36:52 -0400

    fix var name

--- packages/prop-house-communities/src/actions/getName.ts
+++ packages/prop-house-communities/src/actions/getName.ts
@@ -6,14 +6,14 @@ import NameABI from '../abi/NameABI.json';
  * Gets `name` from contract (assuming it complies w the ERC721/ERC20 standard)
  */
 export const getName = async (
-  commnunityAddress: string,
+  communityAddress: string,
   provider: Provider,
 ): Promise<string | undefined> => {
-  if (!ethers.utils.isAddress(commnunityAddress)) throw new Error('Community address is not valid');
+  if (!ethers.utils.isAddress(communityAddress)) throw new Error('Community address is not valid');
 
-  const contract = new ethers.Contract(commnunityAddress, NameABI, provider);
+  const contract = new ethers.Contract(communityAddress, NameABI, provider);
 
   const name = await contract.name();
-  if (!name) throw Error(`Error fetching name for contract ${commnunityAddress}`);
+  if (!name) throw Error(`Error fetching name for contract ${communityAddress}`);
   return name;
 };
