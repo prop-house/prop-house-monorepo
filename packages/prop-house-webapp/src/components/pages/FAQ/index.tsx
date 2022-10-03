17f6081f8f74dc79189f37348477cc702d54e63f
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-10-03 10:27:39 -0400

    add new faq content

--- packages/prop-house-webapp/src/components/pages/FAQ/index.tsx
+++ packages/prop-house-webapp/src/components/pages/FAQ/index.tsx
@@ -8,17 +8,30 @@ interface ContentItem {
   content: React.ReactNode;
 }
 
-const content: ContentItem[] = [
-  { title: 'faq1title', content: 'faq1answer' },
-  { title: 'faq2title', content: 'faq2answer' },
-  { title: 'faq3title', content: 'faq3answer' },
-  { title: 'faq4title', content: 'faq4answer' },
-  { title: 'faq5title', content: 'faq5answer' },
-  { title: 'faq6title', content: 'faq6answer' },
-];
-
 const FAQ = () => {
   const { t } = useTranslation();
+
+  const content: ContentItem[] = [
+    { title: 'faq1title', content: 'faq1answer' },
+    { title: 'faq2title', content: 'faq2answer' },
+    { title: 'faq3title', content: 'faq3answer' },
+    { title: 'faq4title', content: 'faq4answer' },
+    { title: 'faq5title', content: 'faq5answer' },
+    { title: 'faq6title', content: 'faq6answer' },
+    { title: 'faq7title', content: 'faq7answer' },
+    { title: 'faq8title', content: 'faq8answer' },
+    { title: 'faq9title', content: 'faq9answer' },
+    { title: 'faq10title', content: 'faq10answer' },
+    { title: 'faq11title', content: 'faq11answer' },
+    { title: 'faq12title', content: 'faq12answer' },
+    { title: 'faq13title', content: 'faq13answer' },
+    { title: 'faq14title', content: 'faq14answer' },
+    { title: 'faq15title', content: 'faq15answer' },
+    { title: 'faq16title', content: 'faq16answer' },
+    { title: 'faq17title', content: 'faq17answer' },
+    { title: 'faq18title', content: 'faq18answer' },
+    { title: 'faq19title', content: 'faq19answer' },
+  ];
 
   return (
     <>
