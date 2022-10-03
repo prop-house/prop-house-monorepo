18c7349c6d317042be77a6ba09ca29b46ca1dd7d
christianturner <christianturner@hey.com>
2022-10-03 10:20:58 -0400

    truncate funded amount if 4 digits

--- packages/prop-house-webapp/src/components/CommunityCard/index.tsx
+++ packages/prop-house-webapp/src/components/CommunityCard/index.tsx
@@ -2,6 +2,7 @@ import classes from './CommunityCard.module.css';
 import { Community } from '@nouns/prop-house-wrapper/dist/builders';
 import CommunityProfImg from '../CommunityProfImg';
 import { useTranslation } from 'react-i18next';
+import TruncateThousands from '../TruncateThousands';
 
 const CommunityCard: React.FC<{
   community: Community;
@@ -21,7 +22,9 @@ const CommunityCard: React.FC<{
               <img src="/eth.png" alt="eth" />
             </div>
             <div className={classes.infoText}>
-              <span className={classes.infoAmount}>{community.ethFunded}</span>{' '}
+              <span className={classes.infoAmount}>
+                <TruncateThousands amount={community.ethFunded} />
+              </span>{' '}
               <span className={classes.infoCopy}>funded</span>
             </div>
           </div>
