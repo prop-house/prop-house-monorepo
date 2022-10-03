44eee5477efbe91266ac3b8830c9a514303044b2
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-10-03 17:03:43 -0400

    add learn more btn

--- packages/prop-house-webapp/src/components/HomeHeader/index.tsx
+++ packages/prop-house-webapp/src/components/HomeHeader/index.tsx
@@ -4,6 +4,7 @@ import HomeTitle from '../HomeTitle';
 import HomeStats from '../HomeStats';
 import HomeSearchBar from '../HomeSeachBar';
 import { StatsProps } from '../pages/Home';
+import { useNavigate } from 'react-router-dom';
 
 interface HomeHeaderProps {
   input: string;
@@ -12,9 +13,14 @@ interface HomeHeaderProps {
 }
 
 const HomeHeader = ({ input, handleSeachInputChange, stats }: HomeHeaderProps) => {
+  const navigate = useNavigate();
+
   return (
     <Row className={classes.wrapper}>
       <HomeTitle />
+      <button className={classes.learnMoreBtn} onClick={() => navigate('/faq')}>
+        Learn more →
+      </button>
       <HomeStats stats={stats} />
       <HomeSearchBar input={input} handleSeachInputChange={handleSeachInputChange} />
     </Row>
