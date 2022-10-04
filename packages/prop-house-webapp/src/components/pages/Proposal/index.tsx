e26066eddcb43132b8a05fd8d0579a284a106bfc
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-10-04 15:25:57 -0400

    tests

--- packages/prop-house-webapp/src/components/pages/Proposal/index.tsx
+++ packages/prop-house-webapp/src/components/pages/Proposal/index.tsx
@@ -89,6 +89,10 @@ const Proposal = () => {
       <Container>
         <Helmet>
           <meta
+            property="og:image"
+            content="http://graphics8.nytimes.com/images/2012/02/19/us/19whitney-span/19whitney-span-articleLarge.jpg"
+          />
+          <meta
             name="twitter:image"
             content="http://graphics8.nytimes.com/images/2012/02/19/us/19whitney-span/19whitney-span-articleLarge.jpg"
           />
