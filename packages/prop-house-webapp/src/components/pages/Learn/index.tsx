ab0f759cd8d571c10b862aae0afef8c2868a250a
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-10-05 13:55:21 -0400

    re-add unnecessary removals

--- packages/prop-house-webapp/src/components/pages/Learn/index.tsx
+++ packages/prop-house-webapp/src/components/pages/Learn/index.tsx
@@ -1,3 +1,14 @@
+import classes from './Learn.module.css';
+import { Row, Col, Image } from 'react-bootstrap';
+import Button, { ButtonColor } from '../../Button';
+import outletsImg from '../../../assets/learn page/outlets.png';
+import auctionImg from '../../../assets/learn page/auction.png';
+import auctionFullImg from '../../../assets/learn page/auction_full.png';
+import communityImg from '../../../assets/learn page/community.png';
+import clsx from 'clsx';
+import { useNavigate } from 'react-router-dom';
+import ContactUsCTA from '../../ContactUsCTA';
+import { useTranslation } from 'react-i18next';
 import Helmet from 'react-helmet';
 
 const Learn = () => {
@@ -10,16 +21,16 @@ const Learn = () => {
         <meta property="og:image" content="cards.backend.prop.house/remote/proposal/3" />
         <meta property="twitter:image" content="cards.backend.prop.house/remote/proposal/3" />
       </Helmet>
-            xs={{ order: "first" }}
-            className={classes.leftCol}
-          >
+      <div className={clsx('break-out', classes.breakOutMobile)}>
+        <Row className={clsx('g-0', classes.row, 'justify-content-start')}>
+          <Col xl={{ span: 5 }} xs={{ order: 'first' }} className={classes.leftCol}>
             <Image src={outletsImg} fluid className={classes.img} />
           </Col>
-          <Col xl={{ span: 5, offset: 1 }} xs={{ order: "last" }}>
-            <h2>{t("plugInto")}</h2>
-            <p>{t("treasury")}</p>
+          <Col xl={{ span: 5, offset: 1 }} xs={{ order: 'last' }}>
+            <h2>{t('plugInto')}</h2>
+            <p>{t('treasury')}</p>
             <Button
-              text={t("exploreHouses")}
+              text={t('exploreHouses')}
               bgColor={ButtonColor.Pink}
               classNames={classes.firstBtn}
               onClick={() => {
@@ -30,34 +41,23 @@ const Learn = () => {
         </Row>
       </div>
 
-      <div className={clsx("break-out", classes.breakOutMobile)}>
-        <Row className={clsx("g-0", classes.row)}>
-          <Col
-            xl={{ span: 4, offset: 1, order: "first" }}
-            xs={{ order: "last" }}
-          >
+      <div className={clsx('break-out', classes.breakOutMobile)}>
+        <Row className={clsx('g-0', classes.row)}>
+          <Col xl={{ span: 4, offset: 1, order: 'first' }} xs={{ order: 'last' }}>
             <Col xs={12}>
-              <h2>{t("fundingRoundsTitle")}</h2>
+              <h2>{t('fundingRoundsTitle')}</h2>
             </Col>
             <Col xs={12}>
               <p>
-                {t("fundingRounds")}
+                {t('fundingRounds')}
                 <br />
                 <br />
-                {t("auctionEnd")}
+                {t('auctionEnd')}
               </p>
             </Col>
           </Col>
-          <Col
-            xl={{ span: 6, offset: 1 }}
-            xs={{ order: "first" }}
-            className={classes.rightCol}
-          >
-            <Image
-              src={auctionImg}
-              fluid
-              className={clsx(classes.img, classes.auctionImg)}
-            />
+          <Col xl={{ span: 6, offset: 1 }} xs={{ order: 'first' }} className={classes.rightCol}>
+            <Image src={auctionImg} fluid className={clsx(classes.img, classes.auctionImg)} />
             <Image
               src={auctionFullImg}
               fluid
@@ -67,30 +67,30 @@ const Learn = () => {
         </Row>
       </div>
 
-      <div className={clsx("break-out", classes.breakOutMobile)}>
-        <Row className={clsx("g-0", classes.row, "justify-content-start")}>
-          <Col xl={5} xs={{ order: "last" }} className={classes.leftCol}>
+      <div className={clsx('break-out', classes.breakOutMobile)}>
+        <Row className={clsx('g-0', classes.row, 'justify-content-start')}>
+          <Col xl={5} xs={{ order: 'last' }} className={classes.leftCol}>
             <Image src={communityImg} fluid className={classes.img} />
           </Col>
-          <Col xl={{ span: 5, offset: 1 }} xs={{ order: "last" }}>
-            <h2>{t("community")}</h2>
-            <p>{t("builderFirst")}</p>
+          <Col xl={{ span: 5, offset: 1 }} xs={{ order: 'last' }}>
+            <h2>{t('community')}</h2>
+            <p>{t('builderFirst')}</p>
 
             <a
               href="https://discord.gg/nouns"
               target="_blank"
               rel="noreferrer"
-              style={{ marginRight: "1rem" }}
+              style={{ marginRight: '1rem' }}
             >
               <Button
-                text={t("goToDiscord")}
+                text={t('goToDiscord')}
                 bgColor={ButtonColor.Purple}
                 classNames={classes.firstBtn}
               />
             </a>
             <a href="https://nouns.wtf/docs" target="_blank" rel="noreferrer">
               <Button
-                text={t("learnMore")}
+                text={t('learnMore')}
                 bgColor={ButtonColor.White}
                 classNames={classes.firstBtn}
               />
