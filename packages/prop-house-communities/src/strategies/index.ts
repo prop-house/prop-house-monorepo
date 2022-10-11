542b1c952d47fb9457b1b7f007671dc3aaee01df
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-10-11 05:26:51 -0400

    fix name

--- packages/prop-house-communities/src/strategies/index.ts
+++ packages/prop-house-communities/src/strategies/index.ts
@@ -1,5 +1,5 @@
 export { balanceOfErc721 } from './balanceOfErc721';
-export { balanceOfErc721Multiple } from './balaceOfErc721Multiple';
+export { balanceOfErc721Multiple } from './balanceOfErc721Multiple';
 export { balanceOfErc20 } from './balanceOfErc20';
 export { nouns } from './nouns';
 export { erc1155 } from './erc1155';
