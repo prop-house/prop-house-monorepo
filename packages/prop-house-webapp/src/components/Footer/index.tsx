03265a01955fcbf8bbf05fec30b4dca2d27c16d7
christianturner <christianturner@hey.com>
2022-10-12 07:04:03 -0400

    change icon sizing and order

--- packages/prop-house-webapp/src/components/Footer/index.tsx
+++ packages/prop-house-webapp/src/components/Footer/index.tsx
@@ -8,19 +8,19 @@ import { externalURL, ExternalURL } from '../../utils/externalURLs';
 const Footer = () => {
   const location = useLocation();
 
-  const discordURL = externalURL(ExternalURL.discord);
   const twitterURL = externalURL(ExternalURL.twitter);
+  const discordURL = externalURL(ExternalURL.discord);
   const githubURL = externalURL(ExternalURL.github);
 
   return (
     <div className={clsx(classes.footerContainer, bgColorForFooter(location.pathname))}>
       <div className={classes.footer}>
-        <a href={discordURL} target="_blank" rel="noreferrer">
-          <FaDiscord />
-        </a>
-
         <a href={twitterURL} target="_blank" rel="noreferrer">
           <FaTwitter />
+        </a>
+
+        <a href={discordURL} target="_blank" rel="noreferrer">
+          <FaDiscord />
         </a>
 
         <a href={githubURL} target="_blank" rel="noreferrer">
