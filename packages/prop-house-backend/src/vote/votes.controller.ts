9e986a59ae2539081444fe8c1a4aedf7e62d5da2
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-10-17 12:22:19 -0400

    rename var

--- packages/prop-house-backend/src/vote/votes.controller.ts
+++ packages/prop-house-backend/src/vote/votes.controller.ts
@@ -52,16 +52,14 @@ export class VotesController {
       Buffer.from(createVoteDto.signedData.message, 'base64').toString(),
     );
     var arr = Object.keys(signedPayload).map((key) => signedPayload[key]);
-    const correspondingVote = arr.find(
-      (v) => v.proposalId === foundProposal.id,
-    );
+    const voteFromPayload = arr.find((v) => v.proposalId === foundProposal.id);
 
     // Verify that signed payload is for corresponding prop and community
     if (
-      correspondingVote.proposalId !== createVoteDto.proposalId ||
-      correspondingVote.communityAddress !== createVoteDto.communityAddress ||
-      correspondingVote.address !== createVoteDto.address ||
-      correspondingVote.weight !== createVoteDto.weight
+      voteFromPayload.proposalId !== createVoteDto.proposalId ||
+      voteFromPayload.communityAddress !== createVoteDto.communityAddress ||
+      voteFromPayload.address !== createVoteDto.address ||
+      voteFromPayload.weight !== createVoteDto.weight
     )
       throw new HttpException(
         "Signed payload and supplied data doesn't match",
@@ -93,7 +91,7 @@ export class VotesController {
     );
 
     // Check that user won't exceed voting power by casting vote
-    if (aggVoteWeightSubmitted + correspondingVote.weight > votingPower)
+    if (aggVoteWeightSubmitted + voteFromPayload.weight > votingPower)
       throw new HttpException(
         'Signer does not have enough voting power to cast vote',
         HttpStatus.BAD_REQUEST,
