67ae4974c22b797075cea9bb65f6ffed2dc0f61f
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-10-18 13:10:24 -0400

    silence warning

--- packages/prop-house-webapp/src/components/TruncateThousands/index.tsx
+++ packages/prop-house-webapp/src/components/TruncateThousands/index.tsx
@@ -3,7 +3,7 @@ const TruncateThousands: React.FC<{ amount: number; decimals?: number }> = props
 
   const addDecimals = (amount: number) =>
     decimals
-      ? amount % 1 != 0
+      ? amount % 1 !== 0
         ? Number(amount).toFixed(decimals)
         : amount
       : Number(amount).toFixed(0);
