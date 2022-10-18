4683104ddce6fd84d4b6419da7e89db330e98a03
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2022-10-18 12:57:37 -0400

    display decimals when appropriate

--- packages/prop-house-webapp/src/components/TruncateThousands/index.tsx
+++ packages/prop-house-webapp/src/components/TruncateThousands/index.tsx
@@ -1,6 +1,10 @@
 const TruncateThousands: React.FC<{ amount: number; decimals?: number }> = props => {
   const { amount, decimals } = props;
-  return <>{amount > 1000 ? `${(amount / 1000).toFixed(decimals ? decimals : 1)}K` : amount}</>;
+
+  const addDecimals = (amount: number) =>
+    decimals ? Number(amount).toFixed(decimals) : Number(amount).toFixed(0);
+
+  return <>{addDecimals(amount > 1000 ? amount / 1000 : amount)}</>;
 };
 
 export default TruncateThousands;
