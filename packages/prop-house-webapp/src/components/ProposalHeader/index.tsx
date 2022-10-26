9f57833e91d02c25e9ebccf0089c4d04ae40fb2f
christianturner <christianturner@hey.com>
2022-10-26 16:26:22 -0400

    new Proposal Modal

---
+++ packages/prop-house-webapp/src/components/ProposalHeader/index.tsx
@@ -0,0 +1,81 @@
+import classes from './ProposalHeader.module.css';
+import EthAddress from '../EthAddress';
+import { ImArrowLeft2, ImArrowRight2 } from 'react-icons/im';
+import { Direction } from '@nouns/prop-house-wrapper/dist/builders';
+// import { useTranslation } from 'react-i18next';
+
+export interface ProposalHeaderProps {
+  fieldTitle: string;
+  address: string;
+  proposalId: number;
+  backButton: React.ReactNode;
+  propIndex: number | undefined;
+  numberOfProps: number;
+  handleDirectionalArrowClick: (e: any) => void;
+}
+
+const ProposalHeader: React.FC<ProposalHeaderProps> = props => {
+  const {
+    backButton,
+    fieldTitle,
+    address,
+    proposalId,
+    propIndex,
+    numberOfProps,
+    handleDirectionalArrowClick,
+  } = props;
+
+  // const { t } = useTranslation();
+
+  return (
+    <div className={classes.headerContainer}>
+      <div className={classes.headerContainer}>
+        <div className={classes.headerPropInfo}>
+          {address && proposalId && (
+            <div className={classes.subinfo}>
+              <div className={classes.communityAndPropNumber}>
+                <span className={classes.propNumber}>
+                  Prop {propIndex} of {numberOfProps}
+                </span>{' '}
+                <span className={classes.creditDash}>—</span>
+                <span className={classes.propCredit}>#{proposalId} by</span>
+                <div className={classes.submittedBy}>
+                  <EthAddress
+                    address={address}
+                    hideDavatar={true}
+                    className={classes.submittedBy}
+                  />
+                </div>
+              </div>
+            </div>
+          )}
+
+          <p className={classes.propTitle}>{fieldTitle}</p>
+        </div>
+
+        <div className={classes.btnContainer}>
+          <div className={classes.propNavigationButtons}>
+            <button
+              className={classes.backToAuction}
+              disabled={propIndex === 1}
+              onClick={() => handleDirectionalArrowClick(Direction.Down)}
+            >
+              <ImArrowLeft2 size={'1.5rem'} />
+            </button>
+
+            <button
+              className={classes.backToAuction}
+              onClick={() => handleDirectionalArrowClick(Direction.Up)}
+              disabled={propIndex === numberOfProps}
+            >
+              <ImArrowRight2 size={'1.5rem'} />
+            </button>
+          </div>
+          {backButton && backButton}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProposalHeader;
--- packages/prop-house-webapp/src/components/pages/Proposal/index.tsx
+++ packages/prop-house-webapp/src/components/pages/Proposal/index.tsx
@@ -1,6 +1,6 @@
 import classes from './Proposal.module.css';
 import { useParams } from 'react-router';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { useAppSelector } from '../../../hooks';
 import NotFound from '../../NotFound';
 import { useEffect, useRef, useState } from 'react';
@@ -10,34 +10,44 @@ import { useDispatch } from 'react-redux';
 import {
   setActiveCommunity,
   setActiveProposal,
+  setActiveProposals,
   setActiveRound,
 } from '../../../state/slices/propHouse';
-import RenderedProposalFields from '../../RenderedProposalFields';
+import ProposalContent from '../../ProposalContent';
 import proposalFields from '../../../utils/proposalFields';
-import { IoArrowBackCircleOutline } from 'react-icons/io5';
+import { IoClose } from 'react-icons/io5';
 import LoadingIndicator from '../../LoadingIndicator';
-import { StoredProposalWithVotes } from '@nouns/prop-house-wrapper/dist/builders';
-import { Container } from 'react-bootstrap';
+import { Direction, StoredProposalWithVotes } from '@nouns/prop-house-wrapper/dist/builders';
+import { Col, Container, Row } from 'react-bootstrap';
 import { buildRoundPath } from '../../../utils/buildRoundPath';
 import { cardServiceUrl, CardType } from '../../../utils/cardServiceUrl';
 import OpenGraphElements from '../../OpenGraphElements';
+import ProposalHeader from '../../ProposalHeader';
+import Divider from '../../Divider';
+// import { AuctionStatus, auctionStatus } from '../../../utils/auctionStatus';
+// import { dispatchSortProposals, SortType } from '../../../utils/sortingProposals';
 
 const Proposal = () => {
   const params = useParams();
   const { id } = params;
   const { library: provider } = useEthers();
   const navigate = useNavigate();
+  const { pathname } = useLocation();
 
   const [failedFetch, setFailedFetch] = useState(false);
+  const [currentProposal, setCurrentProposal] = useState<StoredProposalWithVotes | any>();
 
   const dispatch = useDispatch();
   const proposal = useAppSelector(state => state.propHouse.activeProposal);
+  const proposals = useAppSelector(state => state.propHouse.activeProposals);
+  const [currentPropIndex, setCurrentPropIndex] = useState<number | any>();
+
   const community = useAppSelector(state => state.propHouse.activeCommunity);
   const round = useAppSelector(state => state.propHouse.activeRound);
   const backendHost = useAppSelector(state => state.configuration.backendHost);
   const backendClient = useRef(new PropHouseWrapper(backendHost, provider?.getSigner()));
 
-  const handleBackClick = () => {
+  const handleClosePropModal = () => {
     if (!community || !round) return;
     navigate(buildRoundPath(community, round), { replace: false });
   };
@@ -56,6 +66,7 @@ const Proposal = () => {
           Number(id),
         )) as StoredProposalWithVotes;
         document.title = `${proposal.title}`;
+        setCurrentProposal(proposal);
         dispatch(setActiveProposal(proposal));
       } catch (e) {
         setFailedFetch(true);
@@ -85,6 +96,43 @@ const Proposal = () => {
     fetchCommunity();
   }, [id, dispatch, proposal]);
 
+  // fetch proposals
+  useEffect(() => {
+    if (!round) return;
+
+    const fetchAuctionProposals = async () => {
+      const proposals = await backendClient.current.getAuctionProposals(round.id);
+      dispatch(setActiveProposals(proposals));
+
+      // dispatchSortProposals(
+      //   dispatch,
+      //   auctionStatus(round) === AuctionStatus.AuctionEnded
+      //     ? SortType.VoteCount
+      //     : SortType.CreatedAt,
+      //   false,
+      // );
+
+      currentProposal &&
+        setCurrentPropIndex(
+          proposals.findIndex((p: StoredProposalWithVotes) => p.id === currentProposal.id) + 1,
+        );
+    };
+
+    fetchAuctionProposals();
+    return () => {
+      dispatch(setActiveProposals([]));
+    };
+  }, [currentProposal, dispatch, round]);
+
+  const handleDirectionalArrowClick = (direction: Direction) => {
+    if (!community || !round) return;
+
+    currentPropIndex &&
+      proposals &&
+      proposal &&
+      navigate(`${pathname.slice(0, -2)}${proposals[currentPropIndex].id}`);
+  };
+
   return (
     <>
       <Container>
@@ -95,20 +143,32 @@ const Proposal = () => {
             imageUrl={cardServiceUrl(CardType.proposal, proposal.id).href}
           />
         )}
-        {proposal ? (
+
+        {currentProposal ? (
           <>
-            <RenderedProposalFields
-              fields={proposalFields(proposal)}
-              address={proposal.address}
-              proposalId={proposal.id}
-              community={community}
-              roundName={round && round?.title}
-              backButton={
-                <div className={classes.backToAuction} onClick={() => handleBackClick()}>
-                  <IoArrowBackCircleOutline size={'1.5rem'} />
-                </div>
-              }
-            />
+            <Row>
+              <Col xl={12}>
+                {proposals && (
+                  <ProposalHeader
+                    backButton={
+                      <div className={classes.backToAuction} onClick={() => handleClosePropModal()}>
+                        <IoClose size={'1.5rem'} />
+                      </div>
+                    }
+                    fieldTitle={proposalFields(currentProposal).title}
+                    address={currentProposal.address}
+                    proposalId={currentProposal.id}
+                    propIndex={currentPropIndex}
+                    numberOfProps={proposals.length}
+                    handleDirectionalArrowClick={handleDirectionalArrowClick}
+                  />
+                )}
+
+                <Divider />
+
+                <ProposalContent fields={proposalFields(currentProposal)} />
+              </Col>
+            </Row>
           </>
         ) : failedFetch ? (
           <NotFound />
