49ccec1643cbdb141a4367ef693c51881c2621f0
christianturner <christianturner@hey.com>
2022-10-28 17:05:16 -0400

    rm duplicate div

--- packages/prop-house-webapp/src/components/ProposalHeader/index.tsx
+++ packages/prop-house-webapp/src/components/ProposalHeader/index.tsx
@@ -29,50 +29,44 @@ const ProposalHeader: React.FC<ProposalHeaderProps> = props => {
 
   return (
     <div className={classes.headerContainer}>
-      <div className={classes.headerContainer}>
-        <div className={classes.headerPropInfo}>
-          {address && proposalId && (
-            <div className={classes.subinfo}>
-              <div className={classes.communityAndPropNumber}>
-                <span className={classes.propNumber}>
-                  Prop {propIndex} of {numberOfProps}
-                </span>{' '}
-                <span className={classes.creditDash}>—</span>
-                <span className={classes.propCredit}>#{proposalId} by</span>
-                <div className={classes.submittedBy}>
-                  <EthAddress
-                    address={address}
-                    hideDavatar={true}
-                    className={classes.submittedBy}
-                  />
-                </div>
+      <div className={classes.headerPropInfo}>
+        {address && proposalId && (
+          <div className={classes.subinfo}>
+            <div className={classes.communityAndPropNumber}>
+              <span className={classes.propNumber}>
+                Prop {propIndex} of {numberOfProps}
+              </span>{' '}
+              <span className={classes.creditDash}>—</span>
+              <span className={classes.propCredit}>#{proposalId} by</span>
+              <div className={classes.submittedBy}>
+                <EthAddress address={address} hideDavatar={true} className={classes.submittedBy} />
               </div>
             </div>
-          )}
-
-          <p className={classes.propTitle}>{fieldTitle}</p>
-        </div>
-
-        <div className={classes.btnContainer}>
-          <div className={classes.propNavigationButtons}>
-            <button
-              className={classes.backToAuction}
-              disabled={propIndex === 1}
-              onClick={() => handleDirectionalArrowClick(Direction.Down)}
-            >
-              <ImArrowLeft2 size={'1.5rem'} />
-            </button>
-
-            <button
-              className={classes.backToAuction}
-              onClick={() => handleDirectionalArrowClick(Direction.Up)}
-              disabled={propIndex === numberOfProps}
-            >
-              <ImArrowRight2 size={'1.5rem'} />
-            </button>
           </div>
-          {backButton && backButton}
+        )}
+
+        <p className={classes.propTitle}>{fieldTitle}</p>
+      </div>
+
+      <div className={classes.btnContainer}>
+        <div className={classes.propNavigationButtons}>
+          <button
+            className={classes.backToAuction}
+            disabled={propIndex === 1}
+            onClick={() => handleDirectionalArrowClick(Direction.Down)}
+          >
+            <ImArrowLeft2 size={'1.5rem'} />
+          </button>
+
+          <button
+            className={classes.backToAuction}
+            onClick={() => handleDirectionalArrowClick(Direction.Up)}
+            disabled={propIndex === numberOfProps}
+          >
+            <ImArrowRight2 size={'1.5rem'} />
+          </button>
         </div>
+        {backButton && backButton}
       </div>
     </div>
   );
