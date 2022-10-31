add121d510b0cc19f7df8424af05c29f82e233f2
christianturner <christianturner@hey.com>
2022-10-31 15:39:27 -0400

    active step logic

--- packages/prop-house-webapp/src/components/ManagerSecondaryCard/index.tsx
+++ packages/prop-house-webapp/src/components/ManagerSecondaryCard/index.tsx
@@ -1,9 +1,14 @@
+import { Dispatch, SetStateAction } from 'react';
 import Button, { ButtonColor } from '../Button';
 import Card, { CardBgColor, CardBorderRadius } from '../Card';
 import ManagerInstruction from '../ManagerInstruction';
 import classes from './ManagerSecondaryCard.module.css';
 
-const ManagerSecondaryCard = () => {
+const ManagerSecondaryCard: React.FC<{
+  activeStep: number;
+  setActiveStep: Dispatch<SetStateAction<number>>;
+}> = props => {
+  const { activeStep, setActiveStep } = props;
   return (
     <>
       <Card
@@ -16,17 +21,19 @@ const ManagerSecondaryCard = () => {
         <hr className={classes.divider} />
 
         <ManagerInstruction
+          activeStep={activeStep}
           number={1}
           title="Create a House"
           instruction="Define the parameters for your your contract. These includes title, image, description
             and the voting strategies."
           button={
             <Button
+              disabled={activeStep !== 1}
               text="Deploy contract"
               bgColor={ButtonColor.Pink}
-              // onClick={() => {
-              //   setShowErrorModal(false);
-              // }}
+              onClick={() => {
+                setActiveStep(2);
+              }}
             />
           }
         />
@@ -34,6 +41,7 @@ const ManagerSecondaryCard = () => {
         <hr className={classes.divider} />
 
         <ManagerInstruction
+          activeStep={activeStep}
           number={2}
           title="Deposit funds"
           instruction="Approve and deposit the funds used to reward Round winners. Rewards can come in the form of ETH, ERC20s, ERC721s, or ERC1155s."
@@ -43,6 +51,7 @@ const ManagerSecondaryCard = () => {
         <hr className={classes.divider} />
 
         <ManagerInstruction
+          activeStep={activeStep}
           number={3}
           title="Initiate a round"
           instruction="Define the name, description, awards types and other parameters to kick off your first Round."
