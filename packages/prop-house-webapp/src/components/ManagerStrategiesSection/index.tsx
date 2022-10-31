fab9d88cd6cfd306686449f7f764cb3eed26df97
christianturner <christianturner@hey.com>
2022-10-31 09:35:01 -0400

    add strategies section for manager UI

--- packages/prop-house-webapp/src/components/ManagerStrategiesSection/index.tsx
+++ packages/prop-house-webapp/src/components/ManagerStrategiesSection/index.tsx
@@ -1,3 +1,4 @@
+import Button, { ButtonColor } from '../Button';
 import classes from './ManagerStrategiesSection.module.css';
 
 const ManagerStrategiesSection = () => {
@@ -10,6 +11,72 @@ const ManagerStrategiesSection = () => {
           rounds created for your House. You can decide on which strategy to use when creating each
           Round.
         </p>
+
+        <div className={classes.strategy}>
+          <input type="checkbox" />
+
+          <div className={classes.sections}>
+            <div className={classes.section}>
+              <p className={classes.title}>Token balance</p>
+              <p className={classes.subtitle}>One vote per each ERC20/ERC721 held.</p>
+            </div>
+
+            <div className={classes.section}>
+              <p className={classes.title}>Token contract address</p>
+              <input placeholder="0x830BD73E4184ceF73443C15111a1DF14e495C706" type="text" />
+            </div>
+          </div>
+        </div>
+
+        <hr className={classes.divider} />
+
+        <div className={classes.strategy}>
+          <input type="checkbox" />
+
+          <div className={classes.sections}>
+            <div className={classes.section}>
+              <p className={classes.title}>Token Balance with a Multiplier</p>
+              <p className={classes.subtitle}>
+                Choose how many votes are allotted for each ERC20/ERC721 held.
+              </p>
+            </div>
+
+            <div className={classes.section}>
+              <p className={classes.title}>Token contract address</p>
+              <input placeholder="0x830BD73E4184ceF73443C15111a1DF14e495C706" type="text" />
+            </div>
+
+            <div className={classes.section}>
+              <p className={classes.title}>Token Multiplier</p>
+              <p className={classes.subtitle}>
+                Each ERC20/ERC721 will get{' '}
+                <span className={classes.multiplierAmount}>10 votes</span>.
+              </p>
+
+              <div className={classes.multiplierInputAndBtns}>
+                <input type="checkbox" />
+                <div className={classes.multiplierBtns}>
+                  <Button
+                    text="↓"
+                    // bgColor={isAllotting() ? ButtonColor.PurpleLight : ButtonColor.Gray}
+                    bgColor={ButtonColor.Gray}
+                    classNames={classes.voteBtn}
+                    // onClick={e => handleClickVote(e, Direction.Down)}
+                    // disabled={}
+                  />
+                  <Button
+                    text="↑"
+                    // bgColor={isAllotting() ? ButtonColor.PurpleLight : ButtonColor.Gray}
+                    bgColor={ButtonColor.Gray}
+                    classNames={classes.voteBtn}
+                    // onClick={e => handleClickVote(e, Direction.Up)}
+                    // disabled={}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
