6e1450d96ab4ed4c6c7ab8764247763276a5699b
christianturner <christianturner@hey.com>
2022-10-31 15:39:27 -0400

    active step logic

--- packages/prop-house-webapp/src/components/ManagerInstruction/index.tsx
+++ packages/prop-house-webapp/src/components/ManagerInstruction/index.tsx
@@ -1,20 +1,33 @@
+import clsx from 'clsx';
 import React from 'react';
 import classes from './ManagerInstruction.module.css';
+import { FaCheck as CheckIcon } from 'react-icons/fa';
 
 const ManagerInstruction: React.FC<{
+  activeStep: number;
   number: number;
   title: string;
   instruction: string;
   optional?: boolean;
   button?: React.ReactNode;
 }> = props => {
-  const { number, title, instruction, optional, button } = props;
+  const { activeStep, number, title, instruction, optional, button } = props;
+
+  const completedStep = () => activeStep > number;
 
   return (
     <>
       <div className={classes.instructionSection}>
         <div className={classes.instructionTitle}>
-          <span className={classes.number}>{number}</span>
+          <span className={clsx(classes.number, completedStep() && classes.pinkBg)}>
+            {completedStep() ? (
+              <span className={classes.check}>
+                <CheckIcon />
+              </span>
+            ) : (
+              number
+            )}
+          </span>
 
           <div className={classes.titleAndOptional}>
             {optional && <span className={classes.optional}>Optional</span>}
@@ -22,9 +35,13 @@ const ManagerInstruction: React.FC<{
           </div>
         </div>
 
-        <p className={classes.instruction}>{instruction}</p>
+        {!completedStep() && (
+          <>
+            <p className={classes.instruction}>{instruction}</p>
 
-        {button && button}
+            {button && button}
+          </>
+        )}
       </div>
     </>
   );
