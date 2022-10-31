58c67d77542708aec4c8d72ed5e4747c29ceae61
christianturner <christianturner@hey.com>
2022-10-30 23:18:56 -0400

    split out manager card sections into components

---
+++ packages/prop-house-webapp/src/components/ManagerNamingSection/index.tsx
@@ -0,0 +1,34 @@
+import classes from './ManagerNamingSection.module.css';
+
+const ManagerNamingSection = () => {
+  return (
+    <>
+      <p className={classes.title}>Create and deploy the contract for your House</p>
+
+      <hr className={classes.divider} />
+
+      <div className={classes.nameHouseSection}>
+        <div className={classes.imgContainer}>
+          <img src="tricolor.png" alt="drop-img-here" />
+        </div>
+
+        <div className={classes.inputs}>
+          <div className={classes.titleAndInput}>
+            <p className={classes.subtitle}>Name your House</p>
+            <input placeholder="ex. Nouns" />
+          </div>
+
+          <div className={classes.titleAndInput}>
+            <p className={classes.subtitle}>Add an image</p>
+            <input placeholder="ex. https://example.com/nouns.jpg" />
+            <span className={classes.inputNote}>
+              Enter a url for a hosted image to represents your House.
+            </span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ManagerNamingSection;
