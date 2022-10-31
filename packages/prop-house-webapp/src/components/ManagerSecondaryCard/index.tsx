1291704492300db1d70e01ab1b60737396fd5cd3
christianturner <christianturner@hey.com>
2022-10-31 10:41:45 -0400

    break out manager's secondary card instructions to component

--- packages/prop-house-webapp/src/components/ManagerSecondaryCard/index.tsx
+++ packages/prop-house-webapp/src/components/ManagerSecondaryCard/index.tsx
@@ -1,4 +1,6 @@
+import Button, { ButtonColor } from '../Button';
 import Card, { CardBgColor, CardBorderRadius } from '../Card';
+import ManagerInstruction from '../ManagerInstruction';
 import classes from './ManagerSecondaryCard.module.css';
 
 const ManagerSecondaryCard = () => {
@@ -9,7 +11,43 @@ const ManagerSecondaryCard = () => {
         borderRadius={CardBorderRadius.thirty}
         classNames={classes.secondaryCard}
       >
-        <p>Create your House</p>
+        <p className={classes.title}>Create your House</p>
+
+        <hr className={classes.divider} />
+
+        <ManagerInstruction
+          number={1}
+          title="Create a House"
+          instruction="Define the parameters for your your contract. These includes title, image, description
+            and the voting strategies."
+          button={
+            <Button
+              text="Deploy contract"
+              bgColor={ButtonColor.Pink}
+              // onClick={() => {
+              //   setShowErrorModal(false);
+              // }}
+            />
+          }
+        />
+
+        <hr className={classes.divider} />
+
+        <ManagerInstruction
+          number={2}
+          title="Deposit funds"
+          instruction="Approve and deposit the funds used to reward Round winners. Rewards can come in the form of ETH, ERC20s, ERC721s, or ERC1155s."
+          optional
+        />
+
+        <hr className={classes.divider} />
+
+        <ManagerInstruction
+          number={3}
+          title="Initiate a round"
+          instruction="Define the name, description, awards types and other parameters to kick off your first Round."
+          optional
+        />
       </Card>
     </>
   );
