3a05af260f33622097dbd18e9ad2a93108f116ce
christianturner <christianturner@hey.com>
2022-10-31 23:58:53 -0400

    UI components for step 3

---
+++ packages/prop-house-webapp/src/components/SetupRoundStep/index.tsx
@@ -0,0 +1,15 @@
+import Divider from '../Divider';
+import SetupRoundDetails from '../SetupRoundDetails';
+import SetupRoundInfo from '../SetupRoundInfo';
+
+const SetupRoundStep = () => {
+  return (
+    <>
+      <SetupRoundInfo />
+      <Divider />
+      <SetupRoundDetails />
+    </>
+  );
+};
+
+export default SetupRoundStep;
