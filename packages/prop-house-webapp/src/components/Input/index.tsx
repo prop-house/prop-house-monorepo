29bfbaaa80bc53900bb1ad00af0de5000db8ea22
christianturner <christianturner@hey.com>
2022-11-01 06:20:40 -0400

    support vertical & horizontal layout inputs

--- packages/prop-house-webapp/src/components/Input/index.tsx
+++ packages/prop-house-webapp/src/components/Input/index.tsx
@@ -1,16 +1,35 @@
+import clsx from 'clsx';
+import { Dispatch, SetStateAction } from 'react';
 import classes from './Input.module.css';
 
 const Input: React.FC<{
   title: string;
+  type: 'text' | 'number';
   placeholder: string;
+  value: string | number;
+  row?: boolean;
+  onChange: Dispatch<SetStateAction<any>>;
+  classNames?: string;
   note?: string;
 }> = props => {
-  const { title, placeholder, note } = props;
+  const { title, type, placeholder, value, row, onChange, classNames, note } = props;
 
   return (
-    <div className={classes.inputContainer}>
+    <div
+      className={clsx(
+        classes.inputContainer,
+        row && classes.rowLayout,
+        type === 'number' && classes.numberInput,
+      )}
+    >
       <p className={classes.inputTitle}>{title}</p>
-      <input placeholder={placeholder} />
+      <input
+        className={classNames}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
       {note && <span className={classes.inputNote}>{note}</span>}
     </div>
   );
