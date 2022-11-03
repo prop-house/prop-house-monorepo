bc2622547f3bb18a3596a1df8950e37c0c0c8ffd
christianturner <christianturner@hey.com>
2022-11-03 13:11:54 -0400

    move conditional checks to parent component

--- packages/prop-house-webapp/src/components/ProposalFooter/index.tsx
+++ packages/prop-house-webapp/src/components/ProposalFooter/index.tsx
@@ -1,6 +1,5 @@
 import { ProgressBar } from 'react-bootstrap';
 import classes from './ProposalFooter.module.css';
-import { useEthers } from '@usedapp/core';
 import clsx from 'clsx';
 import PropCardVotingModule from '../PropCardVotingModule';
 import { StoredAuction, StoredProposalWithVotes } from '@nouns/prop-house-wrapper/dist/builders';
@@ -11,8 +10,6 @@ import TruncateThousands from '../TruncateThousands';
 import { VoteAllotment } from '../../types/VoteAllotment';
 import { voteWeightForAllottedVotes } from '../../utils/voteWeightForAllottedVotes';
 import { Dispatch, SetStateAction } from 'react';
-
-export interface ProposalFooterProps {}
 
 const ProposalFooter: React.FC<{
   round: StoredAuction;
@@ -34,59 +31,51 @@ const ProposalFooter: React.FC<{
     setShowVotingModal,
   } = props;
 
-  const { account } = useEthers();
-
   return (
     <>
       <div className={classes.footerContainer}>
-        {account && votingPower > 0 && (
-          <div className={classes.votingContainer}>
-            <div className={classes.votingProgressBar}>
-              <div className={classes.votingInfo}>
-                <span>Cast your votes</span>
+        <div className={classes.votingContainer}>
+          <div className={classes.votingProgressBar}>
+            <div className={classes.votingInfo}>
+              <span>Cast your votes</span>
 
-                <span className={classes.totalVotes}>{`${
-                  votesLeftToAllot > 0
-                    ? `${votingPower - submittedVotes - numAllotedVotes} left`
-                    : 'no votes left'
-                }`}</span>
-              </div>
-
-              <ProgressBar
-                className={clsx(
-                  classes.votingBar,
-                  submittedVotes > 0 && votingPower !== submittedVotes && 'roundAllotmentBar',
-                )}
-              >
-                <ProgressBar variant="success" now={(submittedVotes / votingPower) * 100} />
-
-                <ProgressBar
-                  variant="warning"
-                  now={(numAllotedVotes / votingPower) * 100}
-                  key={2}
-                />
-              </ProgressBar>
+              <span className={classes.totalVotes}>{`${
+                votesLeftToAllot > 0
+                  ? `${votingPower - submittedVotes - numAllotedVotes} left`
+                  : 'no votes left'
+              }`}</span>
             </div>
 
-            <div className={classes.voteAllotmentSection}>
-              <div className={classes.icon}>
-                <VoteIcon /> <TruncateThousands amount={proposal.voteCount} /> <span>+</span>
-              </div>
+            <ProgressBar
+              className={clsx(
+                classes.votingBar,
+                submittedVotes > 0 && votingPower !== submittedVotes && 'roundAllotmentBar',
+              )}
+            >
+              <ProgressBar variant="success" now={(submittedVotes / votingPower) * 100} />
 
-              <PropCardVotingModule proposal={proposal} cardStatus={ProposalCardStatus.Voting} />
-
-              <Button
-                classNames={classes.submitVotesButton}
-                text={'Submit votes'}
-                bgColor={ButtonColor.Purple}
-                disabled={
-                  voteWeightForAllottedVotes(voteAllotments) === 0 || submittedVotes === votingPower
-                }
-                onClick={() => setShowVotingModal(true)}
-              />
-            </div>
+              <ProgressBar variant="warning" now={(numAllotedVotes / votingPower) * 100} key={2} />
+            </ProgressBar>
           </div>
-        )}
+
+          <div className={classes.voteAllotmentSection}>
+            <div className={classes.icon}>
+              <VoteIcon /> <TruncateThousands amount={proposal.voteCount} /> <span>+</span>
+            </div>
+
+            <PropCardVotingModule proposal={proposal} cardStatus={ProposalCardStatus.Voting} />
+
+            <Button
+              classNames={classes.submitVotesButton}
+              text={'Submit votes'}
+              bgColor={ButtonColor.Purple}
+              disabled={
+                voteWeightForAllottedVotes(voteAllotments) === 0 || submittedVotes === votingPower
+              }
+              onClick={() => setShowVotingModal(true)}
+            />
+          </div>
+        </div>
       </div>
     </>
   );
