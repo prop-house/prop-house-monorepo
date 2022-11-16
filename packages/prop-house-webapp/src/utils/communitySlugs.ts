1b1c8ddcd1e56c6c342c71e0d63608af337b7e37
christianturner <christianturner@hey.com>
2022-11-16 16:45:46 -0500

    use slugify to account for all other special characters

--- packages/prop-house-webapp/src/utils/communitySlugs.ts
+++ packages/prop-house-webapp/src/utils/communitySlugs.ts
@@ -1,4 +1,5 @@
-export const nameToSlug = (name: string) =>
-  name.replaceAll(':', '').replaceAll(' ', '-').toLowerCase();
+import slugify from 'slugify';
+
+export const nameToSlug = (name: string) => slugify(name, { lower: true, strict: true });
 
 export const slugToName = (slug: string) => slug.replaceAll('-', ' ');
