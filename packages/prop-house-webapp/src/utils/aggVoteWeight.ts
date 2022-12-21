f766177428cfad655824814a833950b48dd532f9
solimander <solimander-dev@protonmail.com>
2022-12-21 12:33:53 -0700

    Only include validated votes in remaining vote calcs

--- packages/prop-house-webapp/src/utils/aggVoteWeight.ts
+++ packages/prop-house-webapp/src/utils/aggVoteWeight.ts
@@ -1,12 +1,18 @@
-import { StoredProposalWithVotes, StoredVote } from '@nouns/prop-house-wrapper/dist/builders';
+import { SignatureState, StoredProposalWithVotes, StoredVote } from '@nouns/prop-house-wrapper/dist/builders';
 import extractAllVotes from './extractAllVotes';
 
 /**
- * Calculates aggregate vote weight for votes extracted from a set of proposals
+ * Calculates aggregate validated vote weight for votes extracted from a set of proposals
  */
-export const aggVoteWeightForProps = (proposals: StoredProposalWithVotes[], address: string) =>
+export const aggValidatedVoteWeightForProps = (proposals: StoredProposalWithVotes[], address: string) =>
   extractAllVotes(proposals, address).reduce(
-    (agg, current) => Number(agg) + Number(current.weight),
+    (agg, current) => {
+      // Only validated votes should count towards remaining vote weight
+      if (current.signatureState !== SignatureState.VALIDATED) {
+        return agg;
+      }
+      return Number(agg) + Number(current.weight);
+    },
     0,
   );
 
