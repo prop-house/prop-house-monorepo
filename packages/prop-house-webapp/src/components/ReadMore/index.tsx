a7a81bbe8c6349da45af3dfc9a1be03f29367f7d
christianturner <christianturner@hey.com>
2023-01-06 14:02:28 -0500

    conditionally show read more button when 4 lines or more

--- packages/prop-house-webapp/src/components/ReadMore/index.tsx
+++ packages/prop-house-webapp/src/components/ReadMore/index.tsx
@@ -1,19 +1,36 @@
 import classes from './ReadMore.module.css';
-import { useState } from 'react';
+import { useLayoutEffect, useRef, useState } from 'react';
 
 const ReadMore: React.FC<{
   description: JSX.Element;
 }> = props => {
   const { description } = props;
 
-  const [readMore, setReadMore] = useState(false);
+  const ref = useRef(null);
+  const [open, setOpen] = useState(false);
+  const [showLink, setShowLink] = useState(false);
+
+  useLayoutEffect(() => {
+    // @ts-ignore
+    if (ref.current && ref.current.clientHeight < ref.current.scrollHeight) {
+      setShowLink(true)
+    }
+  }, [ref])
+
+  let classNames = classes.readMoreText;
+  if (open) {
+    classNames += classes.clampLine;
+  }
+
   return (
     <div className={classes.readMoreContainer}>
-      <div className={readMore ? '' : classes.clampLine}>
-        {description}
-      </div>
+      <div className={classNames} ref={ref}> {description} </div>
 
-      <div className={classes.readMoreLessButton} onClick={() => setReadMore(!readMore)}>{readMore ? 'Read less' : 'Read more'}</div>
+      {showLink &&
+        <div className={classes.readMoreLessButton} onClick={() => setOpen(!open)} >
+          {open ? 'Read less' : 'Read more'}
+        </div>
+      }
     </div>
 
   );
