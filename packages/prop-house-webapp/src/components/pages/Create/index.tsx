92398101bc7558701fff4540788a555861c688a3
christianturner <christianturner@hey.com>
2023-01-06 10:34:27 -0500

    new proposal success modal with twitter sharing

--- packages/prop-house-webapp/src/components/pages/Create/index.tsx
+++ packages/prop-house-webapp/src/components/pages/Create/index.tsx
@@ -1,7 +1,7 @@
 import classes from './Create.module.css';
 import { Row, Col, Container } from 'react-bootstrap';
 import Button, { ButtonColor } from '../../Button';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { useEffect, useState, useRef } from 'react';
 import ProposalEditor from '../../ProposalEditor';
 import Preview from '../Preview';
@@ -14,13 +14,12 @@ import { PropHouseWrapper } from '@nouns/prop-house-wrapper';
 import isAuctionActive from '../../../utils/isAuctionActive';
 import { ProposalFields } from '../../../utils/proposalFields';
 import useWeb3Modal from '../../../hooks/useWeb3Modal';
-import Modal from '../../Modal';
 import removeTags from '../../../utils/removeTags';
 import { useTranslation } from 'react-i18next';
 import FundingAmount from '../../FundingAmount';
-import { nameToSlug } from '../../../utils/communitySlugs';
 import LoadingIndicator from '../../LoadingIndicator';
 import Divider from '../../Divider';
+import ProposalSuccessModal from '../../ProposalSuccessModal';
 
 const isValidPropData = (data: ProposalFields) =>
   data.title.length > 4 &&
@@ -38,11 +37,11 @@ const Create: React.FC<{}> = () => {
   const activeCommunity = location.state.community;
 
   const [showPreview, setShowPreview] = useState(false);
-  const [showModal, setShowModal] = useState(false);
+  const [propId, setPropId] = useState<null | number>(null);
+  const [showProposalSuccessModal, setShowProposalSuccessModal] = useState(false);
 
   const proposalEditorData = useAppSelector(state => state.editor.proposal);
   const dispatch = useAppDispatch();
-  const navigate = useNavigate();
   const connect = useWeb3Modal();
 
   const backendHost = useAppSelector(state => state.configuration.backendHost);
@@ -68,48 +67,26 @@ const Create: React.FC<{}> = () => {
         activeAuction.id,
       ),
     );
+
+    setPropId(proposal.id);
     dispatch(appendProposal({ proposal }));
     dispatch(clearProposal());
-    setShowModal(true);
-  };
-  const successfulSubmissionModalContent = {
-    title: t('congrats'),
-    content: (
-      <>
-        <p>
-          {`
-          ${t(`successfulSubmission`)} \n
-          ${activeCommunity && activeCommunity.name} ${`(${
-            activeAuction && activeAuction.title
-          })`}`}
-        </p>
-        <Button
-          text={t('viewRound')}
-          bgColor={ButtonColor.White}
-          onClick={() =>
-            navigate(
-              `/${activeCommunity && nameToSlug(activeCommunity.name)}/${nameToSlug(
-                activeAuction.title,
-              )}`,
-            )
-          }
-        />
-      </>
-    ),
-    onDismiss: () =>
-      navigate(
-        `/${activeCommunity && nameToSlug(activeCommunity.name)}/${nameToSlug(
-          activeAuction.title,
-        )}`,
-      ),
+    setShowProposalSuccessModal(true);
   };
 
   return (
     <Container>
       {activeAuction ? (
         <>
-          {showModal && <Modal data={successfulSubmissionModalContent} />}
-
+          {showProposalSuccessModal && propId && (
+            <ProposalSuccessModal
+              showModal={showProposalSuccessModal}
+              setShowModal={setShowProposalSuccessModal}
+              proposalId={propId}
+              house={activeCommunity.name}
+              round={activeAuction.title}
+            />
+          )}
           <Row>
             <Col xl={12} className={classes.proposalHelperWrapper}>
               <h1 className={classes.proposalHelper}>
