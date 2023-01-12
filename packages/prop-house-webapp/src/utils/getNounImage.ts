5d1692798f5ef9bbd885c460b64f7271422e7cc9
christianturner <christianturner@hey.com>
2023-01-11 22:11:19 -0500

    noun image util func

---
+++ packages/prop-house-webapp/src/utils/getNounImage.ts
@@ -0,0 +1,14 @@
+export const NounImage = {
+  Banana: { src: '/heads/banana.png', alt: 'banana' },
+  Blackhole: { src: '/heads/blackhole.png', alt: 'blackhole' },
+  Calculator: { src: '/heads/calculator.png', alt: 'calculator' },
+  Chart: { src: '/heads/chart.png', alt: 'chart' },
+  Computer: { src: '/heads/computer.png', alt: 'computer' },
+  Glasses: { src: '/heads/glasses.png', alt: 'glasses' },
+  Heart: { src: '/heads/laptop.png', alt: 'laptop' },
+  Laptop: { src: '/heads/laptop.png', alt: 'laptop' },
+  Thumbsup: { src: '/heads/thumbsup.png', alt: 'thumbsup' },
+  Trashcan: { src: '/heads/trashcan.png', alt: 'trashcan' },
+  Wallet: { src: '/heads/wallet.png', alt: 'wallet' },
+  Whale: { src: '/heads/whale.png', alt: 'whale' },
+};
