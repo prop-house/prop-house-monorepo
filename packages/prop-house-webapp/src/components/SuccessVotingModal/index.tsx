d81fff8264de82d9e9115e2364bcd6d9a5665e33
christianturner <christianturner@hey.com>
2023-01-12 14:22:06 -0500

    Use new modal: SuccessVotingModal

--- packages/prop-house-webapp/src/components/SuccessVotingModal/index.tsx
+++ packages/prop-house-webapp/src/components/SuccessVotingModal/index.tsx
@@ -1,46 +1,34 @@
 import React, { Dispatch, SetStateAction } from 'react';
-import classes from './SuccessVotingModal.module.css';
-import clsx from 'clsx';
-import Modal from 'react-modal';
 import Button, { ButtonColor } from '../Button';
 import { useTranslation } from 'react-i18next';
+import Modal from '../Modal';
+import { NounImage } from '../../utils/getNounImage';
 
 const SuccessVotingModal: React.FC<{
   showSuccessVotingModal: boolean;
-  numPropsVotedFor: number;
   setShowSuccessVotingModal: Dispatch<SetStateAction<boolean>>;
+  numPropsVotedFor: number;
   signerIsContract: boolean;
 }> = props => {
-  const { showSuccessVotingModal, setShowSuccessVotingModal, numPropsVotedFor, signerIsContract } = props;
+  const { showSuccessVotingModal, setShowSuccessVotingModal, numPropsVotedFor, signerIsContract } =
+    props;
   const { t } = useTranslation();
 
-  const eoaSignerMsg = `${t('youveSuccessfullyVotedFor')} ${numPropsVotedFor} ${numPropsVotedFor === 1 ? t('prop') : t('props')
-    }!`;
-  const contractSignerMsg = `${t('youveSubmittedVotesFor')} ${numPropsVotedFor === 1 ? t('prop') : t('props')
-    }. ${t('theyWillBeCounted')}.`;
+  const eoaSignerMsg = `${t('youveSuccessfullyVotedFor')} ${numPropsVotedFor} ${
+    numPropsVotedFor === 1 ? t('prop') : t('props')
+  }!`;
+  const contractSignerMsg = `${t('youveSubmittedVotesFor')} ${
+    numPropsVotedFor === 1 ? t('prop') : t('props')
+  }. ${t('theyWillBeCounted')}.`;
 
   return (
     <Modal
-      isOpen={showSuccessVotingModal}
-      onRequestClose={() => setShowSuccessVotingModal(false)}
-      className={clsx(classes.modal)}
-    >
-      <div className={classes.container}>
-        <div className={classes.imgContainer}>
-          <img src="/rednoggles.png" alt="noggles" />
-        </div>
-
-        <div className={classes.titleContainer}>
-          <p className={classes.modalTitle}>{t('veryNounish')}</p>
-          <p className={classes.modalSubtitle}>
-            {signerIsContract ? contractSignerMsg : eoaSignerMsg}
-          </p>
-        </div>
-      </div>
-
-      <hr className={classes.divider} />
-
-      <div className={classes.buttonContainer}>
+      showModal={showSuccessVotingModal}
+      setShowModal={setShowSuccessVotingModal}
+      title={t('veryNounish')}
+      subtitle={signerIsContract ? contractSignerMsg : eoaSignerMsg}
+      image={NounImage.Glasses}
+      button={
         <Button
           text={t('close')}
           bgColor={ButtonColor.White}
@@ -48,8 +36,8 @@ const SuccessVotingModal: React.FC<{
             setShowSuccessVotingModal(false);
           }}
         />
-      </div>
-    </Modal>
+      }
+    />
   );
 };
 
