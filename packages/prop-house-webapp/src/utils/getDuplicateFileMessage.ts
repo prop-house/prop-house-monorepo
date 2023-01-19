edd6566de99af9fed9998a0d35d3da892cd8516e
christianturner <christianturner@hey.com>
2023-01-19 14:04:50 -0500

    error handling for duplicate uploads

---
+++ packages/prop-house-webapp/src/utils/getDuplicateFileMessage.ts
@@ -0,0 +1,21 @@
+/**
+ * Returns an error message to denote which file names are duplicates
+ * @param fileName Array of file name
+ * @returns String of duplicate file name
+ */
+const getDuplicateFileMessage = (fileName: string[]): string => {
+  let lastFileName;
+
+  if (fileName.length > 1) {
+    lastFileName = fileName.pop();
+    return `${fileName.map(t => t).join(', ')}${
+      lastFileName ? ` and ${lastFileName}` : ''
+    } are already in the queue`;
+  } else if (fileName.length === 1) {
+    return `${fileName[0]} is already in queue`;
+  } else {
+    return '';
+  }
+};
+
+export default getDuplicateFileMessage;
