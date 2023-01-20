82994bc77e9648d52146b021f0062306c0ac7365
christianturner <christianturner@hey.com>
2023-01-19 23:11:50 -0500

    filter out USDC from homepage stats

--- packages/prop-house-webapp/src/components/pages/Home/index.tsx
+++ packages/prop-house-webapp/src/components/pages/Home/index.tsx
@@ -46,8 +46,12 @@ const Home = () => {
       setCommunities(communities.sort((a, b) => (a.ethFunded < b.ethFunded ? 1 : -1)));
 
       const accEthFunded = communities
+        // filter out Meebits/APE
         .filter((c: any) => c.contractAddress !== '0x7Bd29408f11D2bFC23c34f18275bBf23bB716Bc7')
+        // filter out UMA
         .filter((c: any) => c.contractAddress !== '0x2381b67c6f1cb732fdf8b3b29d3260ec6f7420bc')
+        // filter out USDC
+        .filter((c: any) => c.contractAddress !== '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48')
         .reduce((prev, current) => prev + current.ethFunded, 0);
       const accRounds = communities.reduce((prev, current) => prev + current.numAuctions, 0);
       const accProps = communities.reduce((prev, current) => prev + current.numProposals, 0);
