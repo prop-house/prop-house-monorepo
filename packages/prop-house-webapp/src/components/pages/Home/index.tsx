5354d7efecff3cf81a40a466724fd27ab16399b0
christianturner <christianturner@hey.com>
2023-01-20 05:40:06 -0500

    abstract logic to helper function

--- packages/prop-house-webapp/src/components/pages/Home/index.tsx
+++ packages/prop-house-webapp/src/components/pages/Home/index.tsx
@@ -8,6 +8,7 @@ import { PropHouseWrapper } from '@nouns/prop-house-wrapper';
 import { useEthers } from '@usedapp/core';
 import { useAppSelector } from '../../../hooks';
 import NavBar from '../../NavBar';
+import filterNonEthFunds from '../../../utils/filterNonEthFunds';
 
 export interface StatsProps {
   accEthFunded: number;
@@ -45,14 +46,7 @@ const Home = () => {
 
       setCommunities(communities.sort((a, b) => (a.ethFunded < b.ethFunded ? 1 : -1)));
 
-      const accEthFunded = communities
-        // filter out Meebits/APE
-        .filter((c: any) => c.contractAddress !== '0x7Bd29408f11D2bFC23c34f18275bBf23bB716Bc7')
-        // filter out UMA
-        .filter((c: any) => c.contractAddress !== '0x2381b67c6f1cb732fdf8b3b29d3260ec6f7420bc')
-        // filter out USDC
-        .filter((c: any) => c.contractAddress !== '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48')
-        .reduce((prev, current) => prev + current.ethFunded, 0);
+      const accEthFunded = filterNonEthFunds(communities);
       const accRounds = communities.reduce((prev, current) => prev + current.numAuctions, 0);
       const accProps = communities.reduce((prev, current) => prev + current.numProposals, 0);
       setStats({
