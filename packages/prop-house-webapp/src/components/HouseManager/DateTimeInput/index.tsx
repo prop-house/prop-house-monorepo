c8ccbadbf26da340f59f144dde0384abd222f9fe
christianturner <christianturner@hey.com>
2023-02-10 10:52:38 -0500

    date time input placeholder & styles

--- packages/prop-house-webapp/src/components/HouseManager/DateTimeInput/index.tsx
+++ packages/prop-house-webapp/src/components/HouseManager/DateTimeInput/index.tsx
@@ -1,6 +1,7 @@
 import classes from './DateTimeInput.module.css';
 import 'react-datetime/css/react-datetime.css';
 import DateTime from 'react-datetime';
+import clsx from 'clsx';
 
 const DateTimeInput: React.FC<{
   selectedDate: Date | string;
@@ -9,8 +10,12 @@ const DateTimeInput: React.FC<{
 }> = props => {
   const { selectedDate, onDateChange, isValidDate } = props;
 
+  // when the date is not selected, it is a string vs a date object
+  const placeholder = typeof selectedDate === 'string';
+
   return (
-    <div className={classes.container}>
+    // placeholder class makes the text gray
+    <div className={clsx(classes.container, placeholder && classes.placeholder)}>
       <DateTime value={selectedDate} onChange={onDateChange} isValidDate={isValidDate} />
     </div>
   );
