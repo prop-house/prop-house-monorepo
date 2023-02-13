6c061779355062e666a235d4013cb5c7ad2573dc
christianturner <christianturner@hey.com>
2023-02-13 12:59:53 -0500

    initial round redux state

--- packages/prop-house-webapp/src/components/HouseManager/NameTheRound/index.tsx
+++ packages/prop-house-webapp/src/components/HouseManager/NameTheRound/index.tsx
@@ -1,17 +1,37 @@
+import { InitialRoundProps, updateRound } from '../../../state/slices/round';
 import Footer from '../Footer';
 import Group from '../Group';
 import Header from '../Header';
 import Input from '../Input';
 import Text from '../Text';
+import { useDispatch } from 'react-redux';
+import { useAppSelector } from '../../../hooks';
 
 const NameTheRound = () => {
+  const dispatch = useDispatch();
+  const round = useAppSelector(state => state.round.round);
+
+  const handleChange = (
+    property: keyof InitialRoundProps,
+    value: InitialRoundProps[keyof InitialRoundProps],
+  ) => {
+    const newRound = { ...round, [property]: value };
+    dispatch(updateRound(newRound));
+  };
+
   return (
     <>
       <Header title="What's your round about?" />
 
       <Group>
         <Text type="subtitle">Round name</Text>
-        <Input placeholder={'Hack-a-thon'} value={''} onChange={() => {}} />
+        <Input
+          placeholder={'Hack-a-thon'}
+          value={round.title}
+          onChange={e => {
+            handleChange('title', e.target.value);
+          }}
+        />
       </Group>
 
       <Group>
@@ -21,8 +41,10 @@ const NameTheRound = () => {
           placeholder={
             'Describe the round. Think about the goals, timeline and how you will encourage builders to participate.'
           }
-          value={''}
-          onChange={() => {}}
+          value={round.description}
+          onChange={e => {
+            handleChange('description', e.target.value);
+          }}
         />
       </Group>
 
