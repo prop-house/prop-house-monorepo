90eb714f3af8297a441f76f0429a6bf221a4cf0d
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2023-02-14 12:12:18 -0500

    add rug radio

--- packages/prop-house-communities/src/communities/index.ts
+++ packages/prop-house-communities/src/communities/index.ts
@@ -88,5 +88,7 @@ export const communities = new CaseInsensitiveMap(
     ),
     // Chaos
     '0xc16923543829f002E4A3f37e3E2e7CC9B8a87b96': balanceOfErc20(18),
+    // Rug Radio
+    '0x6235CAEea7C515DaC14060Ec23a760090655F21b': balanceOfErc20(18),
   }),
 );
