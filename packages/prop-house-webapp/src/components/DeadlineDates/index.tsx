c3cb6f63d72656e232a58a63726e2be5e9ba32c4
christianturner <christianturner@hey.com>
2023-02-15 15:45:45 -0500

    update deadline dates range and remove static round copy

--- packages/prop-house-webapp/src/components/DeadlineDates/index.tsx
+++ packages/prop-house-webapp/src/components/DeadlineDates/index.tsx
@@ -1,27 +1,14 @@
 import { StoredAuction } from '@nouns/prop-house-wrapper/dist/builders';
-import { auctionStatus, AuctionStatus } from '../../utils/auctionStatus';
+import { InitialRoundProps } from '../../state/slices/round';
 import formatTime from '../../utils/formatTime';
 import classes from './DeadlineDates.module.css';
 
-const DeadlineDates: React.FC<{ round: StoredAuction }> = props => {
+const DeadlineDates: React.FC<{ round: StoredAuction | InitialRoundProps }> = props => {
   const { round } = props;
-  const { startTime, proposalEndTime, votingEndTime } = round;
-
-  const notStarted = auctionStatus(round) === AuctionStatus.AuctionNotStarted;
-  const proposing = auctionStatus(round) === AuctionStatus.AuctionAcceptingProps;
-  const voting = auctionStatus(round) === AuctionStatus.AuctionVoting;
-
-  const proposingStarts = formatTime(startTime);
-  const proposingEnds = formatTime(proposalEndTime);
-  const votingEnds = formatTime(votingEndTime);
 
   return (
     <div className={classes.date}>
-      {proposing || notStarted
-        ? `${proposingStarts} - ${proposingEnds}`
-        : voting
-        ? `${proposingEnds} - ${votingEnds}`
-        : `${proposingStarts} - ${votingEnds}`}
+      {`${formatTime(round.startTime!)} - ${formatTime(round.votingEndTime!)}`}
     </div>
   );
 };
