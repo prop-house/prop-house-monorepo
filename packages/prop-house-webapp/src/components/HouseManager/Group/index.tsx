3acc134cf856d6f28b5ae9a9f2d7701f3a20f140
christianturner <christianturner@hey.com>
2023-02-15 22:24:01 -0500

    removed Flex component for refactored Group component

--- packages/prop-house-webapp/src/components/HouseManager/Group/index.tsx
+++ packages/prop-house-webapp/src/components/HouseManager/Group/index.tsx
@@ -1,13 +1,37 @@
-import clsx from 'clsx';
-import classes from './Group.module.css';
+import { CSSProperties } from 'react';
 
 const Group: React.FC<{
   children: React.ReactNode;
+  gap?: number;
+  mt?: number;
+  mb?: number;
+  margin?: number;
+  row?: boolean;
   classNames?: string;
 }> = props => {
-  const { children, classNames } = props;
+  const { children, gap, mt, mb, margin, row, classNames } = props;
 
-  return <div className={clsx(classes.group, classNames)}>{children}</div>;
+  const gapValue = gap ? `${gap}px` : undefined;
+
+  let marginValue;
+  if (margin) {
+    marginValue = `${margin}px 0`;
+  } else {
+    marginValue = `${mt ?? 0}px 0px ${mb ?? 0}px 0px`;
+  }
+
+  const styles: CSSProperties = {
+    display: 'flex',
+    flexDirection: row ? 'row' : 'column',
+    gap: gapValue,
+    margin: marginValue,
+  };
+
+  return (
+    <div style={styles} className={classNames}>
+      {children}
+    </div>
+  );
 };
 
 export default Group;
