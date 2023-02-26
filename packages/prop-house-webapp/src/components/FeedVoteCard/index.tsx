34e656f7bf3ecf5f162b3aebdce64cc0a51da67e
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2023-02-26 11:29:39 -0500

    add click through for prop feed

--- packages/prop-house-webapp/src/components/FeedVoteCard/index.tsx
+++ packages/prop-house-webapp/src/components/FeedVoteCard/index.tsx
@@ -1,14 +1,24 @@
 import { StoredVoteWithProposal } from '@nouns/prop-house-wrapper/dist/builders';
+import { useNavigate } from 'react-router-dom';
 import EthAddress from '../EthAddress';
 import classes from './FeedVoteCard.module.css';
 
 const FeedVoteCard: React.FC<{ vote: StoredVoteWithProposal }> = props => {
   const { vote } = props;
 
+  const navigate = useNavigate();
+
+  const handleOnClick = () => {
+    navigate('/proposal/1');
+  };
+
   return (
     <div className={classes.feedRow}>
       <span className={classes.owner}>{<EthAddress address={vote.address} />}</span> voted{' '}
-      {vote.weight} times for <span className={classes.propName}>{vote.proposal.title}</span>
+      {vote.weight} times for{' '}
+      <span className={classes.propName} onClick={() => handleOnClick()}>
+        {vote.proposal.title}
+      </span>
     </div>
   );
 };
