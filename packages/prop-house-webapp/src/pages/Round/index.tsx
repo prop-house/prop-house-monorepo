438c28557629b30aeb73e79497efaa5c14060937
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2023-02-27 23:07:47 -0700

    fix recursion

--- packages/prop-house-webapp/src/pages/Round/index.tsx
+++ packages/prop-house-webapp/src/pages/Round/index.tsx
@@ -55,7 +55,7 @@ const Round = () => {
 
   // if no data is found in store (ie round page is entry point), fetch community and round
   useEffect(() => {
-    if (round && community) return;
+    if (round) return;
 
     const fetchCommunityAndRound = async () => {
       try {
