99299e286865841fe6706e65bc915db304bb7bfe
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2023-03-03 21:20:49 -0700

    add swiper for smooth round card scrolling

--- packages/prop-house-webapp/src/pages/Base/index.tsx
+++ packages/prop-house-webapp/src/pages/Base/index.tsx
@@ -1,8 +1,9 @@
 import classes from './Base.module.css';
+import './Base.css';
 import { PropHouseWrapper } from '@nouns/prop-house-wrapper';
 import { StoredAuction, StoredVoteWithProposal } from '@nouns/prop-house-wrapper/dist/builders';
 import { getRelevantComms } from 'prop-house-communities';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { Col, Container, Navbar, Row } from 'react-bootstrap';
 import { useAccount, useBlockNumber, useProvider } from 'wagmi';
 import FeedVoteCard from '../../components/FeedVoteCard';
@@ -12,6 +13,13 @@ import Button, { ButtonColor } from '../../components/Button';
 import SimpleRoundCard from '../../components/SimpleRoundCard';
 import { FiActivity } from 'react-icons/fi';
 import { BiBadgeCheck } from 'react-icons/bi';
+import { isMobile } from 'web3modal';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Mousewheel, Pagination } from 'swiper';
+// Styles must use direct files imports
+import 'swiper/swiper.min.css';
+import 'swiper/modules/mousewheel/mousewheel.min.css';
+import 'swiper/modules/pagination/pagination.min.css';
 
 const Base = () => {
   const { address: account } = useAccount();
@@ -19,7 +27,6 @@ const Base = () => {
   const provider = useProvider();
 
   const VOTE_LOAD = 10;
-
   const [loadingRelComms, setLoadingRelComms] = useState(false);
   const [relevantCommunities, setRelevantCommunites] = useState<string[] | undefined>(undefined);
   const [rounds, setRounds] = useState<StoredAuction[]>();
@@ -76,57 +83,78 @@ const Base = () => {
   };
 
   return (
-    <Container>
-      <Navbar />
-
-      <Row>
-        <Col>
-          <div className={classes.sectionTitle}>
-            <BiBadgeCheck className={classes.icon} size={22} />
-            <>Active rounds</>
-          </div>
-          {rounds ? (
-            <Row>
-              {rounds.map(round => (
-                <Col md={6}>
-                  <SimpleRoundCard round={round} displayTldr={false} />
-                </Col>
-              ))}
-            </Row>
-          ) : (
-            <LoadingIndicator />
-          )}
-        </Col>
-      </Row>
-
-      <Row>
-        <Col>
-          <div className={classes.sectionTitle}>
-            <FiActivity className={classes.icon} size={22} />
-            <>Activity</>
-          </div>
-          {votes && votes.length > 0 ? (
-            <Row>
-              {votes.map(v => (
+    <>
+      <Container>
+        <Navbar />
+      </Container>
+      <Container>
+        <Row>
+          <Col>
+            <div className={classes.sectionTitle}>
+              <BiBadgeCheck className={classes.icon} size={22} />
+              <>Active rounds</>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <Container fluid>
+        <Row>
+          <Col>
+            {rounds ? (
+              <Swiper
+                modules={[Mousewheel, Pagination]}
+                mousewheel={isMobile() ? false : true}
+                pagination={{
+                  enabled: isMobile() ? true : false,
+                  dynamicBullets: true,
+                }}
+                spaceBetween={10}
+                slidesPerView={isMobile() ? 1 : 3}
+                slidesOffsetBefore={isMobile() ? 0 : 220}
+                slidesOffsetAfter={isMobile() ? 0 : 20}
+              >
+                {rounds.map(r => (
+                  <SwiperSlide>
+                    <SimpleRoundCard round={r} displayTldr={false} />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            ) : (
+              <LoadingIndicator />
+            )}
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <Row>
+          <Col>
+            <div className={classes.sectionTitle}>
+              <FiActivity className={classes.icon} size={22} />
+              <>Activity</>
+            </div>
+            {votes && votes.length > 0 ? (
+              <Row>
+                {votes.map(v => (
+                  <Col xs={12}>
+                    <FeedVoteCard vote={v} />
+                  </Col>
+                ))}
                 <Col xs={12}>
-                  <FeedVoteCard vote={v} />
+                  <Button
+                    text="load more votes"
+                    bgColor={ButtonColor.Green}
+                    onClick={() => fetchMoreVotes()}
+                    classNames={classes.loadMoreBtn}
+                  />
                 </Col>
-              ))}
-              <Col xs={12}>
-                <Button
-                  text="load more votes"
-                  bgColor={ButtonColor.Green}
-                  onClick={() => fetchMoreVotes()}
-                  classNames={classes.loadMoreBtn}
-                />
-              </Col>
-            </Row>
-          ) : (
-            <LoadingIndicator />
-          )}
-        </Col>
-      </Row>
-    </Container>
+              </Row>
+            ) : (
+              <LoadingIndicator />
+            )}
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
