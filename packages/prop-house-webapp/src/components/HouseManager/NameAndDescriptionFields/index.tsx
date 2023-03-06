15003e1182681fb4e0557e2db9cfb09ff97b8fc4
christianturner <christianturner@hey.com>
2023-03-06 00:04:24 -0500

    rich text editor step 1

--- packages/prop-house-webapp/src/components/HouseManager/NameAndDescriptionFields/index.tsx
+++ packages/prop-house-webapp/src/components/HouseManager/NameAndDescriptionFields/index.tsx
@@ -4,10 +4,10 @@ import Group from '../Group';
 import Text from '../Text';
 import { useDispatch } from 'react-redux';
 import { useAppSelector } from '../../../hooks';
-import { useState } from 'react';
+import { useMemo, useRef, useState } from 'react';
 import clsx from 'clsx';
 import { capitalize } from '../../../utils/capitalize';
-import ReactQuill from 'react-quill';
+import ReactQuill, { Quill } from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
 const NameAndDescriptionFields = () => {
@@ -18,7 +18,48 @@ const NameAndDescriptionFields = () => {
   const [description, setDescription] = useState(round.description || '');
   const [errors, setErrors] = useState<{ title?: string; description?: string }>({});
 
-  const modules = { toolbar: [['bold', 'italic', 'link']] };
+  let quillRef = useRef<ReactQuill>(null);
+  var icons = Quill.import('ui/icons');
+  icons['undo'] = `<svg viewbox="0 0 18 18">
+  <polygon class="ql-fill ql-stroke" points="6 10 4 12 2 10 6 10"></polygon>
+  <path class="ql-stroke" d="M8.09,13.91A4.6,4.6,0,0,0,9,14,5,5,0,1,0,4,9"></path>
+</svg>`;
+  icons['redo'] = `<svg viewbox="0 0 18 18">
+  <polygon class="ql-fill ql-stroke" points="12 10 14 12 16 10 12 10"></polygon>
+  <path class="ql-stroke" d="M9.91,13.91A4.6,4.6,0,0,1,9,14a5,5,0,1,1,5-5"></path>
+</svg>`;
+
+  const handleUndo = () => {
+    if (quillRef.current) {
+      const quill = quillRef.current.getEditor();
+      // @ts-ignore
+      if (quill) quill.history.undo();
+    }
+  };
+
+  const handleRedo = () => {
+    if (quillRef.current) {
+      const quill = quillRef.current.getEditor();
+      // @ts-ignore
+      if (quill) quill.history.redo();
+    }
+  };
+  const modules = useMemo(
+    () => ({
+      toolbar: {
+        handlers: { undo: handleUndo, redo: handleRedo },
+        container: [
+          ['undo', 'redo'],
+          ['bold', 'italic', 'link'],
+        ],
+      },
+      clipboard: {
+        matchVisual: false,
+      },
+    }),
+    [],
+  );
+
   const formats = ['bold', 'italic', 'link'];
 
   const handleBlur = (field: 'title' | 'description') => {
@@ -88,6 +129,7 @@ const NameAndDescriptionFields = () => {
             }
             modules={modules}
             formats={formats}
+            ref={quillRef}
           />
         </Group>
 
