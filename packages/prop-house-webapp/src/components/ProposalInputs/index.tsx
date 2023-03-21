9625dee7e55ce9b9b1e98589261faf9f815b02d7
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2023-03-21 10:34:51 -0400

    refactor

--- packages/prop-house-webapp/src/components/ProposalInputs/index.tsx
+++ packages/prop-house-webapp/src/components/ProposalInputs/index.tsx
@@ -13,34 +13,35 @@ import inputHasImage from '../../utils/inputHasImage';
 import { isInfAuction } from '../../utils/auctionType';
 import { useLocation } from 'react-router-dom';
 import { useSigner } from 'wagmi';
+import InputFormGroup from '../InputFormGroup';
 
 const ProposalInputs: React.FC<{
   quill: any;
   quillRef: any;
-  formData: any;
+  formData: FormDataType[];
   descriptionData: any;
   onDataChange: (data: Partial<ProposalFields>) => void;
   onFileDrop: any;
   editorBlurred: boolean;
   setEditorBlurred: (blurred: boolean) => void;
   initReqAmount: number | null;
-}> = props => {
-  const {
-    quill,
-    quillRef,
-    formData,
-    descriptionData,
-    onDataChange,
-    editorBlurred,
-    setEditorBlurred,
-    onFileDrop,
-    initReqAmount,
-  } = props;
+}> = ({
+  quill,
+  quillRef,
+  formData,
+  descriptionData,
+  onDataChange,
+  editorBlurred,
+  setEditorBlurred,
+  onFileDrop,
+  initReqAmount,
+}) => {
   const location = useLocation();
   // active round comes from two diff places depending on where inputs are being displayed
   const roundFromLoc = location.state && location.state.auction; // creating new prop
   const roundFromStore = useAppSelector(state => state.propHouse.activeRound); // editing old prop
   const isInfRound = isInfAuction(roundFromLoc ? roundFromLoc : roundFromStore);
+  const remainingBal = 100;
 
   const data = useAppSelector(state => state.editor.proposal);
   const { data: signer } = useSigner();
@@ -56,112 +57,104 @@ const ProposalInputs: React.FC<{
   }, [signer, host]);
 
   const maxFundingReq = 100;
+
+  const titleAndTldrInputs = (data: any, isTitleSection: boolean = false) => (
+    <InputFormGroup
+      titleLabel={data.title}
+      content={
+        <>
+          <Form.Control
+            as={data.type as any}
+            autoFocus={data.focus}
+            maxLength={data.maxCount && data.maxCount}
+            placeholder={data.placeholder}
+            className={clsx(classes.input, data.fieldName === 'what' && classes.descriptionInput)}
+            onChange={e => {
+              setBlurred(false);
+              onDataChange({ [data.fieldName]: e.target.value });
+            }}
+            value={data && data.fieldValue}
+            onBlur={() => {
+              setBlurred(true);
+            }}
+          />
+          {blurred && validateInput(data.minCount, data.fieldValue.length) && (
+            <p className={classes.inputError}>{data.error}</p>
+          )}
+        </>
+      }
+      charsLabel={
+        data.maxCount ? `${data.fieldValue.length}/${data.maxCount}` : data.fieldValue.length
+      }
+      formGroupClasses={isTitleSection ? classes.infRoundTitleSection : ''}
+    />
+  );
+
   return (
     <>
       <Row>
         <Col xl={12}>
           <Form className={classes.form}>
-            {formData.map((input: FormDataType, index: number) => {
-              return (
-                <div className={isInfRound && index === 0 ? classes.infRoundSectionsContainer : ''}>
-                  <Form.Group
-                    className={clsx(
-                      classes.inputGroup,
-                      isInfRound && index === 0 ? classes.infRoundTitleSection : '',
-                    )}
-                  >
-                    <div className={classes.inputSection} key={input.title}>
-                      <div className={classes.inputInfo}>
-                        <Form.Label className={classes.inputLabel}>{input.title}</Form.Label>
-                        <Form.Label className={classes.inputChars}>
-                          {input.maxCount
-                            ? `${input.fieldValue.length}/${input.maxCount}`
-                            : input.fieldValue.length}
-                        </Form.Label>
-                      </div>
-
+            <div className={clsx(isInfRound && classes.infRoundSectionsContainer)}>
+              {/** TITLE */}
+              {titleAndTldrInputs(formData[0], true)}
+              {/** FUNDS REQ */}
+              {isInfRound && (
+                <InputFormGroup
+                  titleLabel="Funds Request"
+                  content={
+                    <>
                       <Form.Control
-                        as={input.type as any}
-                        autoFocus={input.focus}
-                        maxLength={input.maxCount && input.maxCount}
-                        placeholder={input.placeholder}
-                        className={clsx(
-                          classes.input,
-                          input.fieldName === 'what' && classes.descriptionInput,
-                        )}
+                        type="number"
+                        className={clsx(classes.input, classes.reqAmountInput)}
+                        value={fundReq || ''}
                         onChange={e => {
-                          setBlurred(false);
-                          onDataChange({ [input.fieldName]: e.target.value });
+                          setFundReq(Number(e.target.value));
+                          onDataChange({ reqAmount: Number(e.target.value) });
                         }}
-                        value={data && input.fieldValue}
-                        onBlur={() => {
-                          setBlurred(true);
-                        }}
+                        isInvalid={fundReq ? fundReq > remainingBal : false}
                       />
+                      <Form.Control.Feedback type="invalid">
+                        Exceeds max amount of {remainingBal}
+                      </Form.Control.Feedback>
+                    </>
+                  }
+                />
+              )}
+            </div>
 
-                      {blurred && validateInput(input.minCount, input.fieldValue.length) && (
-                        <p className={classes.inputError}>{input.error}</p>
-                      )}
-                    </div>
-                  </Form.Group>
-                  {isInfRound && index === 0 && (
-                    <Form.Group className={classes.inputGroup}>
-                      <div className={classes.inputSection} key={input.title}>
-                        <div className={classes.inputInfo}>
-                          <Form.Label className={classes.inputLabel}>Funds Request</Form.Label>
-                        </div>
-                        <Form.Control
-                          type="number"
-                          className={clsx(classes.input, classes.reqAmountInput)}
-                          value={fundReq || ''}
-                          onChange={e => {
-                            setFundReq(Number(e.target.value));
-                            onDataChange({ reqAmount: Number(e.target.value) });
-                          }}
-                        />
-                        <Form.Control.Feedback type="invalid">
-                          Exceeds max amount of {1}
-                        </Form.Control.Feedback>
-                      </div>
-                    </Form.Group>
-                  )}
-                </div>
-              );
-            })}
+            {/** TLDR */}
+            {titleAndTldrInputs(formData[1])}
 
-            <Form.Group>
-              <div className={classes.inputInfo}>
-                <Form.Label className={clsx(classes.inputLabel, classes.descriptionLabel)}>
-                  {descriptionData.title}
-                </Form.Label>
+            {/** DESCRIPTION */}
 
-                <Form.Label className={classes.inputChars}>
-                  {quill && quill.getText().length - 1}
-                </Form.Label>
-              </div>
-
-              <>
-                {/* 
+            <InputFormGroup
+              titleLabel={descriptionData.title}
+              content={
+                <>
+                  {/* 
                     When scrolling past the window height the sticky Card header activates, but the header has rounded borders so you still see the borders coming up from the Card body. `hideBorderBox` is a sticky, empty div with a fixed height that hides these borders. 
                   */}
-                <div className="hideBorderBox"></div>
-                <div
-                  ref={quillRef}
-                  onDrop={onFileDrop}
-                  placeholder={descriptionData.placeholder}
-                  onBlur={() => {
-                    setEditorBlurred(true);
-                  }}
-                />
+                  <div className="hideBorderBox"></div>
+                  <div
+                    ref={quillRef}
+                    onDrop={onFileDrop}
+                    placeholder={descriptionData.placeholder}
+                    onBlur={() => {
+                      setEditorBlurred(true);
+                    }}
+                  />
 
-                {editorBlurred &&
-                  quill &&
-                  !inputHasImage(descriptionData.fieldValue) &&
-                  validateInput(descriptionData.minCount, quill.getText().length - 1) && (
-                    <p className={classes.inputError}>{descriptionData.error}</p>
-                  )}
-              </>
-            </Form.Group>
+                  {editorBlurred &&
+                    quill &&
+                    !inputHasImage(descriptionData.fieldValue) &&
+                    validateInput(descriptionData.minCount, quill.getText().length - 1) && (
+                      <p className={classes.inputError}>{descriptionData.error}</p>
+                    )}
+                </>
+              }
+              charsLabel={quill && quill.getText().length - 1}
+            />
           </Form>
         </Col>
       </Row>
