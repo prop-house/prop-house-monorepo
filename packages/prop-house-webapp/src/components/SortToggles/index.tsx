8caba09eba95f954c352974815669a7397e8cfb0
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2023-03-22 10:47:09 -0400

    remove sort toggles

--- packages/prop-house-webapp/src/components/SortToggles/index.tsx
+++ packages/prop-house-webapp/src/components/SortToggles/index.tsx
@@ -2,12 +2,11 @@ import classes from './SortToggles.module.css';
 import { StoredAuctionBase } from '@nouns/prop-house-wrapper/dist/builders';
 import { auctionStatus, AuctionStatus } from '../../utils/auctionStatus';
 import { useDispatch } from 'react-redux';
-import { IoArrowDown, IoArrowUp } from 'react-icons/io5';
 import clsx from 'clsx';
 import { useTranslation } from 'react-i18next';
 import { useState } from 'react';
 import { useAppSelector } from '../../hooks';
-import { isInfAuction, isTimedAuction } from '../../utils/auctionType';
+import { isInfAuction } from '../../utils/auctionType';
 import {
   filterInfRoundProposals,
   InfRoundFilterType,
@@ -81,38 +80,6 @@ const SortToggles: React.FC<{
   return (
     <>
       <div className={classes.sortContainer}>
-        {isTimedAuction(auction) && (
-          <>
-            {/** SORT BY BY VOTES */}
-            {allowSortByVotes && (
-              <div
-                onClick={() => handleSortByVotes()}
-                className={clsx(
-                  classes.sortItem,
-                  votesSorted && classes.active,
-                  disabled() && classes.disabled,
-                )}
-              >
-                <div className={classes.sortLabel}>{t('votesCap')}</div>
-                {votesAscending ? <IoArrowDown size={'1.5rem'} /> : <IoArrowUp size={'1.5rem'} />}
-              </div>
-            )}
-            {/** SORT BY BY CREATION DATE */}
-            <div
-              onClick={() => handleSortByCreationDate()}
-              className={clsx(
-                classes.sortItem,
-                datesSorted && classes.active,
-                (!auction || auctionNotStarted || (proposals && proposals.length <= 1)) &&
-                  classes.disabled,
-              )}
-            >
-              <div className={classes.sortLabel}>{t('created')}</div>
-              {dateAscending ? <IoArrowDown size={'1.5rem'} /> : <IoArrowUp size={'1.5rem'} />}
-            </div>
-          </>
-        )}
-
         {isInfAuction(auction) && (
           <>
             <div
