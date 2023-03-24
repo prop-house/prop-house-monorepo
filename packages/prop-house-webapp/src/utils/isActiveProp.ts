a49902a7657f1ad5ed06502cf97e0103b17d95be
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2023-03-24 10:08:20 -0400

    fix func check

--- packages/prop-house-webapp/src/utils/isActiveProp.ts
+++ packages/prop-house-webapp/src/utils/isActiveProp.ts
@@ -2,7 +2,8 @@ import { StoredInfiniteAuction, StoredProposal } from '@nouns/prop-house-wrapper
 import dayjs from 'dayjs';
 
 /**
- * Checks whether or not an infinite round proposal is active (creating within voting period)
+ * Checks whether or not an infinite round proposal is active: created within voting period and has not met quorum.
  */
 export const isActiveProp = (prop: StoredProposal, round: StoredInfiniteAuction) =>
-  dayjs(prop.createdDate).add(round.votingPeriod, 'seconds').isAfter(dayjs());
+  dayjs(prop.createdDate).add(round.votingPeriod, 'seconds').isAfter(dayjs()) &&
+  prop.voteCount < round.quorum;
