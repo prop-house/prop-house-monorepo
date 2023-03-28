6dc362f9d8347df4ad5ade48823b8268cb3f0515
solimander <solimander-dev@protonmail.com>
2023-03-28 12:15:27 -0600

    consume wagmi in prop house (support more later)

--- packages/prop-house-sdk-react/src/components/PropHouseProvider.tsx
+++ packages/prop-house-sdk-react/src/components/PropHouseProvider.tsx
@@ -1,18 +1,37 @@
-import { PropHouse, PropHouseConfig, Custom } from '@prophouse/sdk';
-import React, { createContext, useState } from 'react';
+import { PropHouse, Custom, Starknet, Newable, StrategyHandlerBase, VotingStrategyConfig } from '@prophouse/sdk';
+import React, { createContext, useEffect, useState } from 'react';
+import { useNetwork, useProvider, useSigner } from 'wagmi';
 
 export type PropHouseProps<CS extends Custom | void = void> = {
   children: React.ReactNode;
-  config: PropHouseConfig<CS>;
+  starknet?: Starknet;
+  customStrategies?: Newable<StrategyHandlerBase<VotingStrategyConfig<CS>>>[];
+  customStarknetRelayer?: string;
 };
 
 export const PropHouseContext = createContext<PropHouse | undefined>(undefined);
 
-export const PropHouseProvider = ({ children, config }: PropHouseProps) => {
-  const [propHouse] = useState(new PropHouse(config));
+export const PropHouseProvider = <CS extends Custom | void = void>({ children, ...props }: PropHouseProps<CS>) => {
+  const provider = useProvider();
+  const { data: signer } = useSigner();
+  const { chain } = useNetwork();
+  
+  const [propHouse, setPropHouse] = useState<PropHouse<CS>>();
+
+  useEffect(() => {
+    if ((!provider && !signer) || !chain) return;
+
+    setPropHouse(
+      new PropHouse<CS>({
+        ...props,
+        evmChainId: chain.id,
+        evm: signer || provider,
+      })
+    );
+  }, [provider, signer, chain]);
 
   return (
-    <PropHouseContext.Provider value={propHouse}>
+    <PropHouseContext.Provider value={propHouse as PropHouse}>
       {children}
     </PropHouseContext.Provider>
   );
--- packages/prop-house-sdk-react/src/index.tsx
+++ packages/prop-house-sdk-react/src/index.tsx
@@ -1,2 +1,4 @@
+export * from '@prophouse/sdk';
+
 export { PropHouseProvider, PropHouseContext } from './components';
 export { usePropHouse } from './hooks';
