7aaf6b7610621b0ac437a5608108004941573843
solimander <solimander-dev@protonmail.com>
2023-03-28 12:53:25 -0600

    Update wagmi

--- packages/prop-house-sdk-react/src/hooks/usePropHouse.ts
+++ packages/prop-house-sdk-react/src/hooks/usePropHouse.ts
@@ -2,12 +2,6 @@ import { PropHouse, Custom } from '@prophouse/sdk';
 import { PropHouseContext } from '../components';
 import { useContext } from 'react';
 
-export const usePropHouse = <CS extends Custom | void = void>(): PropHouse<CS> => {
-  const propHouse = useContext(PropHouseContext);
-
-  if (!propHouse) {
-    throw new Error('You must provide PropHouse via PropHouseProvider');
-  }
-
-  return propHouse as PropHouse<CS>;
+export const usePropHouse = <CS extends Custom | void = void>(): PropHouse<CS> | undefined => {
+  return useContext(PropHouseContext) as PropHouse<CS> | undefined;
 };
