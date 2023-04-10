e9f6558aa27bcd3cd32925c65470323a8df519f1
christianturner <christianturner@hey.com>
2023-04-10 16:52:35 -0400

    update background color rules for admin paths

--- packages/prop-house-webapp/src/utils/bgColorForPage.ts
+++ packages/prop-house-webapp/src/utils/bgColorForPage.ts
@@ -4,7 +4,9 @@ const bgColorForPage = (path: string) => {
     path === '/faq' ||
     path === '/create' ||
     path === '/rounds' ||
-    path === '/create-round'
+    path === '/create-round' ||
+    path === '/admin' ||
+    path === '/admin/rounds'
   )
     return 'bgGray';
 
