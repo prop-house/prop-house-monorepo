3268d268814e399f222e155fa70cc55271bbd942
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2023-04-12 15:08:00 -0400

    remove cors

--- packages/prop-house-supabase/supabase/functions/reply/index.ts
+++ packages/prop-house-supabase/supabase/functions/reply/index.ts
@@ -2,12 +2,8 @@ import { serve } from 'https://deno.land/std@0.168.0/http/server.ts';
 import prophouse from 'https://esm.sh/@prophouse/communities@0.1.4';
 import { ethers } from 'https://esm.sh/ethers@5.7.2';
 import { isProposer as _isProposer } from '../_shared/isProposer.ts';
-import { corsHeaders } from '../_shared/cors.ts';
 
 serve(async req => {
-  if (req.method === 'OPTIONS') {
-    return new Response('ok', { headers: corsHeaders });
-  }
   const provider = new ethers.providers.JsonRpcProvider('99fa8a6adf2f4215ba86661c36cb7e84');
   const { userAddress, communityAddress, blockTag, proposalId } = await req.json();
 
@@ -24,7 +20,7 @@ serve(async req => {
   };
 
   return new Response(JSON.stringify(data), {
-    headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+    headers: { 'Content-Type': 'application/json' },
     status: 200,
   });
 });
