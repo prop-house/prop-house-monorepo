c2dd4ee69fee860bc67cbc63c852e47ae5793204
christianturner <christianturner@hey.com>
2023-04-12 23:04:26 -0400

    update comment

--- packages/prop-house-webapp/src/components/RoundCreatorProtectedRoute/index.tsx
+++ packages/prop-house-webapp/src/components/RoundCreatorProtectedRoute/index.tsx
@@ -5,6 +5,7 @@ interface RoundCreatorProtectedRouteProps {
   children: JSX.Element;
 }
 
+// This is a route that only allows connected users to access it, if there's no connected wallet, it will redirect to the home page
 const RoundCreatorProtectedRoute = ({ account, children }: RoundCreatorProtectedRouteProps) => {
   if (!account) return <Navigate to="/" />;
 
