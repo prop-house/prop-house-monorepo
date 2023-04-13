ffb095ad02afc366c7d66f7be615df5c3a20bc24
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2023-04-13 15:23:53 -0400

    add cors support

--- packages/prop-house-supabase/supabase/functions/reply/index.ts
+++ packages/prop-house-supabase/supabase/functions/reply/index.ts
@@ -3,8 +3,13 @@ import prophouse from 'https://esm.sh/@prophouse/communities@0.1.4';
 import { ethers } from 'https://esm.sh/ethers@5.7.2';
 import { isProposer as _isProposer } from '../_shared/isProposer.ts';
 import { insertReply } from '../_shared/insertReply.ts';
+import { corsHeaders } from '../_shared/cors.ts';
 
 serve(async req => {
+  if (req.method === 'OPTIONS') {
+    return new Response('ok', { headers: corsHeaders });
+  }
+
   const INFURA_PROJECT_ID = Deno.env.get('INFURA_PROJECT_ID');
 
   const provider = new ethers.providers.JsonRpcProvider(INFURA_PROJECT_ID);
@@ -24,7 +29,7 @@ serve(async req => {
     return new Response(
       JSON.stringify({ error: `${userAddress} is not allowed to reply to proposal ${proposalId}` }),
       {
-        headers: { 'Content-Type': 'application/json' },
+        headers: { ...corsHeaders, 'Content-Type': 'application/json' },
         status: 403,
       },
     );
@@ -34,7 +39,7 @@ serve(async req => {
     return new Response(
       JSON.stringify({ success: `${userAddress} added a reply to proposal ${proposalId}` }),
       {
-        headers: { 'Content-Type': 'application/json' },
+        headers: { ...corsHeaders, 'Content-Type': 'application/json' },
         status: 200,
       },
     );
@@ -42,7 +47,7 @@ serve(async req => {
     return new Response(
       JSON.stringify({ error: `error inserting reply into db. error: ${error}` }),
       {
-        headers: { 'Content-Type': 'application/json' },
+        headers: { ...corsHeaders, 'Content-Type': 'application/json' },
         status: 400,
       },
     );
