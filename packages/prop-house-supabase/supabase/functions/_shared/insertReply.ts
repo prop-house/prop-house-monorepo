b91e1f32d725bd6520c7149873087a0a99616d76
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2023-04-14 14:01:40 -0400

    move client creation back into insert reply func

--- packages/prop-house-supabase/supabase/functions/_shared/insertReply.ts
+++ packages/prop-house-supabase/supabase/functions/_shared/insertReply.ts
@@ -1,8 +1,13 @@
-import { client } from './client.ts';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.20.0';
 
 export const insertReply = async (address: string, proposalId: number, content: string) => {
-  const supabase = client();
+  const supabaseUrl = Deno.env.get('SUPABASE_URL');
+  const supabaseKey = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY');
 
+  if (!supabaseUrl || !supabaseKey)
+    throw new Error('Supabase URL or key not set in environment variables');
+
+  const supabase = createClient(supabaseUrl, supabaseKey);
   const { error } = await supabase.from('reply').insert({
     address,
     proposalId,
