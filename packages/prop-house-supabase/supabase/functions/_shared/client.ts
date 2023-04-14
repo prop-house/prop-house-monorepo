89054fcc60d63d220869e86dbbd640e3765b97b1
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2023-04-14 13:49:38 -0400

    update dev

--- packages/prop-house-supabase/supabase/functions/_shared/client.ts
+++ packages/prop-house-supabase/supabase/functions/_shared/client.ts
@@ -1,16 +1,8 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.20.0';
 
 export const client = () => {
-  let supabaseUrl;
-  let supabaseKey;
-
-  if (Deno.env.get('SUP_URL_LOCAL')) {
-    supabaseUrl = Deno.env.get('SUP_URL_LOCAL');
-    supabaseKey = Deno.env.get('SUP_KEY_LOCAL');
-  } else {
-    supabaseUrl = Deno.env.get('SUPABASE_URL');
-    supabaseKey = Deno.env.get('SUPABASE_KEY');
-  }
+  const supabaseUrl = Deno.env.get('SUPABASE_URL');
+  const supabaseKey = Deno.env.get('SUPABASE_KEY');
 
   if (!supabaseUrl || !supabaseKey)
     throw new Error('Supabase URL or key not set in environment variables');
