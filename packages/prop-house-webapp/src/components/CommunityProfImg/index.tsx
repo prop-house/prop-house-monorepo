563ef7d5f8bd8bcf233952c0071228cfcaecb747
solimander <solimander-dev@protonmail.com>
2023-04-24 15:01:59 -0600

    Use existing ipfs gateway function

--- packages/prop-house-webapp/src/components/CommunityProfImg/index.tsx
+++ packages/prop-house-webapp/src/components/CommunityProfImg/index.tsx
@@ -2,13 +2,12 @@ import classes from './CommunityProfImg.module.css';
 import loadingNoun from '../../assets/loading-skull-noun.gif';
 import { House } from '@prophouse/sdk-react';
 import clsx from 'clsx';
+import buildIpfsPath from '../../utils/buildIpfsPath';
 
 // TODO: Move to utils
 const extractIPFSContentID = (uri: string | undefined) => {
   return uri?.match('Qm[1-9A-HJ-NP-Za-km-z]{44,}|b[A-Za-z2-7]{58,}|B[A-Z2-7]{58,}|z[1-9A-HJ-NP-Za-km-z]{48,}|F[0-9A-F]{50,}')?.[0];
 };
-
-const ipfsGateway = (cid: string) => `https://prophouse.mypinata.cloud/ipfs/${cid}`;
 
 // The void noun is a placeholder image for houses that don't have a profile image
 const FALLBACK_HOUSE_IMAGE_CID = 'bafkreialzck4dnbaa75ma25d2qhpy3ptav4o2o7awn2dxhzjntg25zbzoe';
@@ -23,7 +22,7 @@ const CommunityProfImg: React.FC<{
   return community ? (
       <img
         crossOrigin="anonymous"
-        src={ipfsGateway(extractIPFSContentID(community.imageURI) ?? FALLBACK_HOUSE_IMAGE_CID)}
+        src={buildIpfsPath(extractIPFSContentID(community.imageURI) ?? FALLBACK_HOUSE_IMAGE_CID)}
         alt="community profile"
         className={clsx(classes.img, hover && classes.hoverImg)}
       />
