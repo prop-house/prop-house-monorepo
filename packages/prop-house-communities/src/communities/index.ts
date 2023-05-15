4480285831e5cc067b8feb787f500789befd7cc7
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2023-05-15 10:20:37 -0400

    update nounish

--- packages/prop-house-communities/src/communities/index.ts
+++ packages/prop-house-communities/src/communities/index.ts
@@ -125,7 +125,7 @@ export const communities = new CaseInsensitiveMap(
     // mxtter
     '0xBe2d48C0752713E342c869A50a7c33e00bf4C409': balanceOfErc721(),
     // nounish
-    '0x49a5aEec7d9AC70990404278e93031E4C3C71875': fixedVotes(100),
+    '0x89A814E79ce54B9bdc443779BEC7d9699De85D45': fixedVotes(100),
     // nouns esports
     '0x9a64c732c284fd248e71fb2d9850f47e6ef6e32d': balanceOfErc721(),
     // turbo voting token
