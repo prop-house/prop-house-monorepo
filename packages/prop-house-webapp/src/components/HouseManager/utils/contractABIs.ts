dea5bfa5b374579071e16bdb46c454fe874eb830
christianturner <christianturner@hey.com>
2023-06-26 11:20:44 -0400

    ABIs for 721 & 1155

--- packages/prop-house-webapp/src/components/HouseManager/utils/contractABIs.ts
+++ packages/prop-house-webapp/src/components/HouseManager/utils/contractABIs.ts
@@ -1,3 +1,4 @@
+// ERC-20
 export const erc20BalanceOfInterface = [
   {
     constant: true,
@@ -58,6 +59,131 @@ export const erc20ApproveInterface = [
     ],
     payable: false,
     stateMutability: 'nonpayable',
+    type: 'function',
+  },
+];
+
+// ERC-721
+export const erc721BalanceOfInterface = [
+  {
+    constant: true,
+    inputs: [{ name: 'owner', type: 'address' }],
+    name: 'balanceOf',
+    outputs: [{ name: '', type: 'uint256' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+];
+
+export const erc721ApproveInterface = [
+  {
+    constant: false,
+    inputs: [
+      {
+        name: 'approved',
+        type: 'address',
+      },
+      {
+        name: 'tokenId',
+        type: 'uint256',
+      },
+    ],
+    name: 'approve',
+    outputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+];
+
+export const erc721GetApprovedInterface = [
+  {
+    constant: true,
+    inputs: [
+      {
+        name: 'tokenId',
+        type: 'uint256',
+      },
+    ],
+    name: 'getApproved',
+    outputs: [
+      {
+        name: '',
+        type: 'address',
+      },
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+];
+
+// ERC-1155
+export const erc1155BalanceOfInterface = [
+  {
+    constant: true,
+    inputs: [
+      {
+        name: 'account',
+        type: 'address',
+      },
+      {
+        name: 'id',
+        type: 'uint256',
+      },
+    ],
+    name: 'balanceOf',
+    outputs: [{ name: '', type: 'uint256' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+];
+
+export const erc1155SetApprovalForAllInterface = [
+  {
+    constant: false,
+    inputs: [
+      {
+        name: 'operator',
+        type: 'address',
+      },
+      {
+        name: 'approved',
+        type: 'bool',
+      },
+    ],
+    name: 'setApprovalForAll',
+    outputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+];
+
+export const erc1155IsApprovedForAllInterface = [
+  {
+    constant: true,
+    inputs: [
+      {
+        name: 'account',
+        type: 'address',
+      },
+      {
+        name: 'operator',
+        type: 'address',
+      },
+    ],
+    name: 'isApprovedForAll',
+    outputs: [
+      {
+        name: '',
+        type: 'bool',
+      },
+    ],
+    payable: false,
+    stateMutability: 'view',
     type: 'function',
   },
 ];
