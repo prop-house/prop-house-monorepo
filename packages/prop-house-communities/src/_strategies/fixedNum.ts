9ab7d64d8d6c77ee4dde8764376961bd6e7959d9
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2023-08-01 18:04:51 -0400

    update strat

--- packages/prop-house-communities/src/_strategies/fixedNum.ts
+++ packages/prop-house-communities/src/_strategies/fixedNum.ts
@@ -3,13 +3,17 @@ import { StrategyFactory, _Strategy } from '../types/_Strategy';
 
 export interface fixedNumStratArgs extends BaseArgs {
   num: number;
+  addresses?: string[];
 }
 
 export const fixedNum: StrategyFactory<fixedNumStratArgs> = (
   params: fixedNumStratArgs,
 ): _Strategy => {
   return async () => {
-    const { num } = params;
+    const { num, addresses, account } = params;
+
+    if (addresses) return addresses.find(a => a.toLowerCase() == account.toLowerCase()) ? num : 0;
+
     return num;
   };
 };
