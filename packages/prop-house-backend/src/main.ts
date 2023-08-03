66a73f3727191d2fef90477a9802f8a5932d74b8
devcarrot <devcarrot@protonmail.com>
2023-08-03 00:11:38 -0600

    Explicitly set trust proxy

--- packages/prop-house-backend/src/main.ts
+++ packages/prop-house-backend/src/main.ts
@@ -2,9 +2,11 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
+import { NestExpressApplication } from "@nestjs/platform-express"
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true });
+  app.set('trust proxy', 1);
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   app.useGlobalPipes(
