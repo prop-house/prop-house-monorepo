1b3105a4f087382039e8cfd4eeffa79394e55183
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2023-08-17 16:44:21 -0400

    fix avatar fetching w updated wagmi calls

--- packages/prop-house-webapp/src/components/Avatar/index.tsx
+++ packages/prop-house-webapp/src/components/Avatar/index.tsx
@@ -1,14 +1,18 @@
 import classes from './Avatar.module.css';
-import { useEnsAvatar } from 'wagmi';
+import { useEnsAvatar, useEnsName } from 'wagmi';
 import Jazzicon from 'react-jazzicon/dist/Jazzicon';
 import { jsNumberForAddress } from 'react-jazzicon';
 
 const Avatar: React.FC<{ address: string; diameter: number }> = props => {
   const { address, diameter } = props;
-  const { data } = useEnsAvatar({ name: `0x${address.substring(2)}` });
 
-  return data ? (
-    <img key={address} src={data} alt={`Avatar for ${address}`} className={classes.ensAvatar} />
+  const { data: ensName } = useEnsName({
+    address: address as `0x${string}`,
+  });
+  const { data: avatar } = useEnsAvatar({ name: ensName });
+
+  return avatar ? (
+    <img key={address} src={avatar} alt={`Avatar for ${address}`} className={classes.ensAvatar} />
   ) : (
     <Jazzicon diameter={diameter} seed={jsNumberForAddress(address)} />
   );
