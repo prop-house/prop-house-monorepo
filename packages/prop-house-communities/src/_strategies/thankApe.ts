0d0a8c9d464e0253da10b8ed6a79b16f3eb9c3fe
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2023-09-18 11:02:05 -0400

    init commit

--- packages/prop-house-communities/src/_strategies/thankApe.ts
+++ packages/prop-house-communities/src/_strategies/thankApe.ts
@@ -1,6 +1,5 @@
-import ThriveCoinVoterList from '../abi/ThriveCoinVoterList.json';
 import ApeCoinStaking from '../abi/ApeCoinStaking.json';
-import { Contract, providers } from 'ethers';
+import { Contract } from 'ethers';
 import { parseBlockTag } from '../utils/parseBlockTag';
 import { StrategyFactory, _Strategy } from '../types/_Strategy';
 import { BaseArgs } from '../actions/execStrategy';
@@ -9,34 +8,37 @@ import BigNumber from 'bignumber.js';
 import { formatUnits } from 'ethers/lib/utils';
 
 export interface thankApeStratArgs extends BaseArgs {
-  address: string;
-  polygonBlockNumber?: number;
-  mainnetBlockNumber?: number;
+  blockTag?: number;
 }
 
 export const thankApe: StrategyFactory<thankApeStratArgs> = (
   params: thankApeStratArgs,
 ): _Strategy => {
   return async () => {
-    const { account, address, polygonBlockNumber, mainnetBlockNumber, provider } = params;
+    const { account, blockTag, provider } = params;
 
-    const mainnetProvider = new providers.InfuraProvider(process.env.INFURA_PROJECT_ID);
-
-    // ThriveCoin
-    const contract = new Contract(address, ThriveCoinVoterList, provider);
-    const hasVoteRight = await contract.hasVoteRight(account, {
-      blockTag: parseBlockTag(polygonBlockNumber),
+    // ThankApe check
+    const url = `https://core.api.thrivecoin.com/v1/eligibility/thank-ape/voting/${account}`;
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        AuthenticationToken: process.env.REACT_APP_THANKAPE_AUTH_TOKEN!,
+      },
     });
+    const { data, errors } = await response.json();
+
+    console.log('response: ', data);
+    console.log('errors: ', errors);
+
+    const isEligible = data.eligible;
+
+    if (!isEligible || errors) return 0;
 
     // Staked ApeCoin
     const stakedApeCoinContractAddress = '0x5954aB967Bc958940b7EB73ee84797Dc8a2AFbb9';
-    const stakedApeCoin = new Contract(
-      stakedApeCoinContractAddress,
-      ApeCoinStaking,
-      mainnetProvider,
-    );
+    const stakedApeCoin = new Contract(stakedApeCoinContractAddress, ApeCoinStaking, provider);
     const stakedApeCoinBalance = await stakedApeCoin.stakedTotal(account, {
-      blockTag: parseBlockTag(mainnetBlockNumber),
+      blockTag: parseBlockTag(blockTag),
     });
     const parsedStakedApeCoinBalance = new BigNumber(
       formatUnits(stakedApeCoinBalance, 18).toString(),
@@ -47,9 +49,9 @@ export const thankApe: StrategyFactory<thankApeStratArgs> = (
     const balanceOfErc20Args = {
       strategyName: 'balanceOfErc20',
       account: account,
-      provider: mainnetProvider,
+      provider: provider,
       contract: apeCoinContractAddress,
-      blockTag: mainnetBlockNumber,
+      blockTag: blockTag,
       decimals: 18,
       multiplier: 1,
     } as balanceOfErc20StratArgs;
@@ -62,7 +64,7 @@ export const thankApe: StrategyFactory<thankApeStratArgs> = (
     if (totalApeCoin >= 10000) return 40;
     if (totalApeCoin >= 1000) return 20;
     if (totalApeCoin >= 100) return 10;
-    if (totalApeCoin >= 10 || hasVoteRight) return 5;
+    if (totalApeCoin >= 10) return 5;
     if (totalApeCoin >= 1) return 1;
     return 0;
   };
