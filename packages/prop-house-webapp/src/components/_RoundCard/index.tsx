22b3e14ddd9f2c19cf5afeaed35e96b3baa0d08b
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2023-09-19 15:05:15 -0400

    adapt round header to new round obj

--- packages/prop-house-webapp/src/components/_RoundCard/index.tsx
+++ packages/prop-house-webapp/src/components/_RoundCard/index.tsx
@@ -3,32 +3,42 @@ import { RoundWithHouse } from '@prophouse/sdk-react';
 import Card, { CardBgColor, CardBorderRadius } from '../Card';
 import EthAddress from '../EthAddress';
 import AwardsDisplay from '../AwardsDisplay';
+import { useNavigate } from 'react-router-dom';
+import { nameToSlug } from '../../utils/communitySlugs';
 
 const RoundCard_: React.FC<{ round: RoundWithHouse }> = props => {
   const { round } = props;
 
+  let navigate = useNavigate();
+
   return (
-    <Card
-      bgColor={CardBgColor.White}
-      borderRadius={CardBorderRadius.twenty}
-      classNames={classes.roundCard}
+    <div
+      onClick={e => {
+        navigate(`/${nameToSlug(round.address)}`);
+      }}
     >
-      <div className={classes.container}>
-        <div className={classes.title}>{round.title}</div>
-        <div className={classes.creatorAndAwardContainer}>
-          <EthAddress
-            address={round.house.address}
-            imgSrc={round.house.imageURI?.replace(
-              /prophouse.mypinata.cloud/g,
-              'cloudflare-ipfs.com',
-            )}
-            addAvatar={true}
-            className={classes.roundCreator}
-          />
-          <AwardsDisplay awards={round.config.awards} />
+      <Card
+        bgColor={CardBgColor.White}
+        borderRadius={CardBorderRadius.twenty}
+        classNames={classes.roundCard}
+      >
+        <div className={classes.container}>
+          <div className={classes.title}>{round.title}</div>
+          <div className={classes.creatorAndAwardContainer}>
+            <EthAddress
+              address={round.house.address}
+              imgSrc={round.house.imageURI?.replace(
+                /prophouse.mypinata.cloud/g,
+                'cloudflare-ipfs.com',
+              )}
+              addAvatar={true}
+              className={classes.roundCreator}
+            />
+            <AwardsDisplay awards={round.config.awards} />
+          </div>
         </div>
-      </div>
-    </Card>
+      </Card>
+    </div>
   );
 };
 
--- packages/prop-house-webapp/src/utils/auctionStatus.ts
+++ packages/prop-house-webapp/src/utils/auctionStatus.ts
@@ -1,6 +1,7 @@
 import { StoredAuctionBase, StoredTimedAuction } from '@nouns/prop-house-wrapper/dist/builders';
 import dayjs from 'dayjs';
 import { isInfAuction } from './auctionType';
+import { Round } from '@prophouse/sdk-react';
 
 export enum AuctionStatus {
   AuctionNotStarted,
@@ -37,6 +38,32 @@ export const auctionStatus = (auction: StoredAuctionBase): AuctionStatus => {
   }
 };
 
+/**
+ * Calculates auction state
+ * @param auction Auction to check status of.
+ */
+export const _auctionStatus = (round: Round): AuctionStatus => {
+  const _now = dayjs();
+  const _roundStartTime = dayjs(round.config.proposalPeriodStartTimestamp);
+
+  if (_now.isBefore(_roundStartTime)) return AuctionStatus.AuctionNotStarted;
+
+  const _proposalEndTime = dayjs(round.config.proposalPeriodEndTimestamp);
+  const _votingEndTime = dayjs(round.config.votePeriodEndTimestamp);
+
+  switch (true) {
+    case _now.isBefore(_roundStartTime):
+      return AuctionStatus.AuctionNotStarted;
+    case _now.isAfter(_roundStartTime) && _now.isBefore(_proposalEndTime):
+      return AuctionStatus.AuctionAcceptingProps;
+    case _now.isAfter(_proposalEndTime) && _now.isBefore(_votingEndTime):
+      return AuctionStatus.AuctionVoting;
+    case _now.isAfter(_votingEndTime):
+      return AuctionStatus.AuctionEnded;
+    default:
+      return AuctionStatus.AuctionEnded;
+  }
+};
 /**
  * Returns copy for deadline corresponding to auction status
  */
