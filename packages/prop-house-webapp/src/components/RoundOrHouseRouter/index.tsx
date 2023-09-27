c08929d78bee15b62a7a3b3e047a4894f35c5ca6
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2023-09-27 16:19:48 -0400

    fix route comp to manage switching between house and round addresses

--- packages/prop-house-webapp/src/components/RoundOrHouseRouter/index.tsx
+++ packages/prop-house-webapp/src/components/RoundOrHouseRouter/index.tsx
@@ -1,51 +1,60 @@
 import { usePropHouse } from '@prophouse/sdk-react';
 import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import HousePage from '../../pages/HousePage';
 import RoundPage from '../../pages/RoundPage';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { setOnchainActiveHouse, setOnchainActiveRound } from '../../state/slices/propHouse';
 import LoadingIndicator from '../LoadingIndicator';
 
-const RoundOrHouseRouter: React.FC<{}> = props => {
-  const location = useLocation();
-  const roundOrHouseAddress = location.pathname.substring(1).split('/')[0];
+const RoundOrHouseRouter: React.FC<{}> = () => {
+  const { roundOrHouse: roundOrHouseAddress } = useParams();
 
   const propHouse = usePropHouse();
   const dispatch = useAppDispatch();
 
   const round = useAppSelector(state => state.propHouse.onchainActiveRound);
   const house = useAppSelector(state => state.propHouse.onchainActiveHouse);
+
   const [loading, setLoading] = useState(false);
+  const [isRoundOrHouse, setIsRoundOrHouse] = useState<'round' | 'house'>();
   const [errorFetchingRoundAndHouse, setErrorFetchingRoundAndHouse] = useState(false);
+  const [lastAddressFetched, setLastAddressFetched] = useState<string>();
 
   useEffect(() => {
-    if (round || house || errorFetchingRoundAndHouse) return;
+    if (lastAddressFetched === roundOrHouseAddress || !roundOrHouseAddress || loading) return;
 
     const fetchRoundOrHouse = async () => {
       setLoading(true);
       try {
         dispatch(setOnchainActiveHouse(await propHouse.query.getHouse(roundOrHouseAddress)));
+        setIsRoundOrHouse('house');
+        setLastAddressFetched(roundOrHouseAddress);
         setLoading(false);
       } catch (e) {
         try {
           const roundWithHouse = await propHouse.query.getRoundWithHouseInfo(roundOrHouseAddress);
           dispatch(setOnchainActiveRound(roundWithHouse));
           dispatch(setOnchainActiveHouse(roundWithHouse.house));
+          setIsRoundOrHouse('round');
+          setLastAddressFetched(roundOrHouseAddress);
           setLoading(false);
         } catch (e) {
-          setLoading(false);
           setErrorFetchingRoundAndHouse(true);
+          setLoading(false);
         }
       }
     };
     fetchRoundOrHouse();
-  });
+  }, [round, house, roundOrHouseAddress]);
 
+  if (errorFetchingRoundAndHouse) return <>Error fetching round or house</>;
   if (loading) return <LoadingIndicator />;
-  if (!loading && errorFetchingRoundAndHouse) return <>Errror fetching round or house</>;
 
-  return round ? <RoundPage /> : <HousePage />;
+  if (isRoundOrHouse === 'house' && house) return <HousePage />;
+  if (isRoundOrHouse === 'round' && round && house) return <RoundPage />;
+
+  return <>Error</>;
 };
 
 export default RoundOrHouseRouter;
