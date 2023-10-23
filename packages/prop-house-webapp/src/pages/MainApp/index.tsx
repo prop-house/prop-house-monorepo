d1ab01dec2687cbd5283c67fff75d6c08e07a22a
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2023-10-23 16:02:34 -0400

    test mobile layout

--- packages/prop-house-webapp/src/pages/MainApp/index.tsx
+++ packages/prop-house-webapp/src/pages/MainApp/index.tsx
@@ -1,10 +1,11 @@
 import classes from './MainApp.module.css';
 import { House, Proposal, RoundWithHouse, Vote, usePropHouse } from '@prophouse/sdk-react';
 import { useEffect, useState } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row, Tab, Tabs } from 'react-bootstrap';
 import EthAddress from '../../components/EthAddress';
 import { useNavigate } from 'react-router-dom';
 import RoundCard from '../../components/RoundCard';
+import { isMobile } from 'web3modal';
 
 const MainApp = () => {
   const prophouse = usePropHouse();
@@ -77,87 +78,114 @@ const MainApp = () => {
     fetchVotes();
   });
 
+  const housesFeed = (
+    <Col xl={2}>
+      <h5 style={{ marginBottom: '16px' }}>Communities</h5>
+      <div className={classes.housesContainer}>
+        {houses &&
+          houses.map((house, i) => {
+            return (
+              <div
+                key={i}
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  fontWeight: 'bold',
+                  color: 'var(--brand-gray)',
+                  cursor: 'pointer',
+                }}
+                onClick={() => {
+                  navigate(`/${house.address}`);
+                }}
+              >
+                <img
+                  src={house.imageURI?.replace(/prophouse.mypinata.cloud/g, 'cloudflare-ipfs.com')}
+                  alt="house profile"
+                  style={{ height: 16, width: 16, borderRadius: 8, marginRight: 6 }}
+                />
+                {house.name}
+              </div>
+            );
+          })}
+      </div>
+    </Col>
+  );
+
+  const roundsFeed = (
+    <Col xl={5} className="mx-auto">
+      <h5>Rounds</h5>
+      <div className={classes.roundsContainer}>
+        {rounds &&
+          rounds.map((round, i) => {
+            return (
+              <Col xl={12} key={i}>
+                <RoundCard round={round} house={round.house} />
+              </Col>
+            );
+          })}
+      </div>
+    </Col>
+  );
+
+  const activityFeed = (
+    <Col xl={4}>
+      <h5 style={{ marginBottom: '16px' }}>Activity</h5>
+      <div className={classes.activityContainer}>
+        {activity &&
+          activity.map((item, i) => {
+            if ('proposer' in item) {
+              return (
+                <p className={classes.activityItem} key={i}>
+                  <EthAddress
+                    address={item.proposer}
+                    addAvatar={true}
+                    avatarSize={12}
+                    className={classes.address}
+                  />
+                  &nbsp;proposed&nbsp;{item.title}
+                </p>
+              );
+            }
+            return (
+              <p className={classes.activityItem} key={i}>
+                <EthAddress
+                  address={item.voter}
+                  addAvatar={true}
+                  avatarSize={12}
+                  className={classes.address}
+                />
+                &nbsp;voted&nbsp;{item.votingPower}&nbsp;
+              </p>
+            );
+          })}
+      </div>
+    </Col>
+  );
+
   return (
     <Container>
       <Row>
-        <Col xl={2}>
-          <h5 style={{ marginBottom: '16px' }}>Communities</h5>
-          <div className={classes.housesContainer}>
-            {houses &&
-              houses.map((house, i) => {
-                return (
-                  <div
-                    key={i}
-                    style={{
-                      display: 'flex',
-                      alignItems: 'center',
-                      fontWeight: 'bold',
-                      color: 'var(--brand-gray)',
-                      cursor: 'pointer',
-                    }}
-                    onClick={() => {
-                      navigate(`/${house.address}`);
-                    }}
-                  >
-                    <img
-                      src={house.imageURI?.replace(
-                        /prophouse.mypinata.cloud/g,
-                        'cloudflare-ipfs.com',
-                      )}
-                      alt="house profile"
-                      style={{ height: 16, width: 16, borderRadius: 8, marginRight: 6 }}
-                    />
-                    {house.name}
-                  </div>
-                );
-              })}
-          </div>
-        </Col>
-        <Col xl={5} className="mx-auto">
-          <h5>Rounds</h5>
-          <div className={classes.roundsContainer}>
-            {rounds &&
-              rounds.map((round, i) => {
-                return (
-                  <Col xl={12} key={i}>
-                    <RoundCard round={round} house={round.house} />
-                  </Col>
-                );
-              })}
-          </div>
-        </Col>
-        <Col xl={4}>
-          <h5 style={{ marginBottom: '16px' }}>Activity</h5>
-          <div className={classes.activityContainer}>
-            {activity &&
-              activity.map((item, i) => {
-                if ('proposer' in item) {
-                  return (
-                    <p className={classes.activityItem} key={i}>
-                      <EthAddress
-                        address={item.proposer}
-                        addAvatar={true}
-                        avatarSize={12}
-                        className={classes.address}
-                      />
-                      &nbsp;proposed&nbsp;{item.title}
-                    </p>
-                  );
-                }
-                return (
-                  <p className={classes.activityItem} key={i}>
-                    <EthAddress
-                      address={item.voter}
-                      addAvatar={true}
-                      avatarSize={12}
-                      className={classes.address}
-                    />
-                    &nbsp;voted&nbsp;{item.votingPower}&nbsp;
-                  </p>
-                );
-              })}
-          </div>
-        </Col>
+        {isMobile() ? (
+          <>
+            <Tabs defaultActiveKey="rounds" className="mb-3">
+              <Tab eventKey="houses" title="Houses">
+                {housesFeed}
+              </Tab>
+              <Tab eventKey="rounds" title="Rounds">
+                {roundsFeed}
+              </Tab>
+              <Tab eventKey="activity" title="Activity">
+                {activityFeed}
+              </Tab>
+            </Tabs>
+          </>
+        ) : (
+          <>
+            {housesFeed}
+            {roundsFeed}
+            {activityFeed}
+          </>
+        )}
       </Row>
     </Container>
   );
