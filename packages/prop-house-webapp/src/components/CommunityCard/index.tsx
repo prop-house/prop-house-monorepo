1f6241af975a01e45bddd168a01004e44ea84ad3
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2023-11-01 18:43:10 -0400

    add click through for community cards

--- packages/prop-house-webapp/src/components/CommunityCard/index.tsx
+++ packages/prop-house-webapp/src/components/CommunityCard/index.tsx
@@ -3,31 +3,36 @@ import classes from './CommunityCard.module.css';
 import { House } from '@prophouse/sdk-react';
 import Jazzicon from 'react-jazzicon/dist/Jazzicon';
 import Card, { CardBgColor, CardBorderRadius } from '../Card';
+import { useNavigate } from 'react-router-dom';
 
 const CommunityCard: React.FC<{
   house: House;
 }> = props => {
   const { house } = props;
+  const navigate = useNavigate();
 
   return (
-    <Card
-      bgColor={CardBgColor.White}
-      borderRadius={CardBorderRadius.ten}
-      classNames={classes.cardContainer}
-    >
-      {house.imageURI?.includes('prop.house') ? (
-        <img
-          src={house.imageURI?.replace(/prophouse.mypinata.cloud/g, 'cloudflare-ipfs.com')}
-          alt="house profile"
-          className={classes.image}
-        />
-      ) : (
-        <span style={{ marginRight: 6 }}>
-          <Jazzicon diameter={20} seed={jsNumberForAddress(house.address)} />
-        </span>
-      )}
-      <div className={classes.name}>{house.name}</div>
-    </Card>
+    <div onClick={() => navigate(`/${house.address}`)}>
+      <Card
+        bgColor={CardBgColor.White}
+        borderRadius={CardBorderRadius.ten}
+        classNames={classes.cardContainer}
+        onHoverEffect={true}
+      >
+        {house.imageURI?.includes('prop.house') ? (
+          <img
+            src={house.imageURI?.replace(/prophouse.mypinata.cloud/g, 'cloudflare-ipfs.com')}
+            alt="house profile"
+            className={classes.image}
+          />
+        ) : (
+          <span style={{ marginRight: 6 }}>
+            <Jazzicon diameter={20} seed={jsNumberForAddress(house.address)} />
+          </span>
+        )}
+        <div className={classes.name}>{house.name}</div>
+      </Card>
+    </div>
   );
 };
 
