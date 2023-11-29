c7e52361880453e55a8414421b27b90c4e87cf94
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2023-11-29 14:44:28 -0500

    add trophy

--- packages/prop-house-webapp/src/components/ProposalRankings/index.tsx
+++ packages/prop-house-webapp/src/components/ProposalRankings/index.tsx
@@ -1,12 +1,17 @@
+import { HiTrophy } from 'react-icons/hi2';
 import Avatar from '../Avatar';
 import classes from './ProposalRankings.module.css';
 import { Proposal } from '@prophouse/sdk-react';
+import { trophyColors } from '../../utils/trophyColors';
 
 const ProposalRankings: React.FC<{ proposals: Proposal[]; areWinners?: boolean }> = props => {
   const { proposals, areWinners } = props;
   return (
     <div className={classes.container}>
-      <div className={classes.rankingTitle}>{`# ${areWinners ? 'Winners' : 'Rankings'}`}</div>
+      <div className={classes.rankingTitle}>
+        <HiTrophy size={14} color={trophyColors('first')} />
+        {`${areWinners ? 'Winners' : 'Rankings'}`}
+      </div>
       {proposals.map((p, index) => (
         <div key={index} className={classes.item}>
           <span className={classes.place}>{index + 1}.</span>{' '}
