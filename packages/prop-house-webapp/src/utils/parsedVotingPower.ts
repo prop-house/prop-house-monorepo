f294705d24205dbc1ea6c74759d051efa94dee37
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2024-01-10 11:20:07 -0500

    use bnjs to parse exponential notation

--- packages/prop-house-webapp/src/utils/parsedVotingPower.ts
+++ packages/prop-house-webapp/src/utils/parsedVotingPower.ts
@@ -1,16 +1,19 @@
 import { GOV_POWER_OVERRIDES } from './roundOverrides';
-import { BigNumber, ethers } from 'ethers';
+import { BigNumber as BN, ethers } from 'ethers';
+import BigNumber from 'bignumber.js';
 
 // Parses voting power using on decimal points in GOV_POWER_OVERRIDES
 export const parsedVotingPower = (votingPower: string, roundAddress: string) => {
+  const bigNumber = new BigNumber(votingPower);
+
   if (GOV_POWER_OVERRIDES[roundAddress]) {
     const formatted = ethers.utils.formatUnits(
-      votingPower,
+      bigNumber.toFixed(),
       GOV_POWER_OVERRIDES[roundAddress].decimals,
     );
-    const bn = BigNumber.from(parseInt(formatted));
-    return bn.gte(1) ? bn : BigNumber.from(0);
+    const bn = BN.from(parseInt(formatted));
+    return bn.gte(1) ? bn : BN.from(0);
   } else {
-    return BigNumber.from(votingPower);
+    return BN.from(votingPower);
   }
 };
