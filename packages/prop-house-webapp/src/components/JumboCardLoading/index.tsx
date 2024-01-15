577a39604b2b231a6504b0e265665b257e08ff22
cryptoseneca <85328329+cryptoseneca@users.noreply.github.com>
2024-01-15 14:01:45 -0500

    add loading cards for mobile

--- packages/prop-house-webapp/src/components/JumboCardLoading/index.tsx
+++ packages/prop-house-webapp/src/components/JumboCardLoading/index.tsx
@@ -1,35 +1,36 @@
 import clsx from 'clsx';
 import Card, { CardBgColor, CardBorderRadius } from '../Card';
 import classes from '../JumboRoundCard/JumboRoundCard.module.css';
+import customClasses from './JumboCardLoading.module.css';
 import 'react-loading-skeleton/dist/skeleton.css';
 import { Col, Row } from 'react-bootstrap';
 import Skeleton from 'react-loading-skeleton';
+import { isMobile } from 'web3modal';
 
 const JumboCardLoading = () => {
   return (
     <Card
       bgColor={CardBgColor.LightPurple}
       borderRadius={CardBorderRadius.twenty}
-      classNames={clsx(classes.roundCard)}
+      classNames={clsx(classes.roundCard, customClasses.roundCard)}
       onHoverEffect={false}
     >
       <Row>
-        <Col>
+        <Col lg={6}>
           <Skeleton height={30} style={{ marginBottom: '12px' }} />
           <Skeleton height={60} style={{ marginTop: 'auto' }} />
         </Col>
-        <Col>
-          <Skeleton height={30} style={{ marginBottom: '12px' }} />
+        <Col lg={6}>
+          <Skeleton
+            height={30}
+            style={{ marginBottom: '12px', marginTop: isMobile() ? '30px' : '0px' }}
+          />
           <Row>
-            <Col>
-              <Skeleton height={60} inline />
-            </Col>
-            <Col>
-              <Skeleton height={60} inline />
-            </Col>
-            <Col>
-              <Skeleton height={60} inline />
-            </Col>
+            {Array.from(Array(3).keys()).map(i => (
+              <Col>
+                <Skeleton height={60} inline />
+              </Col>
+            ))}
           </Row>
         </Col>
       </Row>
